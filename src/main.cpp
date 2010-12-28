a6a886fd29ccdfa11822780b322f01b83dd8d283
Alan Garny <agarny@hellix.com>
2010-12-28 18:06:16 +0000

    Some reformatting and cleaning up. Added an application icon for Windows and Linux (it was already done for Mac). Set the title of the main window to OpenCOR.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <QtGui/QApplication>
+
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication app(argc, argv);
 
-    return a.exec();
+    // Create the main window
+
+    MainWindow win;
+
+    // Set the application icon, but only for Linux, since in the case of
+    // Windows and Mac, it's done through CMake (see CMakeLists.txt)
+
+#ifdef Q_WS_X11
+    win.setWindowIcon(QIcon(QString(":appIcon")));
+#endif
+
+    // Set the name of the main window to OpenCOR
+
+    win.setWindowTitle("OpenCOR");
+
+    // Show the main window
+
+    win.show();
+
+    // Execute the application
+
+    return app.exec();
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,14 +1,19 @@
 #include "mainwindow.h"
+
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow)
 {
+    // Set up the GUI
+
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    // Delete the GUI
+
     delete ui;
 }
