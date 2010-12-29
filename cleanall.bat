4e1c4f1270dcbfeb3a1ccb1adfec3d46c2396e6b
Alan Garny <agarny@hellix.com>
2010-12-29 23:41:33 +0000

    Now build QtSingleApplication statically.

--- cleanall.bat
+++ cleanall.bat
@@ -7,6 +7,6 @@ CD build
 mingw32-make -j clean
 
 DEL /F *Make* install_manifest.txt OpenCOR* 1> NUL 2> NUL
-RMDIR /S /Q CMakeFiles 3rdparty src _CPack_Packages 1> NUL 2> NUL
+RMDIR /S /Q _CPack_Packages 3rdparty CMakeFiles src 1> NUL 2> NUL
 
 CD ..
