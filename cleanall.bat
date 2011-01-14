fb6e839e6ff7cc9cdc187a808ed21411b896f79c
Alan Garny <agarny@hellix.com>
2011-01-14 19:30:54 +0000

    Forgot to update the cleanall[.bat] files.

--- cleanall.bat
+++ cleanall.bat
@@ -6,7 +6,7 @@ CD build
 
 mingw32-make -j clean
 
-DEL /F *Make* install_manifest.txt OpenCOR* 1> NUL 2> NUL
+DEL /F *Make* install_manifest.txt OpenCOR* version.txt 1> NUL 2> NUL
 RMDIR /S /Q _CPack_Packages 3rdparty CMakeFiles src 1> NUL 2> NUL
 
 CD ..
