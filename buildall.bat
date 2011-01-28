62f9df5cc481acb92413e9c06fb3757ebde9def9
Alan Garny <agarny@hellix.com>
2011-01-28 21:36:35 +0000

    Minor updates.

--- buildall.bat
+++ buildall.bat
@@ -2,18 +2,22 @@
 
 TITLE Building OpenCOR...
 
+ECHO.
 ECHO ---------------------------------------
 ECHO Building the console version of OpenCOR
 ECHO ---------------------------------------
+ECHO.
 
 CD winConsole\build
 
 cmake -G "MinGW Makefiles" ..
 mingw32-make -j
 
+ECHO.
 ECHO -----------------------------------
 ECHO Building the GUI version of OpenCOR
 ECHO -----------------------------------
+ECHO.
 
 CD ..\..\build
 
--- cleanall.bat
+++ cleanall.bat
@@ -2,9 +2,11 @@
 
 TITLE Cleaning the OpenCOR environment...
 
+ECHO.
 ECHO ---------------------------------------
 ECHO Cleaning the console version of OpenCOR
 ECHO ---------------------------------------
+ECHO.
 
 CD winConsole\build
 
@@ -13,9 +15,11 @@ mingw32-make -j clean
 DEL /F *Make* OpenCOR* 1> NUL 2> NUL
 RMDIR /S /Q 3rdparty CMakeFiles 1> NUL 2> NUL
 
+ECHO.
 ECHO -----------------------------------
 ECHO Cleaning the GUI version of OpenCOR
 ECHO -----------------------------------
+ECHO.
 
 CD ..\..\build
 
