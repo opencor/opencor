cad3c213fa4fa4fb103966c17db78fa132ab219a
Alan Garny <agarny@hellix.com>
2011-01-29 18:11:46 +0000

    Some minor changes.

--- makeall.bat
+++ makeall.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
 
-TITLE Building OpenCOR...
+TITLE Making OpenCOR...
 
 ECHO.
-ECHO ---------------------------------------
-ECHO Building the console version of OpenCOR
-ECHO ---------------------------------------
+ECHO -------------------------------------
+ECHO Making the console version of OpenCOR
+ECHO -------------------------------------
 ECHO.
 
 CD winConsole\build
@@ -14,9 +14,9 @@ cmake -G "MinGW Makefiles" ..
 mingw32-make -j
 
 ECHO.
-ECHO -----------------------------------
-ECHO Building the GUI version of OpenCOR
-ECHO -----------------------------------
+ECHO ---------------------------------
+ECHO Making the GUI version of OpenCOR
+ECHO ---------------------------------
 ECHO.
 
 CD ..\..\build
