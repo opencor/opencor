d412c586d49d80857cfb38bd58880c9370f774cc
Alan Garny <agarny@hellix.com>
2011-01-30 00:53:56 +0000

    Finally found what used to make the .com (consolve) version of OpenCOR so damn big (the symbols were all included!). Also improved the cleanall.bat file.

--- cleanall.bat
+++ cleanall.bat
@@ -2,5 +2,5 @@
 
 TITLE Cleaning the OpenCOR environment...
 
-FOR /D %%I IN (build\*.* winConsole\build\*.*) DO RMDIR /S /Q %%I
-FOR    %%I IN (build\*.* winConsole\build\*.*) DO DEL /Q %%I
+FOR /D %%I IN (build\*.* winConsole\build\*.*) DO RMDIR /S /Q "%%I"
+FOR    %%I IN (build\*.* winConsole\build\*.*) DO DEL /Q "%%I"
