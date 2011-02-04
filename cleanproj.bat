e559f3ef345835014ebd51aba2ecbb076951cbb1
Alan Garny <agarny@hellix.com>
2011-02-04 12:13:15 +0000

    Done with implementing an 'optimised' build environment where one can decide to clean only OpenCOR's files or also the third-party libraries' files.

--- cleanproj.bat
+++ cleanproj.bat
@@ -2,5 +2,6 @@
 
 TITLE Cleaning the OpenCOR environment...
 
-FOR /D %%I IN (build\*.* winConsole\build\*.*) DO RMDIR /S /Q "%%I"
-FOR    %%I IN (build\*.* winConsole\build\*.*) DO DEL /Q "%%I"
+FOR /D %%I IN (build\CMakeFiles\*.* build\src\*.* winConsole\build\CMakeFiles\*.*) DO RMDIR /S /Q "%%I"
+FOR    %%I IN (build\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\*.* winConsole\build\CMakeFiles\*.*) DO DEL /Q "%%I"
+FOR    %%I IN (build\CMakeFiles build\src winConsole\build\CMakeFiles) DO RMDIR "%%I"
--- cleanprojall.bat
+++ cleanprojall.bat
@@ -2,5 +2,5 @@
 
 TITLE Cleaning all of the OpenCOR environment...
 
-FOR /D %%I IN (3rdparty\build\*.* build\*.* winConsole\build\*.*) DO RMDIR /S /Q "%%I"
-FOR    %%I IN (3rdparty\build\*.* build\*.* winConsole\build\*.*) DO DEL /Q "%%I"
+FOR /D %%I IN (build\*.* winConsole\build\*.*) DO RMDIR /S /Q "%%I"
+FOR    %%I IN (build\*.* winConsole\build\*.*) DO DEL /Q "%%I"
