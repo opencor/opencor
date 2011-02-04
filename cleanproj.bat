287715a4d6dc47bf0c2b7a43994d149dc64e8609
Alan Garny <agarny@hellix.com>
2011-02-04 16:00:31 +0000

    Minor fix to the cleanproj[.bat] files (didn't take into account the _CPack_Packages folder which gets creaked when packaging OpenCOR).

--- cleanproj.bat
+++ cleanproj.bat
@@ -2,6 +2,6 @@
 
 TITLE Cleaning the OpenCOR environment...
 
-FOR /D %%I IN (build\CMakeFiles\*.* build\src\*.* winConsole\build\CMakeFiles\*.*) DO RMDIR /S /Q "%%I"
-FOR    %%I IN (build\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\*.* winConsole\build\CMakeFiles\*.*) DO DEL /Q "%%I"
-FOR    %%I IN (build\CMakeFiles build\src winConsole\build\CMakeFiles) DO RMDIR "%%I"
+FOR /D %%I IN (build\_CPack_Packages\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\CMakeFiles\*.*) DO RMDIR /S /Q "%%I"
+FOR    %%I IN (build\*.* build\_CPack_Packages\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\*.* winConsole\build\CMakeFiles\*.*) DO DEL /Q "%%I"
+FOR    %%I IN (build\_CPack_Packages build\CMakeFiles build\src winConsole\build\CMakeFiles) DO RMDIR "%%I"
