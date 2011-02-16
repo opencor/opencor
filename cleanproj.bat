af803ce9b25799dd11562bc8793b02ec6922d7ec
Alan Garny <agarny@hellix.com>
2011-02-16 18:56:22 +0000

    Improved the cleanproj.bat file.

--- cleanproj.bat
+++ cleanproj.bat
@@ -4,4 +4,4 @@ TITLE Cleaning the OpenCOR environment...
 
 FOR /D %%I IN (build\_CPack_Packages\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\CMakeFiles\*.*) DO RMDIR /S /Q "%%I"
 FOR    %%I IN (build\*.* build\_CPack_Packages\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\*.* winConsole\build\CMakeFiles\*.*) DO DEL /Q "%%I"
-FOR    %%I IN (build\_CPack_Packages build\CMakeFiles build\src winConsole\build\CMakeFiles) DO RMDIR "%%I"
+FOR    %%I IN (build\_CPack_Packages build\CMakeFiles build\src winConsole\build\CMakeFiles) DO IF EXIST "%%I" ( RMDIR /Q "%%I" )
