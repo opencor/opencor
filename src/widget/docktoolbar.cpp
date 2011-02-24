844d9a6f8032ddf094e5bdb8bf851a9cead14d00
Alan Garny <agarny@hellix.com>
2011-02-24 19:21:20 +0000

    Being pedantic.

--- src/widget/docktoolbar.cpp
+++ src/widget/docktoolbar.cpp
@@ -6,7 +6,7 @@ DockToolBar::DockToolBar(QWidget *pParent) :
     // Remove the border that is normally drawn for a toolbar (indeed it doesn't
     // look great when on a dock window, so...)
 
-    setStyleSheet("QToolBar { border: 0px }");
+    setStyleSheet("QToolBar { border: 0px; }");
 
     // Set the size of the icons to 20 by 20 pixels
 
