0baa18d5bebccb0d4ce77a5155d33cb5560100d9
Alan Garny <agarny@hellix.com>
2011-06-07 14:08:27 +0100

    Further work on the initialisation macro.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -13,18 +13,35 @@ MACRO(INITIALISATION)
     IF("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
         SET(DEBUG_MODE ON)
 
-        MESSAGE("Debug build...")
+        MESSAGE("Building a debug version...")
     ELSE()
         SET(DEBUG_MODE OFF)
 
-        MESSAGE("Release build...")
+        MESSAGE("Building a release version...")
     ENDIF()
+SET(DEBUG_MODE ON)
 
-    # Default compiler settings
+    # Default compiler and linker settings
 
     IF(${DEBUG_MODE})
         SET(CMAKE_CXX_FLAGS "-g -O0")
     ELSE()
         SET(CMAKE_CXX_FLAGS "-O2 -ffast-math")
+    ENDIF()
+
+    SET(LINK_FLAGS_PROPERTIES)
+
+    IF(WIN32)
+        SET(LINK_FLAGS_PROPERTIES "${LINK_FLAGS_PROPERTIES} -Wl,-enable-auto-import")
+        # Note: -Wl,-enable-auto-import allows us to use our various third-party
+        #       libraries without having to worry about importing their functions,
+        #       etc.
+    ENDIF()
+
+    IF(NOT APPLE AND NOT ${DEBUG_MODE})
+        SET(LINK_FLAGS_PROPERTIES "${LINK_FLAGS_PROPERTIES} -Wl,-s")
+        # Note #1: -Wl,-s strips all the symbols, thus reducing the final size of
+        #          OpenCOR
+        # Note #2: the above linking option has become obsolete on Mac OS X, so...
     ENDIF()
 ENDMACRO()
