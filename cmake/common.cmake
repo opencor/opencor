e8cb94ab94f7f48ff60f89bd8ed3d910715e57fc
Alan Garny <agarny@hellix.com>
2011-06-07 14:32:12 +0100

    Some more work on the initialisation macro.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -32,15 +32,26 @@ MACRO(INITIALISATION)
 
     IF(WIN32)
         SET(LINK_FLAGS_PROPERTIES "${LINK_FLAGS_PROPERTIES} -Wl,-enable-auto-import")
-        # Note: -Wl,-enable-auto-import allows us to use our various third-party
-        #       libraries without having to worry about importing their functions,
-        #       etc.
+        # Note: -Wl,-enable-auto-import allows us to use our various
+        #       third-party libraries without having to worry about importing
+        #       their functions, etc.
     ENDIF()
 
     IF(NOT APPLE AND NOT ${DEBUG_MODE})
         SET(LINK_FLAGS_PROPERTIES "${LINK_FLAGS_PROPERTIES} -Wl,-s")
-        # Note #1: -Wl,-s strips all the symbols, thus reducing the final size of
-        #          OpenCOR
-        # Note #2: the above linking option has become obsolete on Mac OS X, so...
+        # Note #1: -Wl,-s strips all the symbols, thus reducing the final size
+        #          of OpenCOR of one its shared libraries
+        # Note #2: the above linking option has become obsolete on Mac OS X,
+        #          so...
+    ENDIF()
+
+    # Default location for third-party libraries
+    # Note: this is only required so that we can quickly test third-party
+    #       libraries without first having to package everything
+
+    IF(APPLE)
+        SET(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/lib)
+    ELSE()
+        SET(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR})
     ENDIF()
 ENDMACRO()
