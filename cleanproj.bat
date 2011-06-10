e63161acad6f39bd6a0ca27766387c024d94c999
Alan Garny <agarny@hellix.com>
2011-06-10 21:29:32 +0100

    Removed cleanprojall[.bat] since we don't really need them now that the third-party libraries are plugins, and anyway it doesn't that long to compile everything.

--- cleanproj.bat
+++ cleanproj.bat
@@ -2,6 +2,5 @@
 
 TITLE Cleaning the OpenCOR environment...
 
-FOR /D %%I IN (build\_CPack_Packages\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\CMakeFiles\*.*) DO RMDIR /S /Q "%%I"
-FOR    %%I IN (build\*.* build\_CPack_Packages\*.* build\CMakeFiles\*.* build\src\*.* winConsole\build\*.* winConsole\build\CMakeFiles\*.*) DO DEL /Q "%%I"
-FOR    %%I IN (build\_CPack_Packages build\CMakeFiles build\src winConsole\build\CMakeFiles) DO IF EXIST "%%I" ( RMDIR /Q "%%I" )
+FOR /D %%I IN (build\*.* winConsole\build\*.*) DO RMDIR /S /Q "%%I"
+FOR    %%I IN (build\*.* winConsole\build\*.*) DO DEL /Q "%%I"
--- cleanprojall.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-TITLE Cleaning all of the OpenCOR environment...
-
-FOR /D %%I IN (build\*.* winConsole\build\*.*) DO RMDIR /S /Q "%%I"
-FOR    %%I IN (build\*.* winConsole\build\*.*) DO DEL /Q "%%I"
