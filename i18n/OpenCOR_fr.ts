1f3c6ad4819289162fd7936afa1ce98efdb03371
Alan Garny <agarny@hellix.com>
2011-06-20 12:34:18 +0100

    Added CheckForUpdatesWindow and PreferencesWindow to the Core plugin.

--- i18n/OpenCOR_fr.ts
+++ i18n/OpenCOR_fr.ts
@@ -2,13 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR" sourcelanguage="en_GB">
 <context>
-    <name>CheckForUpdatesWindow</name>
-    <message>
-        <source>Check for Updates</source>
-        <translation>Vérifier pour des Mises à Jour</translation>
-    </message>
-</context>
-<context>
     <name>MainWindow</name>
     <message>
         <source>&amp;File</source>
@@ -483,13 +476,6 @@
     <message>
         <source>Plugins</source>
         <translation>Extensions</translation>
-    </message>
-</context>
-<context>
-    <name>PreferencesWindow</name>
-    <message>
-        <source>Preferences</source>
-        <translation>Préférences</translation>
     </message>
 </context>
 </TS>
---
+++ src/plugins/misc/Core/i18n/Core_fr.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en_GB">
+<context>
+    <name>CheckForUpdatesWindow</name>
+    <message>
+        <source>Check for Updates</source>
+        <translation>Vérifier pour des Mises à Jour</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesWindow</name>
+    <message>
+        <source>Preferences</source>
+        <translation>Préférences</translation>
+    </message>
+</context>
+</TS>
