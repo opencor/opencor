46efe23a8be00c04ab18f697a57ff6194762e257
Alan Garny <agarny@hellix.com>
2011-07-06 20:07:25 +0100

    Minor cleaning up.

--- src/plugins/misc/VersionManager/src/versionmanagerplugin.h
+++ src/plugins/misc/VersionManager/src/versionmanagerplugin.h
@@ -1,7 +1,6 @@
 #ifndef VERSIONMANAGERPLUGIN_H
 #define VERSIONMANAGERPLUGIN_H
 
-#include "guiinterface.h"
 #include "plugininterface.h"
 
 namespace OpenCOR {
@@ -10,12 +9,10 @@ namespace VersionManager {
 PLUGININFO_FUNC VersionManagerPluginInfo();
 
 class VersionManagerPlugin : public QObject,
-                             public PluginInterface,
-                             public GuiInterface
+                             public PluginInterface
 {
     Q_OBJECT
     Q_INTERFACES(OpenCOR::PluginInterface)
-    Q_INTERFACES(OpenCOR::GuiInterface)
 };
 
 } }
