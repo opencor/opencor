28d9d0b674e3631ec5554e2bbbbfe5791caaf23e
Alan Garny <agarny@hellix.com>
2011-08-03 09:29:34 +0100

    Updated the description for the Viewer plugin.

--- src/plugins/editing/Viewer/src/viewerplugin.cpp
+++ src/plugins/editing/Viewer/src/viewerplugin.cpp
@@ -7,8 +7,8 @@ PLUGININFO_FUNC ViewerPluginInfo()
 {
     QMap<QString, QString> descriptions;
 
-    descriptions.insert("en", "A plugin to graphically visualise mathematical equations");
-    descriptions.insert("fr", "Une extension pour visualiser graphiquement des équations mathématiques");
+    descriptions.insert("en", "A plugin to graphically visualise various concepts (e.g. mathematical equations)");
+    descriptions.insert("fr", "Une extension pour visualiser graphiquement différents concepts (par exemple des équations mathématiques)");
 
     return PluginInfo(PluginInterface::V001, PluginInfo::Gui,
                       PluginInfo::Editing, true,
