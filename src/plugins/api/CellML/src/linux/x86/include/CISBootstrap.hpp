311eb815f6acbadd920b99807d35e30a39450d7a
Alan Garny <agarny@hellix.com>
2011-08-31 08:45:21 +0100

    Added the Linux 32-bit version of the CellML 1.10rc1 binaries.

---
+++ src/plugins/api/CellML/src/linux/x86/include/CISBootstrap.hpp
@@ -0,0 +1,16 @@
+#include "cda_compiler_support.h"
+#ifdef IN_CIS_MODULE
+#define CIS_PUBLIC_PRE CDA_EXPORT_PRE
+#define CIS_PUBLIC_POST CDA_EXPORT_POST
+#else
+#define CIS_PUBLIC_PRE CDA_IMPORT_PRE
+#define CIS_PUBLIC_POST CDA_IMPORT_POST
+#endif
+
+/**
+ * Creates a new CIS integrator.
+ * This code is the external C++ interface which allows you to fetch the
+ * implementation.
+ */
+CIS_PUBLIC_PRE iface::cellml_services::CellMLIntegrationService*
+CreateIntegrationService(void) CIS_PUBLIC_POST;
