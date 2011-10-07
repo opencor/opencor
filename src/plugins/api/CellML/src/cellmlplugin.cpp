2847765e59c4fe11025d60fbe64817c790ad4643
Alan Garny <agarny@hellix.com>
2011-10-07 13:12:31 +1300

    Output the results of a CellML simulation in a temporary .CSV file.

--- src/plugins/api/CellML/src/cellmlplugin.cpp
+++ src/plugins/api/CellML/src/cellmlplugin.cpp
@@ -7,8 +7,8 @@
 #include "IfaceCIS.hxx"
 #include "CISBootstrap.hpp"
 
-#include <QDebug>
 #include <QDir>
+#include <QMessageBox>
 #include <QThread>
 #include <QUrl>
 
@@ -50,11 +50,13 @@ public:
         mRefCount(1),
         mFinished(false)
     {
+        // Determine the header for the results
+
         iface::cellml_services::ComputationTargetIterator *cti = mCodeInformation->iterateTargets();
 
         bool first = true;
 
-        mHeader = QString();
+        QString header = QString();
 
         while (true) {
             iface::cellml_services::ComputationTarget* ct = cti->nextComputationTarget();
@@ -67,13 +69,23 @@ public:
                    || (ct->type() == iface::cellml_services::VARIABLE_OF_INTEGRATION))
                 && (ct->degree() == 0)) {
                 if (!first)
-                    mHeader += ",";
+                    header += ",";
                 else
                     first = false;
 
-                mHeader += QString::fromWCharArray(ct->variable()->name());
+                header += QString::fromWCharArray(ct->variable()->name());
             }
         }
+
+        // Output the header to our results file
+
+        mResultsFile.setFileName(QDir::tempPath()+QDir::separator()+"test_cellml_results.csv");
+
+        mResultsFile.remove();   // Just in case...
+
+        mResultsFile.open( QIODevice::WriteOnly);
+
+        writeResults(header);
     }
 
     virtual void add_ref() throw(std::exception &)
@@ -104,37 +116,13 @@ public:
         return 0;
     }
 
-    virtual void computedConstants(uint32_t, double *pValues) throw (std::exception &)
+    virtual void computedConstants(uint32_t, double *) throw (std::exception &)
     {
-        iface::cellml_services::ComputationTargetIterator *targetsIter = mCodeInformation->iterateTargets();
-
-        qDebug("    Computed constants...");
-
-        while (true) {
-            iface::cellml_services::ComputationTarget *target = targetsIter->nextComputationTarget();
-
-            if (!target)
-                break;
-
-            if (   (target->type() == iface::cellml_services::CONSTANT)
-                && !target->degree())
-                qDebug("        %s = %g",
-                       QString::fromWCharArray(target->variable()->name()).toLatin1().constData(),
-                       pValues[target->assignedIndex()]);
-        }
+        // Do nothing...
     }
 
     virtual void results(uint32_t pNbOfValues, double *pValues) throw (std::exception&)
     {
-        static bool needInit = true;
-
-        if (needInit) {
-            qDebug("    Results...");
-            qDebug("        %s", mHeader.toLatin1().constData());
-
-            needInit = false;
-        }
-
         uint32_t rateIndexCount = mCodeInformation->rateIndexCount();
         uint32_t recSize =  2*rateIndexCount
                            +mCodeInformation->algebraicIndexCount()
@@ -188,7 +176,7 @@ public:
                 values += QString::number(pValues[i+varOff]);
             }
 
-            qDebug("        %s", values.toLatin1().constData());
+            writeResults(values);
         }
     }
 
@@ -199,7 +187,7 @@ public:
 
     virtual void failed(const char *pErrorMsg) throw (std::exception &)
     {
-        qDebug("ERROR: integration failed (%s).", pErrorMsg);
+        QMessageBox::information(0, "CellML Error", QString("Integration failed (%1)").arg(pErrorMsg));
 
         mFinished = true;
     }
@@ -209,6 +197,12 @@ public:
         return mFinished;
     }
 
+    void writeResults(const QString &pResults)
+    {
+        mResultsFile.write((pResults+"\n").toLatin1().constData());
+        mResultsFile.flush();
+    }
+
 private:
     iface::cellml_services::CellMLCompiledModel *mCompiledModel;
     iface::cellml_services::CodeInformation *mCodeInformation;
@@ -216,7 +210,7 @@ private:
     uint32_t mRefCount;
     bool mFinished;
 
-    QString mHeader;
+    QFile mResultsFile;
 };
 
 void CellMLPlugin::initialize()
@@ -237,96 +231,33 @@ void CellMLPlugin::initialize()
 
     Core::saveResourceAs(":test_cellml_model", testCellmlModelFileName);
 
-    qDebug("Loading the model...");
-
     iface::cellml_api::Model *model;
 
     try {
         model = ml->loadFromURL(QUrl::fromLocalFile(testCellmlModelFileName).toString().toStdWString().c_str());
     } catch (iface::cellml_api::CellMLException &) {
-        qDebug("ERROR: %s.",
-               QString::fromWCharArray(ml->lastErrorMessage()).toLatin1().constData());
+        QMessageBox::information(0, "CellML Error", QString::fromWCharArray(ml->lastErrorMessage()));
 
         return;
     }
 
-    qDebug();
-    qDebug("Retrieving some model properties...");
-
-    // Retrieve the model's name
-
-    qDebug("    Model '%s'",
-           QString::fromWCharArray(model->name()).toLatin1().constData());
-
-    // Go through the model's components and their respective variables
-
-    iface::cellml_api::CellMLComponentSet *comps = model->modelComponents();
-    iface::cellml_api::CellMLComponentIterator *compsIter = comps->iterateComponents();
-
-    for (int i = 0; i < comps->length(); i++) {
-        iface::cellml_api::CellMLComponent *comp = compsIter->nextComponent();
-
-        qDebug("        Component '%s'",
-               QString::fromWCharArray(comp->name()).toLatin1().constData());
-
-        iface::cellml_api::CellMLVariableSet *vars = comp->variables();
-        iface::cellml_api::CellMLVariableIterator *varsIter = vars->iterateVariables();
-
-        for(int j = 0; j < vars->length(); ++j)
-            qDebug("            Variable '%s'",
-                   QString::fromStdWString(varsIter->nextVariable()->name()).toLatin1().constData());
-    }
-
-    // Go through the model's connections and information
-
-    iface::cellml_api::ConnectionSet *conns = model->connections();
-    iface::cellml_api::ConnectionIterator *connsIter = conns->iterateConnections();
-
-    for (int i = 0; i < conns->length(); i++) {
-        iface::cellml_api::Connection *conn = connsIter->nextConnection();
-        iface::cellml_api::MapComponents *compMap = conn->componentMapping();
-
-        qDebug("        Connection between '%s' and '%s'",
-               QString::fromStdWString(compMap->firstComponentName()).toLatin1().constData(),
-               QString::fromStdWString(compMap->secondComponentName()).toLatin1().constData());
-
-        iface::cellml_api::MapVariablesSet *varMaps = conn->variableMappings();
-        iface::cellml_api::MapVariablesIterator *varMapsIter = varMaps->iterateMapVariables();
-
-        for(int j = 0; j < varMaps->length(); ++j) {
-            iface::cellml_api::MapVariables *mapVars = varMapsIter->nextMapVariables();
-
-            qDebug("            For variables '%s' and '%s'",
-                   QString::fromStdWString(mapVars->firstVariableName()).toLatin1().constData(),
-                   QString::fromStdWString(mapVars->secondVariableName()).toLatin1().constData());
-        }
-    }
-
     // Run the model
-
-    qDebug();
-    qDebug("Running the model...");
 
     iface::cellml_services::CellMLIntegrationService* cis = CreateIntegrationService();
     iface::cellml_services::ODESolverCompiledModel* compiledModel = 0;
-
-    qDebug("    Compiling the model...");
 
     try
     {
         compiledModel = cis->compileModelODE(model);
     } catch (iface::cellml_api::CellMLException &) {
-        qDebug("ERROR: %s.",
-               QString::fromWCharArray(cis->lastError()).toLatin1().constData());
+        QMessageBox::information(0, "CellML Error", QString::fromWCharArray(cis->lastError()));
 
         return;
     } catch (...) {
-        qDebug("ERROR: unexpected exception calling compileModel.");
+        QMessageBox::information(0, "CellML Error", "Unexpected exception calling compileModel.");
 
         return;
     }
-
-    qDebug("    Creating the ODE solver run...");
 
     iface::cellml_services::ODESolverRun *odeSolverRun = cis->createODEIntegrationRun(compiledModel);
 
@@ -342,11 +273,6 @@ void CellMLPlugin::initialize()
 
     while (!progressObserver->finished())
         SleeperThread::msleep(1);
-
-    // All done...
-
-    qDebug();
-    qDebug("All done...");
 
     QFile::remove(testCellmlModelFileName);
 }
