1cb4928b871fefbf5b4dff86cfd1d42dceb063f5
Alan Garny <agarny@hellix.com>
2011-10-17 23:13:22 +0200

    Added a plugin to get access to Nanojit.

---
+++ src/plugins/3rdparty/Nanojit/src/nanojitplugin.cpp
@@ -0,0 +1,40 @@
+//==============================================================================
+// Nanojit plugin
+//==============================================================================
+
+#include "nanojitplugin.h"
+
+//==============================================================================
+
+namespace OpenCOR {
+namespace Nanojit {
+
+//==============================================================================
+
+PLUGININFO_FUNC NanojitPluginInfo()
+{
+    Descriptions descriptions;
+
+    descriptions.insert("en", "A plugin to use <a href=\"https://developer.mozilla.org/En/Nanojit/\">Nanojit</a>");
+    descriptions.insert("fr", "Une extension pour utiliser <a href=\"https://developer.mozilla.org/En/Nanojit/\">Nanojit</a>");
+
+    return PluginInfo(PluginInfo::V001,
+                      PluginInfo::General,
+                      PluginInfo::Api,
+                      false,
+                      QStringList(),
+                      descriptions);
+}
+
+//==============================================================================
+
+Q_EXPORT_PLUGIN2(Nanojit, NanojitPlugin)
+
+//==============================================================================
+
+}   // namespace Nanojit
+}   // namespace OpenCOR
+
+//==============================================================================
+// End of file
+//==============================================================================
