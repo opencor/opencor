5b06c232ee8a6639bab3e94e5c0a49d67d3a709f
Alan Garny <agarny@hellix.com>
2011-11-03 19:19:42 +0100

    A few more files that Git didn't include in the previous commit. Note: OpenCOR doesn't build at the moment. Still need to work on LLVM...

--- src/plugins/simulation/CoreSimulation/src/coresimulationplugin.cpp
+++ src/plugins/simulation/CoreSimulation/src/coresimulationplugin.cpp
@@ -13,6 +13,20 @@
 
 //==============================================================================
 
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/Constants.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/Instructions.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/ExecutionEngine/Interpreter.h"
+#include "llvm/ExecutionEngine/GenericValue.h"
+#include "llvm/Target/TargetSelect.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/raw_ostream.h"
+
+//==============================================================================
+
 #include "qwt_plot.h"
 #include "qwt_plot_curve.h"
 #include "qwt_plot_grid.h"
@@ -47,6 +61,89 @@ Q_EXPORT_PLUGIN2(CoreSimulation, CoreSimulationPlugin)
 
 void CoreSimulationPlugin::initialize()
 {
+    // Testing the JIT compilation side of LLVM
+    // Note: this is shameless copying/pasting of the How-to-use JIT example...
+
+    InitializeNativeTarget();
+
+    LLVMContext Context;
+
+    // Create some module to put our function into it.
+    Module *M = new Module("test", Context);
+
+    // Create the add1 function entry and insert this entry into module M.  The
+    // function will have a return type of "int" and take an argument of "int".
+    // The '0' terminates the list of argument types.
+    Function *Add1F =
+      cast<Function>(M->getOrInsertFunction("add1", Type::getInt32Ty(Context),
+                                            Type::getInt32Ty(Context),
+                                            (Type *)0));
+
+    // Add a basic block to the function. As before, it automatically inserts
+    // because of the last argument.
+    BasicBlock *BB = BasicBlock::Create(Context, "EntryBlock", Add1F);
+
+    // Get pointers to the constant `1'.
+    Value *One = ConstantInt::get(Type::getInt32Ty(Context), 1);
+
+    // Get pointers to the integer argument of the add1 function...
+    assert(Add1F->arg_begin() != Add1F->arg_end()); // Make sure there's an arg
+    Argument *ArgX = Add1F->arg_begin();  // Get the arg
+    ArgX->setName("AnArg");            // Give it a nice symbolic name for fun.
+
+    // Create the add instruction, inserting it into the end of BB.
+    Instruction *Add = BinaryOperator::CreateAdd(One, ArgX, "addresult", BB);
+
+    // Create the return instruction and add it to the basic block
+    ReturnInst::Create(Context, Add, BB);
+
+    // Now, function add1 is ready.
+
+
+    // Now we going to create function `foo', which returns an int and takes no
+    // arguments.
+    Function *FooF =
+      cast<Function>(M->getOrInsertFunction("foo", Type::getInt32Ty(Context),
+                                            (Type *)0));
+
+    // Add a basic block to the FooF function.
+    BB = BasicBlock::Create(Context, "EntryBlock", FooF);
+
+    // Get pointers to the constant `10'.
+    Value *Ten = ConstantInt::get(Type::getInt32Ty(Context), 10);
+
+    // Pass Ten to the call call:
+    CallInst *Add1CallRes = CallInst::Create(Add1F, Ten, "add1", BB);
+    Add1CallRes->setTailCall(true);
+
+    // Create the return instruction and add it to the basic block.
+    ReturnInst::Create(Context, Add1CallRes, BB);
+
+    // Now we create the JIT.
+    ExecutionEngine* EE = EngineBuilder(M).create();
+
+    outs() << "We just constructed this LLVM module:\n\n" << *M;
+    outs() << "\n\nRunning foo: ";
+    outs().flush();
+
+    // Call the `foo' function with no arguments:
+    std::vector<GenericValue> noargs;
+    GenericValue gv = EE->runFunction(FooF, noargs);
+
+    // Import result of execution:
+    outs() << "Result: " << gv.IntVal << "\n";
+    EE->freeMachineCodeForFunction(FooF);
+    delete EE;
+    llvm_shutdown();
+
+
+
+
+
+
+
+
+
     // Create our simulation view widget
 
     mSimulationView = new QwtPlot(mMainWindow);
