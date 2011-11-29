3c8724165cccafded8ead6f890d3d5ca9c04abea
Alan Garny <agarny@hellix.com>
2011-11-29 11:00:55 +0100

    Added some basic code for our forthcoming CVODE and IDA plugins.

---
+++ src/plugins/3rdparty/CVODE/src/cvodeplugin.cpp
@@ -0,0 +1,40 @@
+//==============================================================================
+// CVODE plugin
+//==============================================================================
+
+#include "cvodeplugin.h"
+
+//==============================================================================
+
+namespace OpenCOR {
+namespace CVODE {
+
+//==============================================================================
+
+PLUGININFO_FUNC CVODEPluginInfo()
+{
+    Descriptions descriptions;
+
+    descriptions.insert("en", "A plugin to use <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html#descr_cvode\">CVODE</a>");
+    descriptions.insert("fr", "Une extension pour utiliser <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html#descr_cvode\">CVODE</a>");
+
+    return PluginInfo(PluginInfo::V001,
+                      PluginInfo::Gui,
+                      PluginInfo::ThirdParty,
+                      false,
+                      QStringList(),
+                      descriptions);
+}
+
+//==============================================================================
+
+Q_EXPORT_PLUGIN2(CVODE, CVODEPlugin)
+
+//==============================================================================
+
+}   // namespace CVODE
+}   // namespace OpenCOR
+
+//==============================================================================
+// End of file
+//==============================================================================
---
+++ src/plugins/3rdparty/IDA/src/idaplugin.cpp
@@ -0,0 +1,40 @@
+//==============================================================================
+// IDA plugin
+//==============================================================================
+
+#include "idaplugin.h"
+
+//==============================================================================
+
+namespace OpenCOR {
+namespace IDA {
+
+//==============================================================================
+
+PLUGININFO_FUNC IDAPluginInfo()
+{
+    Descriptions descriptions;
+
+    descriptions.insert("en", "A plugin to use <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html#descr_ida\">IDA</a>");
+    descriptions.insert("fr", "Une extension pour utiliser <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html#descr_ida\">IDA</a>");
+
+    return PluginInfo(PluginInfo::V001,
+                      PluginInfo::Gui,
+                      PluginInfo::ThirdParty,
+                      false,
+                      QStringList(),
+                      descriptions);
+}
+
+//==============================================================================
+
+Q_EXPORT_PLUGIN2(IDA, IDAPlugin)
+
+//==============================================================================
+
+}   // namespace IDA
+}   // namespace OpenCOR
+
+//==============================================================================
+// End of file
+//==============================================================================
