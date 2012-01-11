b6c972a092e3ec78f8c8f33a53810de947550caa
Alan Garny <agarny@hellix.com>
2012-01-11 17:23:22 +0100

    Fixed the translation file for the Computer plugin (which got a bit messed up following the renaming from Compiler to Computer).

--- src/plugins/misc/Computer/i18n/Computer_fr.ts
+++ src/plugins/misc/Computer/i18n/Computer_fr.ts
@@ -5,63 +5,63 @@
     <name>OpenCOR::Computer::ComputerEngine</name>
     <message>
         <source>%1 is expected, but &apos;%2&apos; was found instead</source>
-        <translation type="unfinished">%1 est espéré, mais &apos;%2&apos; a été trouvé à la place</translation>
+        <translation>%1 est espéré, mais &apos;%2&apos; a été trouvé à la place</translation>
     </message>
     <message>
         <source>there is already a function called &apos;%1&apos;</source>
-        <translation type="unfinished">il y a déjà une fonction qui s&apos;appelle &apos;%1&apos;</translation>
+        <translation>il y a déjà une fonction qui s&apos;appelle &apos;%1&apos;</translation>
     </message>
     <message>
         <source>either &apos;void&apos; or &apos;double&apos;</source>
-        <translation type="unfinished">soit &apos;void&apos; ou &apos;double&apos;</translation>
+        <translation>soit &apos;void&apos; ou &apos;double&apos;</translation>
     </message>
     <message>
         <source>an identifier</source>
-        <translation type="unfinished">un identifiant</translation>
+        <translation>un identifiant</translation>
     </message>
     <message>
         <source>&apos;(&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>&apos;)&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>&apos;{&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>&apos;}&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>&apos;double&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>&apos;*&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>there is already a parameter called &apos;%1&apos;</source>
-        <translation type="unfinished">il y a déjà un paramètre qui s&apos;appelle &apos;%1&apos;</translation>
+        <translation>il y a déjà un paramètre qui s&apos;appelle &apos;%1&apos;</translation>
     </message>
     <message>
         <source>&apos;return&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>&apos;;&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>the LLVM assembly code could not be parsed: %1</source>
-        <translation type="unfinished">le code assembleur LLVM n&apos;a pas pu être analysé : %1</translation>
+        <translation>le code assembleur LLVM n&apos;a pas pu être analysé : %1</translation>
     </message>
     <message>
         <source>the function &apos;%1&apos; could not be found</source>
-        <translation type="unfinished">La fonction &apos;%1&apos; n&apos;a pas pu être trouvée</translation>
+        <translation>La fonction &apos;%1&apos; n&apos;a pas pu être trouvée</translation>
     </message>
 </context>
 </TS>
