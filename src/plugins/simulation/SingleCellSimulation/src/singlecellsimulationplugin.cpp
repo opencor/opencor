05af2295e8d178fda78322077388b87ccd2caea8
Alan Garny <agarny@hellix.com>
2012-02-03 18:40:17 +0100

    Can now test the van der Pol 1928, Noble 1962 and Zhang 2000 models, and simulation times are comparable to COR (nice!).

--- src/plugins/simulation/SingleCellSimulation/src/singlecellsimulationplugin.cpp
+++ src/plugins/simulation/SingleCellSimulation/src/singlecellsimulationplugin.cpp
@@ -11,6 +11,7 @@
 #include <QFileInfo>
 #include <QMainWindow>
 #include <QPen>
+#include <QTime>
 #include <QVector>
 
 //==============================================================================
@@ -202,12 +203,25 @@ QWidget * SingleCellSimulationPlugin::viewWidget(const QString &pFileName,
             }
         }
 
-        // Compute the model
+        // Compute the model, if supported
 
-        if (   !QFileInfo(pFileName).baseName().compare("van_der_pol_model_1928")
-            || !QFileInfo(pFileName).baseName().compare("zhang_SAN_model_2000_1D_capable")) {
-            bool vanDerPolModel = !QFileInfo(pFileName).baseName().compare("van_der_pol_model_1928");
+        enum Model {
+            VanDerPol1928,
+            Noble1962,
+            Zhang2000,
+            Unknown
+        } model;
 
+        QString fileBaseName = QFileInfo(pFileName).baseName();
+
+        if (!fileBaseName.compare("van_der_pol_model_1928"))
+            model = VanDerPol1928;
+        else if (!fileBaseName.compare("noble_model_1962"))
+            model = Noble1962;
+        else if (!fileBaseName.compare("zhang_SAN_model_2000_1D_capable"))
+            model = Zhang2000;
+
+        if (model != Unknown) {
             typedef QVector<double> Doubles;
 
             int statesCount = cellmlModelRuntime->statesCount();
@@ -219,15 +233,43 @@ QWidget * SingleCellSimulationPlugin::viewWidget(const QString &pFileName,
             typedef void (*RatesFunction)(double, double *, double *, double *, double *);
 
             double voi = 0;   // ms
-            double voiStep = vanDerPolModel?10:0.01;   // ms
-            double voiMax = vanDerPolModel?10000:10;   // ms
+            double voiStep;   // ms
+            double voiMax;    // ms
             double constants[cellmlModelRuntime->constantsCount()];
             double rates[cellmlModelRuntime->ratesCount()];
             double states[statesCount];
+            double algebraic[cellmlModelRuntime->algebraicCount()];
             int voiCount = 0;
-            int voiOutputCount = vanDerPolModel?1:100;
+            int voiOutputCount;   // ms
+            double timeConversion;
+
+            switch (model) {
+            case Noble1962:
+                voiStep        = 0.01;   // ms
+                voiMax         = 1000;   // ms
+                voiOutputCount = 100;
+                timeConversion = 1;      // ms/ms
+
+                break;
+            case Zhang2000:
+                voiStep        = 0.01;    // ms
+                voiMax         = 1000;    // ms
+                voiOutputCount = 100;
+                timeConversion = 0.001;   // ms/s
+
+                break;
+            default:   // van Der Pol 1928
+                voiStep        = 10;      // ms
+                voiMax         = 10000;   // ms
+                voiOutputCount = 1;
+                timeConversion = 0.001;   // ms/s
+            }
 
             CellMLSupport::CellmlModelRuntimeOdeFunctions odeFunctions = cellmlModelRuntime->odeFunctions();
+
+            QTime time;
+
+            time.start();
 
             odeFunctions.initConsts(constants, rates, states);
 
@@ -243,16 +285,16 @@ QWidget * SingleCellSimulationPlugin::viewWidget(const QString &pFileName,
 
                 // Compute the rates
 
-                odeFunctions.rates(voi, constants, rates, states, 0);
+                odeFunctions.rates(voi, constants, rates, states, algebraic);
 
                 // Go to the next voiStep and integrate the states
 
                 voi = ++voiCount*voiStep;
 
                 for (int i = 0; i < statesCount; ++i)
-                    states[i] += 0.001*voiStep*rates[i];
-                    // Note: the scaling factor is because we need to go from
-                    //       seconds to milliseconds...
+                    states[i] += timeConversion*voiStep*rates[i];
+                    // Note: the time conversion is because we may need to go
+                    //       from, say, seconds to milliseconds...
             } while (voi < voiMax);
 
             xData.append(voi);
@@ -260,13 +302,15 @@ QWidget * SingleCellSimulationPlugin::viewWidget(const QString &pFileName,
             for (int i = 0; i < statesCount; ++i)
                 yData[i].append(states[i]);
 
+            qDebug("Simulation time: %s s", qPrintable(QString::number(0.001*time.elapsed(), 'g', 3)));
+
             // Remove any existing curve
 
             resetCurves();
 
             // Add some curves to our plotting area
 
-            for (int i = 0; i < statesCount; ++i) {
+            for (int i = 0, iMax = (model == VanDerPol1928)?statesCount:1; i < iMax; ++i) {
                 QwtPlotCurve *curve = new QwtPlotCurve();
 
                 curve->setRenderHint(QwtPlotItem::RenderAntialiased);
