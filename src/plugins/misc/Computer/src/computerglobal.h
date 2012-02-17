e2a18b6f39d8195fa61d833857e8d0b11d4db945
Alan Garny <agarny@hellix.com>
2012-02-18 09:57:07 +1300

    Minor cleaning up of some of our global plugin header files.

--- src/plugins/misc/Computer/src/computerglobal.h
+++ src/plugins/misc/Computer/src/computerglobal.h
@@ -1,16 +1,12 @@
 #ifndef COMPUTERGLOBAL_H
 #define COMPUTERGLOBAL_H
 
-#ifdef OpenCOR_MAIN
-    #define COMPUTER_EXPORT
-#else
-    #include <QObject>
+#include <QObject>
 
-    #ifdef Computer_PLUGIN
-        #define COMPUTER_EXPORT Q_DECL_EXPORT
-    #else
-        #define COMPUTER_EXPORT Q_DECL_IMPORT
-    #endif
+#ifdef Computer_PLUGIN
+    #define COMPUTER_EXPORT Q_DECL_EXPORT
+#else
+    #define COMPUTER_EXPORT Q_DECL_IMPORT
 #endif
 
 #endif
--- src/plugins/misc/QScintillaSupport/src/qscintillasupportglobal.h
+++ src/plugins/misc/QScintillaSupport/src/qscintillasupportglobal.h
@@ -1,16 +1,12 @@
 #ifndef QSCINTILLASUPPORTGLOBAL_H
 #define QSCINTILLASUPPORTGLOBAL_H
 
-#ifdef OpenCOR_MAIN
-    #define QSCINTILLASUPPORT_EXPORT
-#else
-    #include <QObject>
+#include <QObject>
 
-    #ifdef QScintillaSupport_PLUGIN
-        #define QSCINTILLASUPPORT_EXPORT Q_DECL_EXPORT
-    #else
-        #define QSCINTILLASUPPORT_EXPORT Q_DECL_IMPORT
-    #endif
+#ifdef QScintillaSupport_PLUGIN
+    #define QSCINTILLASUPPORT_EXPORT Q_DECL_EXPORT
+#else
+    #define QSCINTILLASUPPORT_EXPORT Q_DECL_IMPORT
 #endif
 
 #endif
