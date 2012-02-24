9ca235728bd6cb2b5b6c403fafba83c3bed606eb
Alan Garny <agarny@hellix.com>
2012-02-24 14:10:41 +1300

    Removed the Viewer window from the Viewer plugin, but kept the viewer widget itself. Indeed, we probably don't want a Viewer window sitting around all the time if we are in, say, simulation mode. Instead, whatever view needs a viewer should then create an instance of it.

--- src/plugins/editing/Viewer/src/viewerplugin.cpp
+++ src/plugins/editing/Viewer/src/viewerplugin.cpp
@@ -3,11 +3,6 @@
 //==============================================================================
 
 #include "viewerplugin.h"
-#include "viewerwindow.h"
-
-//==============================================================================
-
-#include <QMainWindow>
 
 //==============================================================================
 
@@ -20,8 +15,8 @@ PLUGININFO_FUNC ViewerPluginInfo()
 {
     Descriptions descriptions;
 
-    descriptions.insert("en", "A plugin to graphically visualise various concepts (e.g. mathematical equations)");
-    descriptions.insert("fr", "Une extension pour visualiser graphiquement différents concepts (par exemple des équations mathématiques)");
+    descriptions.insert("en", "A plugin to graphically visualise various modelling concepts (e.g. mathematical equations)");
+    descriptions.insert("fr", "Une extension pour visualiser graphiquement différents concepts de modélisation (par exemple des équations mathématiques)");
 
     return PluginInfo(PluginInfo::V001,
                       PluginInfo::Gui,
@@ -34,56 +29,6 @@ PLUGININFO_FUNC ViewerPluginInfo()
 //==============================================================================
 
 Q_EXPORT_PLUGIN2(Viewer, ViewerPlugin)
-
-//==============================================================================
-
-void ViewerPlugin::initialize()
-{
-    // Create an action to show/hide our viewer window
-
-    mViewerAction = newAction(mMainWindow, true);
-
-    // Create our viewer window
-
-    mViewerWindow = new ViewerWindow(mMainWindow);
-
-    // Set our settings
-
-    mGuiSettings->addWindow(Qt::TopDockWidgetArea, mViewerWindow,
-                            GuiWindowSettings::Editing, mViewerAction);
-}
-
-//==============================================================================
-
-void ViewerPlugin::loadSettings(QSettings *pSettings)
-{
-    // Retrieve our viewer window settings
-
-    loadWindowSettings(pSettings, mViewerWindow);
-}
-
-//==============================================================================
-
-void ViewerPlugin::saveSettings(QSettings *pSettings) const
-{
-    // Keep track of our viewer window settings
-
-    saveWindowSettings(pSettings, mViewerWindow);
-}
-
-//==============================================================================
-
-void ViewerPlugin::retranslateUi()
-{
-    // Retranslate our viewer action
-
-    retranslateAction(mViewerAction, tr("Viewer"),
-                      tr("Show/hide the Viewer window"));
-
-    // Retranslate our viewer window
-
-    mViewerWindow->retranslateUi();
-}
 
 //==============================================================================
 
--- src/plugins/editing/Viewer/src/viewerplugin.h
+++ src/plugins/editing/Viewer/src/viewerplugin.h
@@ -7,9 +7,6 @@
 
 //==============================================================================
 
-#include "coreinterface.h"
-#include "guiinterface.h"
-#include "i18ninterface.h"
 #include "plugininfo.h"
 
 //==============================================================================
@@ -23,31 +20,9 @@ PLUGININFO_FUNC ViewerPluginInfo();
 
 //==============================================================================
 
-class ViewerWindow;
-
-//==============================================================================
-
-class ViewerPlugin : public QObject, public CoreInterface, public GuiInterface,
-                     public I18nInterface
+class ViewerPlugin : public QObject
 {
     Q_OBJECT
-    Q_INTERFACES(OpenCOR::CoreInterface)
-    Q_INTERFACES(OpenCOR::GuiInterface)
-    Q_INTERFACES(OpenCOR::I18nInterface)
-
-public:
-    virtual void initialize();
-
-    virtual void loadSettings(QSettings *pSettings);
-    virtual void saveSettings(QSettings *pSettings) const;
-
-protected:
-    virtual void retranslateUi();
-
-private:
-    QAction *mViewerAction;
-
-    ViewerWindow *mViewerWindow;
 };
 
 //==============================================================================
