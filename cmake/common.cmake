c19f59551451c9103b07aef2272e17704686c909
Alan Garny <agarny@hellix.com>
2012-06-22 16:58:05 +0200

    Some more cleaning up of common.cmake for Mac OS X.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -456,7 +456,7 @@ MACRO(ADD_PLUGIN PLUGIN_NAME)
             STRING(REPLACE "${PLUGIN_BUILD_DIR}/" "" OPENCOR_BINARY_DEPENDENCY "${OPENCOR_BINARY_DEPENDENCY}")
 
             ADD_CUSTOM_COMMAND(TARGET ${PROJECT_NAME} POST_BUILD
-                               COMMAND install_name_tool -change ${PLUGIN_BUILD_DIR}/${OPENCOR_BINARY_DEPENDENCY}
+                               COMMAND install_name_tool -change ${OPENCOR_BINARY_DEPENDENCY}
                                                                  @executable_path/../PlugIns/${MAIN_PROJECT_NAME}/${OPENCOR_BINARY_DEPENDENCY}
                                                                  ${MAC_OS_X_PROJECT_BINARY_DIR}/Contents/PlugIns/${MAIN_PROJECT_NAME}/${PLUGIN_FILENAME})
         ENDFOREACH()
@@ -718,22 +718,6 @@ MACRO(ADD_PLUGIN_BINARY PLUGIN_NAME)
     ADD_CUSTOM_TARGET(${MAIN_PROJECT_NAME}_${PLUGIN_NAME}_COPY_PLUGIN_TO_BUILD_DIRECTORY ALL
                       COMMAND ${CMAKE_COMMAND} -E copy ${PLUGIN_BINARY_DIR}/${PLUGIN_FILENAME}
                                                        ${CMAKE_BINARY_DIR}/${PLUGIN_FILENAME})
-
-    # A few Mac OS X specific things
-
-    IF(APPLE)
-        # Make sure that the plugin refers to the system version of the Qt
-        # libraries on which it depends
-        # Note: indeed, the copy of the binary plugin that we have refers to
-        #       the embedded version of the Qt libraries, so...
-
-        FOREACH(QT_DEPENDENCY ${QT_DEPENDENCIES})
-            ADD_CUSTOM_TARGET(${MAIN_PROJECT_NAME}_${PLUGIN_NAME}_UPDATE_MAC_OS_X_QT_REFERENCE ALL
-                               COMMAND install_name_tool -change @executable_path/../Frameworks/${QT_DEPENDENCY}.framework/Versions/${QT_VERSION_MAJOR}/${QT_DEPENDENCY}
-                                                                 ${QT_LIBRARY_DIR}/${QT_DEPENDENCY}.framework/Versions/${QT_VERSION_MAJOR}/${QT_DEPENDENCY}
-                                                                 ${LIBRARY_OUTPUT_PATH}/${CMAKE_SHARED_LIBRARY_PREFIX}${PLUGIN_NAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
-        ENDFOREACH()
-    ENDIF()
 
     # Package the plugin itself
 
