6979c1b4385282b75f9d6bf41841941c11b37f44
Alan Garny <agarny@hellix.com>
2012-08-15 15:29:10 +0200

    Some work on the documentation. Started using some JavaScript. Also made sure everything is HTML5 compliant.

---
+++ doc/res/common.js
@@ -0,0 +1,14 @@
+function copyright() {
+    var date = new Date();
+
+    document.write("Copyright ©2011-"+date.getFullYear());
+}
+
+// Note: for some very weird reasons, any file which is to be used to generate
+//       a Qt help must be at least 512 bytes big, so...
+
+// ############################################################################
+// ###                                                                     ####
+// ###   Just to make up for the fact that we need at least 512 bytes...   ####
+// ###                                                                     ####
+// ############################################################################
