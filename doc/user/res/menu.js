9948fa23b3b1a9ef4d222f3b04b6773fa9ab058c
Alan Garny <agarny@hellix.com>
2012-09-06 19:04:05 +0200

    Minor fix to the menu in the user documentation.

--- doc/user/res/menu.js
+++ doc/user/res/menu.js
@@ -2,8 +2,8 @@ function headerAndContentsMenu(pageName, relativePath) {
     doHeaderAndContentsMenu(pageName, relativePath, 54, 96, 146,
                             new Array(new Array("index.html", "Home", 0),
                                       new Array("knownIssuesLimitations.html", "Known issues/limitations", 0),
-                                      new Array("licensing.html", "Licensing", 1),
-                                      new Array("dissemination.html", "Dissemination", 1),
-                                      new Array("authors.html", "Authors", 1),
+                                      new Array("licensing.html", "Licensing", 0),
+                                      new Array("dissemination.html", "Dissemination", 0),
+                                      new Array("authors.html", "Authors", 0),
                                       new Array("contactUs.html", "Contact us", 0)));
 }
