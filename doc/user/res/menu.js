7d93cb08d76ed465fed855e3c53f0684394e0e0c
Alan Garny <agarny@hellix.com>
2012-09-07 17:22:59 +0200

    Reworked/updated both the user and developer documentations. Also started work on supporting the help to be dynamic (#105).

--- doc/user/res/menu.js
+++ doc/user/res/menu.js
@@ -2,9 +2,12 @@ function headerAndContentsMenu(pageName, relativePath) {
     doHeaderAndContentsMenu(pageName, relativePath, 54, 96, 146,
                             new Array(new Array(0, "Home", "index.html"),
                                       new Array(),
+                                      new Array(0, "Prerequisites", "prerequisites.html"),
                                       new Array(0, "Known issues/limitations", "knownIssuesLimitations.html"),
+                                      new Array(),
                                       new Array(0, "Licensing", "licensing.html"),
                                       new Array(0, "Dissemination", "dissemination.html"),
                                       new Array(0, "Authors", "authors.html"),
+                                      new Array(),
                                       new Array(0, "Contact us", "contactUs.html")));
 }
