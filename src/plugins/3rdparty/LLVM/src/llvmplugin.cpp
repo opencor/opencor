8672c8626376908f4f6295811e30fd3acefb813b
Alan Garny <agarny@hellix.com>
2012-09-24 12:50:10 +0200

    Some work on our documentation.

--- src/plugins/3rdparty/LLVM/src/llvmplugin.cpp
+++ src/plugins/3rdparty/LLVM/src/llvmplugin.cpp
@@ -15,8 +15,8 @@ PLUGININFO_FUNC LLVMPluginInfo()
 {
     Descriptions descriptions;
 
-    descriptions.insert("en", "A plugin to access <a href=\"http://www.llvm.org/\">LLVM</a> (including <a href=\"http://clang.llvm.org/\">Clang</a>)");
-    descriptions.insert("fr", "Une extension pour accéder à <a href=\"http://www.llvm.org/\">LLVM</a> (y compris <a href=\"http://clang.llvm.org/\">Clang</a>)");
+    descriptions.insert("en", "A plugin to access <a href=\"http://www.llvm.org/\">LLVM</a> (as well as <a href=\"http://clang.llvm.org/\">Clang</a>)");
+    descriptions.insert("fr", "Une extension pour accéder à <a href=\"http://www.llvm.org/\">LLVM</a> (ainsi que <a href=\"http://clang.llvm.org/\">Clang</a>)");
 
     return new PluginInfo(PluginInfo::V001,
                           PluginInfo::General,
