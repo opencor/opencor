c48a8298ea603d28ab40567a30795a7a3eb6a6db
Alan Garny <agarny@hellix.com>
2012-11-14 14:57:20 +0100

    Fixed (as much as possible) an issue with our Plugins window on OS X.
    The issue was that some dependencies and/or descriptions were not fully
    visible. Now, things are better, but there are still some cases where
    things look a bit funny.

--- src/pluginswindow.cpp
+++ src/pluginswindow.cpp
@@ -74,6 +74,15 @@ PluginsWindow::PluginsWindow(PluginManager *pPluginManager, QWidget *pParent) :
 
     mGui->setupUi(this);
 
+    // Make sure that all the widgets in our details form layout can be resized
+    // if necessary and if possible
+    // Note: indeed, it's not the case on OS X since the field growth policy is
+    //       set to FieldsStayAtSizeHint on that platform and also on Windows
+    //       and Linux to make sure that, if anything, we get the same behaviour
+    //       on all the platforms we support...
+
+    mGui->detailsFormLayout->setFieldGrowthPolicy(QFormLayout::AllNonFixedFieldsGrow);
+
     // Update the note label
 
     mGui->noteLabel->setText(mGui->noteLabel->text().arg(qApp->applicationName()));
