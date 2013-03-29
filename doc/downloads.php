0b05ddda594c0168443e666c60c44bdb071cab23
Alan Garny <agarny@hellix.com>
2013-03-30 12:15:50 +1300

    Improved our downloads page.

--- doc/downloads.php
+++ doc/downloads.php
@@ -1,21 +1,4 @@
 <!DOCTYPE html>
-<?php
-    // Retrieve the formatted size of a file
-
-    function formattedFileSize($fileName) {
-        $filePath = $_SERVER["DOCUMENT_ROOT"]."/".$fileName;
-
-        if (file_exists($filePath)) {
-            $units = array("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
-            $fileSize = filesize($filePath);
-            $i = floor(log($fileSize, 1024));
-
-            return ceil($fileSize/pow(1024, $i))." ".$units[$i];
-        } else {
-            return "???";
-        }
-    }
-?>
 <html>
     <head>
         <title>
@@ -40,26 +23,67 @@
         </p>
 
 <?php
-    date_default_timezone_set("Europe/Paris");
+    // Function to retrieve the formatted size of a file
 
-    $downloads = array(
-                     array("Latest snapshot", 21, 3, 2013, "2013-03-21", false,
-                           array(array("Windows", ".exe"), array("Windows", ".zip"),
-                                 array("Linux", ".tar.gz", 32), array("Linux", ".tar.gz", 64),
-                                 array("OSX", ".dmg"), array("OSX", ".zip"))),
-                 );
+    function formattedFileSize($fileName) {
+        $filePath = $_SERVER["DOCUMENT_ROOT"]."/".$fileName;
 
-    foreach ($downloads as $download) {
+        if (file_exists($filePath)) {
+            $units = array("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
+            $fileSize = filesize($filePath);
+            $i = floor(log($fileSize, 1024));
+
+            return ceil($fileSize/pow(1024, $i))." ".$units[$i];
+        } else {
+            return "???";
+        }
+    }
+
+    // Various initialisations
+
+    date_default_timezone_set("Europe/London");
+
+    $versions = array(array("Latest snapshot", 21, 3, 2013, "2013-03-21", false,
+                            array(array("Windows", array(".exe"), array(".zip")),
+                                  array("Linux", array(".tar.gz", 32), array(".tar.gz", 64)),
+                                  array("OS X", array(".dmg"), array(".zip")))));
+
+    // Output our various versions
+
+    $versionIndex = 0;
+
+    while ($versionIndex != sizeof($versions)) {
+        $version = $versions[$versionIndex];
+
+        // Retrieve some information about the version
+
+        $versionTitle = $version[0];
+        $versionDay = $version[1];
+        $versionMonth = $version[2];
+        $versionYear = $version[3];
+        $versionVersion = $version[4];
+        $versionRecommended = $version[5];
+        $versionFiles = $version[6];
+
+        // Output an empty line to separate the different versions, if needed
+
+        if ($versionIndex) {
+?>
+
+<?php
+        }
+
+        // Output some general information about the version
 ?>
         <div class="section">
             <table>
                 <tbody>
                     <tr>
                         <td>
-                            <?php echo $download[0]."\n"; ?>
+                            <?php echo $versionTitle."\n"; ?>
                         </td>
                         <td class="date">
-                            (<?php echo date("j F Y", mktime(0, 0, 0, $download[2], $download[1], $download[3])); ?>)
+                            (<?php echo date("j F Y", mktime(0, 0, 0, $versionMonth, $versionDay, $versionYear)); ?>)
                         </td>
                     </tr>
                 </tbody>
@@ -67,106 +91,162 @@
         </div>
 
 <?php
-        if ($download[5]) {
+        // Output some information about the version files
+
+        $versionClasses = "version";
+
+        if ($versionRecommended)
+            $versionClasses .= " recommended";
 ?>
-        <table class="downloads recommended">
-<?php
-        } else {
-?>
-        <table class="downloads">
-<?php
-        }
-?>
+        <table class="<?php echo $versionClasses; ?>">
             <tbody>
                 <tr>
 <?php
-        $platform = "";
+        // Show n platforms per row
+        // Note: for aesthetical reasons (i.e. having top/left separators all
+        //       the way through), we have a total of m*n platforms...
 
-        foreach ($download[6] as $file) {
-            if ($file[0] != $platform) {
-                // We are dealing with a file for a new platform, so check
-                // whether we need to 'close' the previous platform (and add a
-                // separator) before opening the new one
+        $nbOfPlatformsPerRow = 3;
+        $nbOfPlatforms = sizeof($versionFiles);
 
-                if ($platform != "") {
-                    // There is a previous platform, so close it and add a
-                    // separator
+        while ($nbOfPlatforms % $nbOfPlatformsPerRow)
+            ++$nbOfPlatforms;
+
+        // Percentage of the total width to use for each platform
+
+        $platformWidth = 100/$nbOfPlatformsPerRow."%";
+
+        // Go through our different platforms
+
+        $platformIndex = 0;
+
+        while ($platformIndex != $nbOfPlatforms) {
+            $platformLocalIndex = $platformIndex % $nbOfPlatformsPerRow;
+
+            // 'Close' the current platform if there is a previous platform
+
+            if ($platformIndex) {
+                if (sizeof($platformFiles)) {
 ?>
                                         </ul>
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
-                    </td>
-                    <td class="separator" />
 <?php
                 }
-
-                // Keep track of the new platform
-
-                $platform = $file[0];
-
-                // Open the new platform
 ?>
-                    <td class="platform">
+                    </td>
+<?php
+            }
+
+            // End the current row of platforms and start a new one if we have
+            // just 'closed' the current platform after having listed a row of
+            // platforms
+
+            if ($platformIndex && !$platformLocalIndex) {
+?>
+                </tr>
+                <tr>
+<?php
+            }
+
+            // 'Open' the new platform
+
+            $platformClasses = "platform";
+
+            if ($platformIndex >= $nbOfPlatformsPerRow)
+                $platformClasses .= " topSeparator";
+
+            if ($platformLocalIndex)
+                $platformClasses .= " leftSeparator";
+?>
+                    <td class="<?php echo $platformClasses; ?>" style="width: <?php echo $platformWidth; ?>">
+<?php
+            $platformFiles = $versionFiles[$platformIndex];
+
+            if (sizeof($platformFiles)) {
+                $platformName = $platformFiles[0];
+?>
                         <table>
                             <tbody>
                                 <tr>
                                     <td class="logo">
-                                        <img src="res/pics/<?php echo strtolower($platform); ?>.png" width=72 height=72 alt="<?php echo $platform; ?>">
+                                        <img src="res/pics/<?php echo str_replace(" ", "", strtolower($platformName)); ?>.png" width=72 height=72 alt="<?php echo $platformName; ?>">
                                     </td>
                                     <td>
                                         <div>
-                                            <?php echo $platform."\n"; ?>
+                                            <?php echo $platformName."\n"; ?>
                                         </div>
 
                                         <ul>
 <?php
+                // List the platform files
+
+                $platformFileIndex = 1;
+
+                while ($platformFileIndex != sizeof($platformFiles)) {
+                    $platformFile = $platformFiles[$platformFileIndex];
+
+                    // Retrieve some information about the platform file
+
+                    $platformFileExtension = $platformFile[0];
+                    $platformFileExtraInfo = $platformFile[1];
+
+                    // Determine the file name, type and extra info, if any
+
+                    $platformFileName = "downloads/OpenCOR-".$versionVersion."-".str_replace(" ", "", $platformName).$platformFileExtraInfo.$platformFileExtension;
+
+                    if (   ($platformFileExtension == ".exe")
+                        || ($platformFileExtension == ".dmg"))
+                        $platformFileType = "Installer";
+                    else if ($platformFileExtension == ".zip")
+                        $platformFileType = "ZIP file";
+                    else if ($platformFileExtension == ".tar.gz")
+                        $platformFileType = "Tarball file";
+                    else
+                        $platformFileType = "???";
+
+                    if (isset($platformFileExtraInfo))
+                        $platformFileExtraInfo = " (".$platformFileExtraInfo."-bit)";
+                    else
+                        $platformFileExtraInfo = "";
+
+                    // List the file for download
+?>
+                                            <li><a href="http://www.opencor.ws/<?php echo $platformFileName; ?>"><?php echo $platformFileType; ?></a><?php echo $platformFileExtraInfo; ?> <span class="fileSize">(<?php echo formattedFileSize($platformFileName); ?>)</span></li>
+<?php
+                    // Go to the next platform file
+
+                    ++$platformFileIndex;
+                }
             }
 
-            // Determine the file name
+            // Go to the next platform
 
-            $fileName = "downloads/OpenCOR-".$download[4]."-".$file[0];
-
-            if (isset($file[2]))
-                $fileName .= $file[2];
-
-            $fileName .= $file[1];
-
-            // Determine the file type
-
-            $fileType = "???";
-
-            if (($file[1] == ".exe") || ($file[1] == ".dmg"))
-                $fileType = "Installer";
-            else if ($file[1] == ".zip")
-                $fileType = "ZIP file";
-            else if ($file[1] == ".tar.gz")
-                $fileType = "Tarball file";
-
-            // Determine the file extra info, if any
-
-            $fileExtraInfo = "";
-
-            if (isset($file[2]))
-                $fileExtraInfo = " (".$file[2]."-bit)";
-
-            // List the file for download
-?>
-                                            <li><a href="http://www.opencor.ws/<?php echo $fileName; ?>"><?php echo $fileType; ?></a><?php echo $fileExtraInfo; ?> <span class="fileSize">(<?php echo formattedFileSize($fileName); ?>)</span></li>
-<?php
+            ++$platformIndex;
         }
+
+        // 'Close' the last platform
+
+        if (sizeof($platformFiles)) {
 ?>
                                         </ul>
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
+<?php
+        }
+?>
                     </td>
                 </tr>
             </tbody>
         </table>
 <?php
+        // Go to the next version
+
+        ++$versionIndex;
     }
 ?>
 
