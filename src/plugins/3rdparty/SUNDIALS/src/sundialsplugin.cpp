20527e445ad69d531a8312b1860cb96ac814537d
Alan Garny <agarny@hellix.com>
2013-04-01 15:43:49 +1300

    Slight update to our SUNDIALS plugin.

--- src/plugins/3rdparty/SUNDIALS/src/sundialsplugin.cpp
+++ src/plugins/3rdparty/SUNDIALS/src/sundialsplugin.cpp
@@ -15,8 +15,8 @@ PLUGININFO_FUNC SUNDIALSPluginInfo()
 {
     Descriptions descriptions;
 
-    descriptions.insert("en", QString::fromUtf8("A plugin to access CVODE and IDA from the <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html\">SUNDIALS</a> library"));
-    descriptions.insert("fr", QString::fromUtf8("Une extension pour accéder à CVODE et à IDA de la librairie <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html\">SUNDIALS</a>"));
+    descriptions.insert("en", QString::fromUtf8("A plugin to access CVODE, IDA and KINSOL from the <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html\">SUNDIALS</a> library"));
+    descriptions.insert("fr", QString::fromUtf8("Une extension pour accéder à CVODE, IDA et KINSOL de la librairie <a href=\"https://computation.llnl.gov/casc/sundials/description/description.html\">SUNDIALS</a>"));
 
     return new PluginInfo(PluginInfo::InterfaceVersion001,
                           PluginInfo::General,
