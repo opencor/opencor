6528ac334039223d7a78f0a59b8315412a0575d4
Alan Garny <agarny@hellix.com>
2013-04-17 17:06:45 -0500

    Some minor editing.

--- src/plugins/misc/CellMLSupport/src/cellmlsupportplugin.cpp
+++ src/plugins/misc/CellMLSupport/src/cellmlsupportplugin.cpp
@@ -106,8 +106,8 @@ bool isCellmlFile(const QString &pFileName)
             // or 1.1 namespace
 
             if (   !xml.name().toString().compare("model")
-                && (   (!xml.namespaceUri().toString().compare("http://www.cellml.org/cellml/1.0#"))
-                    || (!xml.namespaceUri().toString().compare("http://www.cellml.org/cellml/1.1#"))))
+                &&  (   (!xml.namespaceUri().toString().compare("http://www.cellml.org/cellml/1.0#"))
+                     || (!xml.namespaceUri().toString().compare("http://www.cellml.org/cellml/1.1#"))))
                 // All the requirements are gathered for the file to be
                 // considered a CellML file, so...
 
