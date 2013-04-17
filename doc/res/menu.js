b35d4c5312478feefc38e43f4b8a468e911f917b
Alan Garny <agarny@hellix.com>
2013-04-17 15:26:56 -0500

    Minor editing of our main documentation's menu.

--- doc/res/menu.js
+++ doc/res/menu.js
@@ -5,7 +5,7 @@ function headerAndContentsMenu(pageName, relativePath) {
                              [0, "User documentation", "user/index.html"],
                              [0, "Developer documentation", "developer/index.html"],
                              [],
-                             [0, "Downloads", "downloads.html"],
+                             [0, "Download", "download.html"],
                              [],
                              [0, "Contact us", "user/contactUs.html"]]);
 }
