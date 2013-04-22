3318acfb040b9f9a7cd0547354413a100af65dd9
Alan Garny <agarny@hellix.com>
2013-04-22 11:39:04 -0500

    Reverted what I thought was some 'safe' cleaning up.

--- src/plugins/misc/Core/src/treeviewwidget.cpp
+++ src/plugins/misc/Core/src/treeviewwidget.cpp
@@ -187,13 +187,16 @@ void TreeViewWidget::mouseDoubleClickEvent(QMouseEvent *pEvent)
 {
     // Retrieve the index of the item which row is the same as our current item,
     // but which column is 0, so that we can expand/collapse properly
+    // Note: to retrieve the current index, we would normally use
+    //       currentIndex(), but we may not be double clicking on an item, so...
 
-    QModelIndex currIndex = currentIndex();
+    QModelIndex currIndex = indexAt(pEvent->pos());
+    QModelIndex index = model()->index(currIndex.row(), 0, currIndex.parent());
 
-    if (model()->hasChildren(currIndex)) {
+    if (model()->hasChildren(index)) {
         // The item has some children, so expand/collapse it
 
-        setExpanded(currIndex, !isExpanded(currIndex));
+        setExpanded(index, !isExpanded(index));
 
         // Accept the event
 
