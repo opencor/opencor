b4a84fac6222178e5cc2cf7b0d3f747871912d22
Alan Garny <agarny@hellix.com>
2013-09-20 10:12:25 +0200

    Made sure that make.bat generates a release version of OpenCOR.

--- make.bat
+++ make.bat
@@ -30,7 +30,7 @@ ECHO.
 
 CD build
 
-cmake -G "NMake Makefiles JOM" ..
+cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ..
 
 SET ExitCode=%ERRORLEVEL%
 
@@ -62,7 +62,7 @@ ECHO.
 
 CD ..\windows\build
 
-cmake -G "NMake Makefiles JOM" ..
+cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ..
 
 SET ExitCode=%ERRORLEVEL%
 
