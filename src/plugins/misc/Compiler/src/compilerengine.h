009f93646f2d896e229033ebdcd719d6c785dcf8
Alan Garny <agarny@hellix.com>
2013-09-28 09:26:58 +0200

    Some minor cleaning up.

--- src/plugins/misc/Compiler/src/compilerengine.h
+++ src/plugins/misc/Compiler/src/compilerengine.h
@@ -29,6 +29,7 @@ specific language governing permissions and limitations under the License.
 //==============================================================================
 
 #include <QObject>
+#include <QString>
 
 //==============================================================================
 
