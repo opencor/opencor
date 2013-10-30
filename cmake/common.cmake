810fafe2f8515659fc941439873ff701f5fd0aa6
Alan Garny <agarny@hellix.com>
2013-10-30 10:49:57 +0100

    Allow offscreen execution only if the tests are built on Travis CI.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -582,6 +582,10 @@ MACRO(ADD_PLUGIN PLUGIN_NAME)
                     ${TEST_HEADER_MOC_FILE}
                 )
 
+                IF(ENABLE_TRAVIS_CI)
+                    ADD_DEFINITIONS(-DTRAVIS_CI)
+                ENDIF()
+
                 ADD_EXECUTABLE(${TEST_NAME}
                     ../../../../tests/testsutils.cpp
 
