f5f310a4c1986132ed22ed9347c3faedb399b2bc
Alan Garny <agarny@hellix.com>
2013-10-31 18:33:22 +0100

    New snapshots.

--- doc/downloads/index.php
+++ doc/downloads/index.php
@@ -47,7 +47,7 @@
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "OS X 10.8 (Mountain Lion) and later", array(".dmg"), array(".zip")))),
-                      array(0, 0, 0, 15, 10, 2013, 2,
+                      array(0, 0, 0, 31, 10, 2013, 2,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.6 (Snow Leopard) and later", array(".dmg"), array(".zip")))),
