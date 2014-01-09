85a7fc30cf19be10ea34c7832bdfb66c0474c4f3
Alan Garny <agarny@hellix.com>
2014-01-09 17:14:56 +0100

    Had forgotten to unignore some LLVM files. Also some work on LLVM on Windows [ci skip].

--- src/plugins/3rdparty/LLVM/lib/Support/Windows/Windows.h
+++ src/plugins/3rdparty/LLVM/lib/Support/Windows/Windows.h
@@ -24,6 +24,9 @@
 #define _WIN32_IE    0x0600 // MinGW at it again.
 #define WIN32_LEAN_AND_MEAN
 
+//---OPENCOR--- BEGIN
+#undef UNICODE
+//---OPENCOR--- END
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Config/config.h" // Get build system configuration settings
