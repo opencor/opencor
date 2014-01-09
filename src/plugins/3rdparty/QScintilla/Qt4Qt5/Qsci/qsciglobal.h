d35a5364143318a8e29e68493fa2d3da6b935292
Alan Garny <agarny@hellix.com>
2014-01-09 09:48:34 +0100

    Upgraded QScintilla to the latest changeset.

--- src/plugins/3rdparty/QScintilla/Qt4Qt5/Qsci/qsciglobal.h
+++ src/plugins/3rdparty/QScintilla/Qt4Qt5/Qsci/qsciglobal.h
@@ -34,7 +34,7 @@ extern "C++" {
 
 
 #define QSCINTILLA_VERSION      0x020801
-#define QSCINTILLA_VERSION_STR  "2.8.1-snapshot-8d23447dbd4d"
+#define QSCINTILLA_VERSION_STR  "2.8.1-snapshot-3be74b3e89e9"
 
 
 // Under Windows, define QSCINTILLA_MAKE_DLL to create a Scintilla DLL, or
