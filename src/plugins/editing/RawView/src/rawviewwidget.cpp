a429eb24dedbd84fe0fd8c9c1c3f8abed3cf9003
Alan Garny <agarny@hellix.com>
2014-02-20 18:17:50 +0100

    Some minor cleaning up [ci skip].

--- src/plugins/editing/RawView/src/rawviewwidget.cpp
+++ src/plugins/editing/RawView/src/rawviewwidget.cpp
@@ -21,6 +21,7 @@ specific language governing permissions and limitations under the License.
 
 #include "cliutils.h"
 #include "filemanager.h"
+#include "qscintillawidget.h"
 #include "rawviewwidget.h"
 
 //==============================================================================
