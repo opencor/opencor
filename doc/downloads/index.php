0dcf10fb423c89c59234db9cd39f42792101bf9a
Alan Garny <agarny@hellix.com>
2014-03-15 11:59:00 +0100

    New snapshots.

--- doc/downloads/index.php
+++ doc/downloads/index.php
@@ -39,15 +39,15 @@
 
     date_default_timezone_set("Europe/London");
 
-    $versions = array(array(0, 2, 0, 19, 11, 2013, 1,
+    $versions = array(array(0, 0, 0, 15, 3, 2014, 2,
+                            array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
+                                  array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
+                                  array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip"))),
+                            "<strong>Note:</strong> this version is fairly stable and therefore the one we would recommend using, especially if you want to get access to the latest features (new snapshots are announced on our <a hrefs=\"https://twitter.com/TeamOpenCOR/\">Twitter feed</a>)."),
+                      array(0, 2, 0, 19, 11, 2013, 1,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip")))),
-                      array(0, 0, 0, 14, 3, 2014, 2,
-                            array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
-                                  array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
-                                  array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip")))/*,
-                            "<strong>Note:</strong> this version is fairly stable and therefore the one we would recommend using, especially if you want to get access to the latest features (new snapshots are announced on our <a hrefs=\"https://twitter.com/TeamOpenCOR/\">Twitter feed</a>)"*/),
                       array(0, 1, 2, 29, 5, 2013, 0,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
