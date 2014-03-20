db32b6adbf947fb11a9573fd418114c104cf00e3
Alan Garny <agarny@hellix.com>
2014-03-20 05:00:32 +0100

    Some minor cleaning up [ci skip].

--- windows/src/main.cpp
+++ windows/src/main.cpp
@@ -33,6 +33,7 @@ specific language governing permissions and limitations under the License.
 #include <QFileInfo>
 #include <QPointer>
 #include <QProcess>
+#include <QSettings>
 
 //==============================================================================
 
