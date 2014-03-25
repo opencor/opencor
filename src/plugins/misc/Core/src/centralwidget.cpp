6f6d28a6d04b95dd8801f6ed1a26f8d83978323c
Alan Garny <agarny@hellix.com>
2014-03-25 18:48:47 +1300

    Prevent our tab bars (in our central widget) from getting the focus.
    Indeed, in some cases, when pressing the Tab key a tab bar may get the
    focus while we really don’t want that (it doesn’t look ‘neat’,
    especially on OS X).

--- src/plugins/misc/Core/src/centralwidget.cpp
+++ src/plugins/misc/Core/src/centralwidget.cpp
@@ -33,6 +33,10 @@ specific language governing permissions and limitations under the License.
 
 //==============================================================================
 
+#include <Qt>
+
+//==============================================================================
+
 #include <QDragEnterEvent>
 #include <QFile>
 #include <QFileInfo>
@@ -1393,17 +1397,18 @@ QTabBar * CentralWidget::newTabBar(const QTabBar::Shape &pShape,
     QTabBar *res = new QTabBar(this);
 
     res->setExpanding(false);
+    // Note: if the above property is not enabled and many files are opened,
+    //       then the central widget will widen reducing the width of any docked
+    //       window which is clearly not what we want, so...
+    res->setFocusPolicy(Qt::NoFocus);
     res->setMovable(pMovable);
     res->setShape(pShape);
     res->setTabsClosable(pTabsClosable);
     res->setUsesScrollButtons(true);
-    // Note #1: the above property is style dependent and it happens that it's
-    //          not enabled on OS X, so... set it in all cases, even though it's
-    //          already set on Windows and Linux, but one can never know what
-    //          the future holds, so...
-    // Note #2: if the above property is not enabled and many files are opened,
-    //          then the central widget will widen reducing the width of any
-    //          docked window which is clearly not what we want, so...
+    // Note: the above property is style dependent and it happens that it's not
+    //       enabled on OS X, so... set it in all cases, even though it's
+    //       already set on Windows and Linux, but one can never know what the
+    //       future holds, so...
 
     return res;
 }
