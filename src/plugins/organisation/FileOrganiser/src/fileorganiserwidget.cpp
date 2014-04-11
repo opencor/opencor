opencor/opencor

commit f4682b158504b2c61eca1acfc68a09f2d2505465

Minor improvements following an Xcode anaylsis.

src/plugins/organisation/FileOrganiser/src/fileorganiserwidget.cpp
@@ -103,6 +103,11 @@ static const QString SettingsSelectedItem = "SelectedItem";
 void FileOrganiserWidget::loadItemSettings(QSettings *pSettings,
                                            QStandardItem *pParentItem)
 {
+    // Make sure that we have a parent item
+
+    if (!pParentItem)
+        return;
+
     // Recursively retrieve the item settings
 
     static int crtItemIndex = -1;

src/plugins/editing/CellMLAnnotationView/src/cellmlannotationviewplugin.cpp
@@ -339,23 +339,21 @@ QWidget * CellMLAnnotationViewPlugin::viewWidget(const QString &pFileName)
 
 //==============================================================================
 
-QWidget * CellMLAnnotationViewPlugin::removeViewWidget(const QString &pFileName)
+void CellMLAnnotationViewPlugin::removeViewWidget(const QString &pFileName)
 {
     // Remove the view widget from our list, should there be one for the given
     // file name
 
-    CellmlAnnotationViewWidget *res = mViewWidgets.value(pFileName);
+    CellmlAnnotationViewWidget *viewWidget = mViewWidgets.value(pFileName);
 
-    if (res) {
+    if (viewWidget) {
         // There is a view widget for the given file name, so delete it and
         // remove it from our list
 
-        delete res;
+        delete viewWidget;
 
         mViewWidgets.remove(pFileName);
     }
-
-    return res;
 }
 
 //==============================================================================

src/plugins/editing/CoreCellMLEditing/src/corecellmleditingplugin.cpp
@@ -188,13 +188,11 @@ QWidget * CoreCellMLEditingPlugin::viewWidget(const QString &pFileName)
 
 //==============================================================================
 
-QWidget * CoreCellMLEditingPlugin::removeViewWidget(const QString &pFileName)
+void CoreCellMLEditingPlugin::removeViewWidget(const QString &pFileName)
 {
     Q_UNUSED(pFileName);
 
     // We don't handle this interface...
-
-    return 0;
 }
 
 //==============================================================================

src/plugins/editing/CoreEditing/src/coreeditingplugin.cpp
@@ -235,13 +235,11 @@ QWidget * CoreEditingPlugin::viewWidget(const QString &pFileName)
 
 //==============================================================================
 
-QWidget * CoreEditingPlugin::removeViewWidget(const QString &pFileName)
+void CoreEditingPlugin::removeViewWidget(const QString &pFileName)
 {
     Q_UNUSED(pFileName);
 
     // We don't handle this interface...
-
-    return 0;
 }
 
 //==============================================================================

src/plugins/editing/RawCellMLView/src/rawcellmlviewplugin.cpp
@@ -211,16 +211,11 @@ QWidget * RawCellMLViewPlugin::viewWidget(const QString &pFileName)
 
 //==============================================================================
 
-QWidget * RawCellMLViewPlugin::removeViewWidget(const QString &pFileName)
+void RawCellMLViewPlugin::removeViewWidget(const QString &pFileName)
 {
     // Ask our generic view widget to finalise the given file
 
     mViewWidget->finalize(pFileName);
-
-    // We don't want to give people the address of the widget that we removed
-    // since it would have to be mViewWidget and we want to be able to reuse it
-
-    return 0;
 }
 
 //==============================================================================

src/plugins/editing/RawView/src/rawviewplugin.cpp
@@ -128,23 +128,21 @@ QWidget * RawViewPlugin::viewWidget(const QString &pFileName)
 
 //==============================================================================
 
-QWidget * RawViewPlugin::removeViewWidget(const QString &pFileName)
+void RawViewPlugin::removeViewWidget(const QString &pFileName)
 {
     // Remove the view widget from our list, should there be one for the given
     // file name
 
-    RawViewWidget *res = mViewWidgets.value(pFileName);
+    RawViewWidget *viewWidget = mViewWidgets.value(pFileName);
 
-    if (res) {
+    if (viewWidget) {
         // There is a view widget for the given file name, so delete it and
         // remove it from our list
 
-        delete res;
+        delete viewWidget;
 
         mViewWidgets.remove(pFileName);
     }
-
-    return res;
 }
 
 //==============================================================================

src/plugins/guiinterface.inl
@@ -12,7 +12,7 @@
     virtual void finalizeView() PURE;
 
     virtual QWidget * viewWidget(const QString &pFileName) PURE;
-    virtual QWidget * removeViewWidget(const QString &pFileName) PURE;
+    virtual void removeViewWidget(const QString &pFileName) PURE;
     virtual QString viewName() const PURE;
 
     virtual QIcon fileTabIcon(const QString &pFileName) const PURE;

src/plugins/misc/CellMLTools/src/cellmltoolsplugin.cpp
@@ -259,13 +259,11 @@ QWidget * CellMLToolsPlugin::viewWidget(const QString &pFileName)
 
 //==============================================================================
 
-QWidget * CellMLToolsPlugin::removeViewWidget(const QString &pFileName)
+void CellMLToolsPlugin::removeViewWidget(const QString &pFileName)
 {
     Q_UNUSED(pFileName);
 
     // We don't handle this interface...
-
-    return 0;
 }
 
 //==============================================================================

commit 6f7a4bffb62a1ac227e8addf8a277a50bd3dde86

Fixed the issue with folders and files not getting remembered from one session to another anymore in our File Organiser window (closes #329).
This was caused by commit f4682b158504b2c61eca1acfc68a09f2d2505465,
which was the result of some Xcode analysis (!?).

src/plugins/organisation/FileOrganiser/src/fileorganiserwidget.cpp
@@ -104,11 +104,6 @@ static const auto SettingsSelectedItem = QStringLiteral("SelectedItem");
 void FileOrganiserWidget::loadItemSettings(QSettings *pSettings,
                                            QStandardItem *pParentItem)
 {
-    // Make sure that we have a parent item
-
-    if (!pParentItem)
-        return;
-
     // Recursively retrieve the item settings
 
     static int crtItemIndex = -1;