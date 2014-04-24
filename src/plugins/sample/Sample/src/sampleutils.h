7d7a680c9ad5a6c0704bf0d36be402bab775909b
Alan Garny <agarny@hellix.com>
2014-04-24 18:30:55 +0200

    Some work towards writing some sample plugins (#337) [ci skip].

---
+++ src/plugins/sample/Sample/src/sampleutils.h
@@ -0,0 +1,49 @@
+/*******************************************************************************
+
+Licensed to the OpenCOR team under one or more contributor license agreements.
+See the NOTICE.txt file distributed with this work for additional information
+regarding copyright ownership. The OpenCOR team licenses this file to you under
+the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+
+*******************************************************************************/
+
+//==============================================================================
+// Sample utilities
+//==============================================================================
+
+#ifndef SAMPLEUTILS_H
+#define SAMPLEUTILS_H
+
+//==============================================================================
+
+#include "sampleglobal.h"
+
+//==============================================================================
+
+namespace OpenCOR {
+namespace Sample {
+
+//==============================================================================
+
+double SAMPLE_EXPORT add(const double &pNb1, const double &pNb2);
+
+//==============================================================================
+
+}   // namespace Sample
+}   // namespace OpenCOR
+
+//==============================================================================
+
+#endif
+
+//==============================================================================
+// End of file
+//==============================================================================
