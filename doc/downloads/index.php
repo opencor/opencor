04ddac4ef18fa0a51d3ea0edd48d7ea2cbead3b5
Alan Garny <agarny@hellix.com>
2014-07-23 18:59:16 +0200

    New snapshots.

--- doc/downloads/index.php
+++ doc/downloads/index.php
@@ -39,7 +39,7 @@
 
     date_default_timezone_set("Europe/London");
 
-    $versions = array(array(0, 0, 0, 13, 4, 2014, 2,
+    $versions = array(array(0, 0, 0, 23, 7, 2014, 2,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip"))),
