1b28184d332a5ed4ac70c845b9dc96165bec6ef2
Alan Garny <agarny@hellix.com>
2014-07-23 19:20:59 +0200

    Updated the download page [ci skip].

--- doc/downloads/index.php
+++ doc/downloads/index.php
@@ -41,7 +41,7 @@
 
     $versions = array(array(0, 0, 0, 23, 7, 2014, 2,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
-                                  array("Linux", "Ubuntu 12.04 LTS (Precise Pangolin) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
+                                  array("Linux", "Ubuntu 13.04 LTS (Raring Ringtail) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip"))),
                             "<strong>Note:</strong> this version is fairly stable and therefore the one we would recommend using, especially if you want to get access to the latest features (new snapshots are announced on our <a href=\"https://twitter.com/TeamOpenCOR/\">Twitter feed</a>)."),
                       array(0, 2, 0, 19, 11, 2013, 1,
