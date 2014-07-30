21da2ac02e26a0ed4c395812343c95c1584ec2b4
Alan Garny <agarny@hellix.com>
2014-07-30 17:45:47 +0200

    Some minor cleaning up [ci skip].

--- src/plugins/support/CellMLSupport/src/cellmlfile.h
+++ src/plugins/support/CellMLSupport/src/cellmlfile.h
@@ -68,8 +68,8 @@ public:
     bool save(const QString &pNewFileName = QString());
 
     bool isValid();
-    static bool isValid(const QString &pFileName, const QString &pFileContents,
-                        CellmlFileIssues &pIssues);
+    bool isValid(const QString &pFileName, const QString &pFileContents,
+                 CellmlFileIssues &pIssues);
 
     bool isModified() const;
     void setModified(const bool &pModified) const;
@@ -128,20 +128,19 @@ private:
 
     void reset();
 
-    static void retrieveImports(iface::cellml_api::Model *pModel,
-                                QList<iface::cellml_api::CellMLImport *> &pImportList,
-                                QStringList &pImportXmlBaseList,
-                                const QString &pXmlBase);
+    void retrieveImports(iface::cellml_api::Model *pModel,
+                         QList<iface::cellml_api::CellMLImport *> &pImportList,
+                         QStringList &pImportXmlBaseList,
+                         const QString &pXmlBase);
 
-    static bool fullyInstantiateImports(iface::cellml_api::Model *pModel,
-                                        CellmlFileIssues &pIssues);
+    bool fullyInstantiateImports(iface::cellml_api::Model *pModel,
+                                 CellmlFileIssues &pIssues);
 
-    static bool doLoad(const QString &pFileName, const QString &pFileContents,
-                       ObjRef<iface::cellml_api::Model> *pModel,
-                       CellmlFileIssues &pIssues);
+    bool doLoad(const QString &pFileName, const QString &pFileContents,
+                ObjRef<iface::cellml_api::Model> *pModel,
+                CellmlFileIssues &pIssues);
 
-    static bool doIsValid(iface::cellml_api::Model *pModel,
-                          CellmlFileIssues &pIssues);
+    bool doIsValid(iface::cellml_api::Model *pModel, CellmlFileIssues &pIssues);
 
     bool rdfTripleExists(iface::cellml_api::CellMLElement *pElement,
                          const QString &pQualifier,
