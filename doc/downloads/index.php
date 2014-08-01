14406995e3c71f6978afe1a03eb2c98812a906d7
Alan Garny <agarny@hellix.com>
2014-08-01 11:26:20 +0200

    New snapshots.

--- doc/downloads/index.php
+++ doc/downloads/index.php
@@ -1,5 +1,5 @@
 <?php
-    $versions = array(array(0, 0, 0, 29, 7, 2014, 2,
+    $versions = array(array(0, 0, 0, 1, 8, 2014, 2,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 13.04 LTS (Raring Ringtail) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip"))),
