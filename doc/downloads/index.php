7c1618e608069e106243ad6b82f9244884237709
Alan Garny <agarny@hellix.com>
2014-08-01 15:56:33 +0200

    Some minor cleaning up [ci skip].

--- doc/downloads/index.php
+++ doc/downloads/index.php
@@ -1,7 +1,7 @@
 <?php
     $versions = array(array(0, 0, 0, 1, 8, 2014, 2,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
-                                  array("Linux", "Ubuntu 13.04 LTS (Raring Ringtail) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
+                                  array("Linux", "Ubuntu 14.04 LTS (Raring Ringtail) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip"))),
                             "<strong>Note:</strong> this version is fairly stable and therefore the one we would recommend using, especially if you want to get access to the latest features (new snapshots are announced on our <a href=\"https://twitter.com/TeamOpenCOR/\">Twitter feed</a>). Previous snapshots can be found <a href=\"previousSnapshots.php\">here</a>."),
                       array(0, 2, 0, 19, 11, 2013, 1,
