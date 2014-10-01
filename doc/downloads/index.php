a7e9df7d197772570ebd9cf2d308a49f4041ad9c
Alan Garny <agarny@hellix.com>
2014-10-01 18:47:50 +0200

    New snapshots.

--- doc/downloads/index.php
+++ doc/downloads/index.php
@@ -3,7 +3,7 @@
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 14.04 LTS (Trusty Tahr) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip")))),
-                      array(0, 0, 0, 7, 9, 2014, 2,
+                      array(0, 0, 0, 1, 10, 2014, 2,
                             array(array("Windows", "Windows XP and later", array(".exe", 32), array(".zip", 32), array(".exe", 64), array(".zip", 64)),
                                   array("Linux", "Ubuntu 14.04 LTS (Trusty Tahr) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip")))),
--- doc/downloads/oldSnapshots.php
+++ doc/downloads/oldSnapshots.php
@@ -1,6 +1,10 @@
 <?php
     $oldSnapshots = true;
-    $versions = array(array(0, 0, 0, 6, 8, 2014, 2,
+    $versions = array(array(0, 0, 0, 7, 9, 2014, 2,
+                            array(array("Windows", "Windows XP and later", array(".exe", 32), array(".zip", 32), array(".exe", 64), array(".zip", 64)),
+                                  array("Linux", "Ubuntu 14.04 LTS (Trusty Tahr) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
+                                  array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip")))),
+                      array(0, 0, 0, 6, 8, 2014, 2,
                             array(array("Windows", "Windows XP and later", array(".exe"), array(".zip")),
                                   array("Linux", "Ubuntu 14.04 LTS (Trusty Tahr) and later", array(".tar.gz", 32), array(".tar.gz", 64)),
                                   array("OS X", "Mac OS X 10.7 (Lion) and later", array(".dmg"), array(".zip")))),
