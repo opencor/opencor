546149ef941e6400932c78f3320f9e87b5d30b69
Alan Garny <agarny@hellix.com>
2014-11-29 18:33:26 +0100

    Some minor cleaning up [ci skip].

--- src/plugins/support/CellMLSupport/src/cellmlfilerdftripleelement.cpp
+++ src/plugins/support/CellMLSupport/src/cellmlfilerdftripleelement.cpp
@@ -112,9 +112,6 @@ CellmlFileRdfTripleElement::CellmlFileRdfTripleElement(iface::rdf_api::Node *pRd
 
     // Keep track of the RDF triple element's value as a string, which is based
     // on its type
-    // Note: indeed, this value is constant, so it's better to generate it once
-    //       and for all than generate it every time we need it (which could
-    //       take some time if it's needed a lot)...
 
     switch (mType) {
     case UriReference:
