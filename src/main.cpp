8314e3a3d6b25dcd334aece7ca0c4bcd52423eb4
Alan Garny <agarny@hellix.com>
2014-12-11 19:09:49 +0100

    Reverting commit f2d5af4794056ab99a47cdb385835aafa820e7d1 (#514) [ci skip].

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,10 @@ specific language governing permissions and limitations under the License.
 #include <QSettings>
 #include <QVariant>
 
+#ifdef Q_OS_WIN
+    #include <QWebSettings>
+#endif
+
 //==============================================================================
 
 #include <QtSingleApplication>
@@ -308,6 +312,20 @@ int main(int pArgC, char *pArgV[])
     // Delete our main window
 
     delete win;
+
+    // We use QtWebKit, and QWebPage in particular, which results in some leak
+    // messages being generated on Windows when leaving OpenCOR. This is because
+    // an object cache is shared between all QWebPage instances. So to destroy a
+    // QWebPage instance doesn't clear the cache, hence the leak messages.
+    // However, those messages are 'only' warnings, so we can safely live with
+    // them. Still, it doesn't look 'good', so we clear the memory caches, thus
+    // avoiding those leak messages...
+    // Note: the below must absolutely be done after calling guiApp->exec() and
+    //       before deleting guiApp...
+
+#ifdef Q_OS_WIN
+    QWebSettings::clearMemoryCaches();
+#endif
 
     // Remove all 'global' instances that were created and used during this
     // session
