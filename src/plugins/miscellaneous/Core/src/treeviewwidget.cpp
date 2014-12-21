d1f65039827c7734eb911c5058d1b1342ba1799c
Alan Garny <agarny@hellix.com>
2014-12-21 23:40:20 +0100

    Some minor cleaning up [ci skip].

--- src/plugins/miscellaneous/Core/src/treeviewwidget.cpp
+++ src/plugins/miscellaneous/Core/src/treeviewwidget.cpp
@@ -14,7 +14,6 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 
 *******************************************************************************/
-#include <QDebug>
 
 //==============================================================================
 // Enhanced tree view widget
