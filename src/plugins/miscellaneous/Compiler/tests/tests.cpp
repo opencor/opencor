9a37f3a73f7f07d871755bdd7c54ddeb80d7a018
Alan Garny <agarny@hellix.com>
2015-03-08 09:25:17 +0100

    LLVM: some work on upgrading to version 3.6 (#558).

--- src/plugins/miscellaneous/Compiler/tests/tests.cpp
+++ src/plugins/miscellaneous/Compiler/tests/tests.cpp
@@ -29,6 +29,20 @@ specific language governing permissions and limitations under the License.
 
 //==============================================================================
 
+#if defined(Q_OS_LINUX) || defined(Q_OS_MAC)
+    #pragma GCC diagnostic ignored "-Wunused-parameter"
+    #pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#endif
+
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+
+#if defined(Q_OS_LINUX) || defined(Q_OS_MAC)
+    #pragma GCC diagnostic error "-Wunused-parameter"
+    #pragma GCC diagnostic error "-Wstrict-aliasing"
+#endif
+
+//==============================================================================
+
 static const double A = 5.0;
 static const double B = 7.9;
 
