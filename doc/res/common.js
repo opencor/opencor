4b1380da677be25976459bfac233cbf3d0024128
Alan Garny <agarny@hellix.com>
2015-04-20 13:58:55 +1200

    Documentation: some work on updating it in preparation for version 0.4 (#594) [ci skip].

--- doc/res/common.js
+++ doc/res/common.js
@@ -120,7 +120,7 @@ function doHeaderAndContentsMenu(pageName, relativePath, r, g, b, data) {
     document.write("        position: fixed;");
     document.write("        top: 29px;");
     document.write("        right: 0px;");
-    document.write("        background: rgb(249, 249, 249);");
+    document.write("        background-color: rgb(249, 249, 249);");
     document.write("        border: 2px solid rgb("+r+", "+g+", "+b+");");
     document.write("        border-top-style: none;");
     document.write("        border-radius: 0px 0px 5px 5px;");
@@ -148,7 +148,7 @@ function doHeaderAndContentsMenu(pageName, relativePath, r, g, b, data) {
     document.write("    }");
     document.write("");
     document.write("    ul.contentsMenu > li > ul > li div.lastMenuItem.clickableMenuItem:hover {");
-    document.write("        background: rgb(249, 249, 249);");
+    document.write("        background-color: rgb(249, 249, 249);");
     document.write("    }");
     document.write("");
     document.write("    ul.contentsMenu > li > ul > li div.lastMenuItem.clickableMenuItem > div:hover {");
@@ -177,7 +177,7 @@ function doHeaderAndContentsMenu(pageName, relativePath, r, g, b, data) {
     document.write("");
     document.write("    ul.contentsMenu > li > ul > li div.nonClickableMenuItem > div a {");
     document.write("        cursor: default;");
-    document.write("        background: rgb(249, 249, 249);");
+    document.write("        background-color: rgb(249, 249, 249);");
     document.write("        color: rgb(0, 0, 0);");
     document.write("    }");
     document.write("");
