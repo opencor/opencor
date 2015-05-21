2e34fa2da107c5dc265ed80d53741fe7d5f63796
Alan Garny <agarny@hellix.com>
2015-05-21 10:48:28 +0200

    Slight updates to the user documentation [ci skip].

--- doc/res/downloads.js
+++ doc/res/downloads.js
@@ -5,37 +5,6 @@ function twoDigits(number) {
         return number;
 }
 
-function month(month) {
-    switch (month) {
-    case 1:
-        return "January";
-    case 2:
-        return "February";
-    case 3:
-        return "March";
-    case 4:
-        return "April";
-    case 5:
-        return "May";
-    case 6:
-        return "June";
-    case 7:
-        return "July";
-    case 8:
-        return "August";
-    case 9:
-        return "September";
-    case 10:
-        return "October";
-    case 11:
-        return "November";
-    case 12:
-        return "December";
-    default:
-        return "???";
-    }
-}
-
 function fileSize(size) {
     if (typeof size !== "undefined") {
         var units = [ "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" ];
--- doc/user/whatIsNew.js
+++ doc/user/whatIsNew.js
@@ -1,5 +1,5 @@
 var jsonData = { "versions": [
-                   { "anchor": "latest", "description": "Latest snapshot", "date": "???",
+                   { "anchor": "latest", "description": "Latest snapshot", "day": 0, "month": 0, "year": 0,
                      "categories": [
                        { "name": "Simulation",
                          "entries": [
@@ -17,7 +17,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "anchor": "0.4.1", "description": "Version 0.4.1", "date": "20 May 2015",
+                   { "anchor": "0.4.1", "description": "Version 0.4.1", "day": 20, "month": 5, "year": 2015,
                      "categories": [
                        { "name": "Editing",
                          "entries": [
@@ -44,7 +44,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "anchor": "0.4", "description": "Version 0.4", "date": "29 April 2015",
+                   { "anchor": "0.4", "description": "Version 0.4", "day": 29, "month": 4, "year": 2015,
                      "categories": [
                        { "name": "General",
                          "entries": [
@@ -146,7 +146,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "anchor": "0.3", "description": "Version 0.3", "date": "19 August 2014",
+                   { "anchor": "0.3", "description": "Version 0.3", "day": 19, "month": 8, "year": 2014,
                      "categories": [
                        { "name": "General",
                          "entries": [
@@ -252,7 +252,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "anchor": "0.2", "description": "Version 0.2", "date": "19 November 2013",
+                   { "anchor": "0.2", "description": "Version 0.2", "day": 19, "month": 11, "year": 2013,
                      "categories": [
                        { "name": "General",
                          "entries": [
@@ -311,7 +311,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "anchor": "0.1.2", "description": "Version 0.1.2", "date": "29 May 2013",
+                   { "anchor": "0.1.2", "description": "Version 0.1.2", "day": 29, "month": 5, "year": 2013,
                      "categories": [
                        { "name": "Simulation",
                          "entries": [
@@ -324,7 +324,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "anchor": "0.1.1", "description": "Version 0.1.1", "date": "17 April 2013",
+                   { "anchor": "0.1.1", "description": "Version 0.1.1", "day": 17, "month": 4, "year": 2013,
                      "categories": [
                        { "name": "General",
                          "entries": [
@@ -343,7 +343,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "anchor": "0.1", "description": "Version 0.1", "date": "1 April 2013",
+                   { "anchor": "0.1", "description": "Version 0.1", "day": 1, "month": 4, "year": 2013,
                      "categories": [
                        { "name": "Original version of OpenCOR" }
                      ]
