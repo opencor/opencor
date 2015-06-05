12e04fb41261a290e260194cdf150b0a0ea9be4c
Alan Garny <agarny@hellix.com>
2015-06-05 20:07:50 +0200

    Some minor cleaning up [ci skip].

--- src/plugins/organisation/FileBrowserWindow/src/filebrowserwindowwidget.cpp
+++ src/plugins/organisation/FileBrowserWindow/src/filebrowserwindowwidget.cpp
@@ -440,11 +440,12 @@ void FileBrowserWindowWidget::keyPressEvent(QKeyEvent *pEvent)
     QStringList crtSelectedFiles = selectedFiles();
 
     if (   crtSelectedFiles.count()
-        && ((pEvent->key() == Qt::Key_Enter) || (pEvent->key() == Qt::Key_Return)))
+        && ((pEvent->key() == Qt::Key_Enter) || (pEvent->key() == Qt::Key_Return))) {
         // There are some files that are selected and we want to open them, so
         // let people know about it
 
         emit filesOpenRequested(crtSelectedFiles);
+    }
 }
 
 //==============================================================================
