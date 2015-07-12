6d307fb7339e0ec5d008581897c11943c3710dda
Alan Garny <agarny@hellix.com>
2015-07-12 11:27:26 +0200

    Some work on adding support for SED-ML (#14) [ci skip].

--- src/plugins/api/SEDMLAPI/include/sedml/common/osx/libsedml-config-common.h
+++ src/plugins/api/SEDMLAPI/include/sedml/common/osx/libsedml-config-common.h
@@ -1,26 +1,26 @@
 /**
  * @file    libsedml-config-common.h.cmake
- * @brief   Configuration variables
+ * @brief   Configuration variables 
  *
  * <!--------------------------------------------------------------------------
  *
  * This file is part of libSEDML.  Please visit http://sed-ml.org for more
- * information about SED-ML. The latest version of libSEDML can be found on
+ * information about SED-ML. The latest version of libSEDML can be found on 
  * github: https://github.com/fbergmann/libSEDML/
- *
- *
- * Copyright (c) 2013-2014, Frank T. Bergmann
+ * 
+ * 
+ * Copyright (c) 2013-2014, Frank T. Bergmann  
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
+ * modification, are permitted provided that the following conditions are met: 
+ * 
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
+ *    list of conditions and the following disclaimer. 
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
+ *    and/or other materials provided with the distribution. 
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -31,7 +31,7 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * ---------------------------------------------------------------------- -->
  *
  * If this file is named <tt>libsedml-config-unix.h</tt>, then it was
@@ -66,7 +66,7 @@
 
 
 /* Define to 1 if you have the <errno.h> header file. */
-#define HAVE_ERRNO_H 1
+#define HAVE_ERRNO_H 1 
 
 /* Define to 1 if you have the <ieeefp.h> header file. */
 /* #undef HAVE_IEEEFP_H */
