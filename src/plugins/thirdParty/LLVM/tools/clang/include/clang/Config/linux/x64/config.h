3a2df1fb1ee2434e8174de4c1afaf1365d0e03f8
Alan Garny <agarny@hellix.com>
2015-07-26 12:29:51 +0200

    LLVM: some work on upgrading to version 3.6.2 (#699) [ci skip].

--- src/plugins/thirdParty/LLVM/tools/clang/include/clang/Config/linux/x64/config.h
+++ src/plugins/thirdParty/LLVM/tools/clang/include/clang/Config/linux/x64/config.h
@@ -24,7 +24,7 @@
 #define GCC_INSTALL_PREFIX ""
 
 /* Define if we have libxml2 */
-/* #undef CLANG_HAVE_LIBXML */
+#define CLANG_HAVE_LIBXML 1
 
 /* The LLVM product name and version */
 #define BACKEND_PACKAGE_STRING "LLVM 3.6.2"
