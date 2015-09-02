260df78a43de414b93d4146833ce0e3f446bc114
Alan Garny <agarny@hellix.com>
2015-09-02 18:30:37 +0200

    Single Cell view: some work towards exporting a simulation to SED-ML (#684) [ci skip].

--- src/plugins/solver/CVODESolver/src/cvodesolverplugin.cpp
+++ src/plugins/solver/CVODESolver/src/cvodesolverplugin.cpp
@@ -70,7 +70,10 @@ QString CVODESolverPlugin::kisaoId(const QString &pId) const
 {
     // Return the KiSAO id for the given id
 
-    return pId;
+    if (!pId.compare(solverName()))
+        return "KISAO:0000283";
+
+    return "???";
 }
 
 //==============================================================================
