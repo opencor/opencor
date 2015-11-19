bd8f20d4b012e98e2be40c44efd3eb8fe648e2cb
Alan Garny <agarny@hellix.com>
2015-11-19 16:52:30 +0100

    Some minor cleaning up [ci skip].

--- src/plugins/support/CellMLSupport/src/cellmlsupportplugin.h
+++ src/plugins/support/CellMLSupport/src/cellmlsupportplugin.h
@@ -24,7 +24,6 @@ specific language governing permissions and limitations under the License.
 
 //==============================================================================
 
-#include "cellmlfile.h"
 #include "filetypeinterface.h"
 #include "guiinterface.h"
 #include "i18ninterface.h"
