485b0ab1c7a5373ebf6dec977a02a8c927ce5df6
Alan Garny <agarny@hellix.com>
2015-12-01 14:13:26 +0100

    COMBINE archive: some work on loading/saving a COMBINE archive [ci skip].

--- src/plugins/support/COMBINESupport/src/combinearchive.cpp
+++ src/plugins/support/COMBINESupport/src/combinearchive.cpp
@@ -125,124 +125,116 @@ static const auto SedmlFormat       = QStringLiteral("http://identifiers.org/com
 
 bool CombineArchive::save(const QString &pNewFileName)
 {
-    // Create a temporary directory where we are going to put ourselves
+    // Keep track of our current directory
 
-    QTemporaryDir temporaryDir;
+    QString origPath = QDir::currentPath();
 
-    if (temporaryDir.isValid()) {
-        // Keep track of our current directory
+    // Create and go to a sub-directory where we are effecitvely going to put
+    // ourselves
 
-        QString origPath = QDir::currentPath();
+    QDir dir;
+    QString fileName = pNewFileName.isEmpty()?mFileName:pNewFileName;
+    QString baseDirName = QFileInfo(fileName).baseName();
+    QString dirName = mDirName+QDir::separator()+baseDirName;
 
-        // Create and go to a sub-directory where we are effecitvely going to
-        // put ourselves
+    dir.mkpath(dirName);
 
-        QDir dir;
-        QString fileName = pNewFileName.isEmpty()?mFileName:pNewFileName;
-        QString baseDirName = QFileInfo(fileName).baseName();
-        QString dirName = temporaryDir.path()+QDir::separator()+baseDirName;
+    QDir::setCurrent(dirName);
 
-        dir.mkpath(dirName);
+    // Create our manifest file
 
-        QDir::setCurrent(dirName);
+    static const QString ManifestFileName = "manifest.xml";
 
-        // Create our manifest file
+    QString fileList = QString();
+    QString fileFormat;
 
-        static const QString ManifestFileName = "manifest.xml";
+    foreach (const CombineArchiveFile &combineArchiveFile, mCombineArchiveFiles) {
+        switch (combineArchiveFile.format()) {
+        case CombineArchiveFile::Cellml:
+            fileFormat = CellmlFormat;
 
-        QString fileList = QString();
-        QString fileFormat;
+            break;
+        case CombineArchiveFile::Cellml_1_0:
+            fileFormat = Cellml_1_0_Format;
 
-        foreach (const CombineArchiveFile &combineArchiveFile, mCombineArchiveFiles) {
-            switch (combineArchiveFile.format()) {
-            case CombineArchiveFile::Cellml:
-                fileFormat = CellmlFormat;
+            break;
+        case CombineArchiveFile::Cellml_1_1:
+            fileFormat = Cellml_1_1_Format;
 
-                break;
-            case CombineArchiveFile::Cellml_1_0:
-                fileFormat = Cellml_1_0_Format;
+            break;
+        case CombineArchiveFile::Sedml:
+            fileFormat = SedmlFormat;
 
-                break;
-            case CombineArchiveFile::Cellml_1_1:
-                fileFormat = Cellml_1_1_Format;
-
-                break;
-            case CombineArchiveFile::Sedml:
-                fileFormat = SedmlFormat;
-
-                break;
-            default:   // CombineArchiveFile::Unknown
-                return false;
-            }
-
-            fileList += "    <content location=\""+combineArchiveFile.location()+"\" format=\""+fileFormat+"\"";
-
-            if (combineArchiveFile.isMaster())
-                fileList += " master=\"true\"";
-
-            fileList += "/>\n";
+            break;
+        default:   // CombineArchiveFile::Unknown
+            return false;
         }
 
-        QByteArray manifestFileContents =  "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-                                           "<omexManifest xmlns=\"http://identifiers.org/combine.specifications/omex-manifest\">\n"
-                                           "    <content location=\".\" format=\"http://identifiers.org/combine.specifications/omex\"/>\n"
-                                          +fileList.toUtf8()
-                                          +"</omexManifest>\n";
+        fileList += "    <content location=\""+combineArchiveFile.location()+"\" format=\""+fileFormat+"\"";
 
-        if (!Core::writeTextToFile(ManifestFileName, manifestFileContents))
-            return false;
+        if (combineArchiveFile.isMaster())
+            fileList += " master=\"true\"";
 
-        // Get a copy of our various files, if any, after creating the
-        // sub-folder(s) in which they are
+        fileList += "/>\n";
+    }
+
+    QByteArray manifestFileContents =  "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                                       "<omexManifest xmlns=\"http://identifiers.org/combine.specifications/omex-manifest\">\n"
+                                       "    <content location=\".\" format=\"http://identifiers.org/combine.specifications/omex\"/>\n"
+                                      +fileList.toUtf8()
+                                      +"</omexManifest>\n";
+
+    if (!Core::writeTextToFile(ManifestFileName, manifestFileContents))
+        return false;
+
+    // Get a copy of our various files, if any, after creating the sub-folder(s)
+    // in which they are
 
 #if defined(Q_OS_WIN)
-        static const QRegularExpression FileNameRegEx = QRegularExpression("\\\\[^\\\\]*$");
+    static const QRegularExpression FileNameRegEx = QRegularExpression("\\\\[^\\\\]*$");
 #elif defined(Q_OS_LINUX) || defined(Q_OS_MAC)
-        static const QRegularExpression FileNameRegEx = QRegularExpression("/[^/]*$");
+    static const QRegularExpression FileNameRegEx = QRegularExpression("/[^/]*$");
 #else
     #error Unsupported platform
 #endif
 
-        foreach (const CombineArchiveFile &combineArchiveFile, mCombineArchiveFiles) {
-            QString destFileName = Core::nativeCanonicalFileName(dirName+QDir::separator()+combineArchiveFile.location());
-            QString destDirName = QString(destFileName).remove(FileNameRegEx);
+    foreach (const CombineArchiveFile &combineArchiveFile, mCombineArchiveFiles) {
+        QString destFileName = Core::nativeCanonicalFileName(dirName+QDir::separator()+combineArchiveFile.location());
+        QString destDirName = QString(destFileName).remove(FileNameRegEx);
 
-            if (!QDir(destDirName).exists()) {
-                if (!dir.mkpath(destDirName))
-                    return false;
-            }
-
-            if (!QFile::copy(combineArchiveFile.fileName(), destFileName))
+        if (!QDir(destDirName).exists()) {
+            if (!dir.mkpath(destDirName))
                 return false;
         }
 
-        // Go back to our original path
-
-        QDir::setCurrent(origPath);
-
-        // Save ourselves to either the given file, which name is given, or to
-        // our current file
-
-        OpenCOR::ZIPSupport::QZipWriter zipWriter(fileName);
-
-        zipWriter.addFile(ManifestFileName, manifestFileContents);
-
-        foreach (const CombineArchiveFile &combineArchiveFile, mCombineArchiveFiles) {
-            QString combineArchiveFileContents;
-
-            if (!Core::readTextFromFile(dirName+QDir::separator()+combineArchiveFile.location(), combineArchiveFileContents))
-                return false;
-
-            zipWriter.addFile(combineArchiveFile.location(),
-                              combineArchiveFileContents.toUtf8());
-        }
-
-        zipWriter.close();
-
-        return true;
-    } else {
-        return false;
+        if (!QFile::copy(combineArchiveFile.fileName(), destFileName))
+            return false;
     }
+
+    // Go back to our original path
+
+    QDir::setCurrent(origPath);
+
+    // Save ourselves to either the given file, which name is given, or to our
+    // current file
+
+    OpenCOR::ZIPSupport::QZipWriter zipWriter(fileName);
+
+    zipWriter.addFile(ManifestFileName, manifestFileContents);
+
+    foreach (const CombineArchiveFile &combineArchiveFile, mCombineArchiveFiles) {
+        QString combineArchiveFileContents;
+
+        if (!Core::readTextFromFile(dirName+QDir::separator()+combineArchiveFile.location(), combineArchiveFileContents))
+            return false;
+
+        zipWriter.addFile(combineArchiveFile.location(),
+                          combineArchiveFileContents.toUtf8());
+    }
+
+    zipWriter.close();
+
+    return true;
 }
 
 //==============================================================================
