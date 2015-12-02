8f7cc4bf0fb0e725791ea2d42f8098fd18889ab1
Alan Garny <agarny@hellix.com>
2015-12-02 19:56:06 +0100

    COMBINE archive: some work on loading/saving a COMBINE archive [ci skip].

--- src/plugins/support/COMBINESupport/i18n/COMBINESupport_fr.ts
+++ src/plugins/support/COMBINESupport/i18n/COMBINESupport_fr.ts
@@ -15,14 +15,6 @@
         <translation>l&apos;archive n&apos;existe pas</translation>
     </message>
     <message>
-        <source>the archive is not signed</source>
-        <translation>l&apos;archive n&apos;est pas signée</translation>
-    </message>
-    <message>
-        <source>the archive does not have the correct signature</source>
-        <translation>l&apos;archive n&apos;a pas la bonne signature</translation>
-    </message>
-    <message>
         <source>the contents of the archive could not be extracted</source>
         <translation>le contenu de l&apos;archive n&apos;a pas pu être extrait</translation>
     </message>
