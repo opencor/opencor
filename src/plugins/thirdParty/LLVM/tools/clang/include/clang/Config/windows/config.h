0913651fb9166ec274748e5d922c6572c80f7745
Alan Garny <agarny@hellix.com>
2016-01-06 16:42:08 +0100

    LLVM: some work towards upgrading to version 3.7.1 (#859) [ci skip].

--- src/plugins/thirdParty/LLVM/tools/clang/include/clang/Config/windows/config.h
+++ src/plugins/thirdParty/LLVM/tools/clang/include/clang/Config/windows/config.h
@@ -30,7 +30,7 @@
 /* #undef CLANG_HAVE_LIBXML */
 
 /* The LLVM product name and version */
-#define BACKEND_PACKAGE_STRING "LLVM 3.7.0"
+#define BACKEND_PACKAGE_STRING "LLVM 3.7.1"
 
 /* Linker version detected at compile time. */
 /* #undef HOST_LINK_VERSION */
