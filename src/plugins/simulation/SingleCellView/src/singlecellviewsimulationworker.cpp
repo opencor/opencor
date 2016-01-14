c4d6ac179d1c48b99e96aea28d27372282cf4219
Alan Garny <agarny@hellix.com>
2016-01-14 19:21:41 +0100

    Some minor cleaning up [ci skip].

--- src/plugins/simulation/SingleCellView/src/singlecellviewsimulationworker.cpp
+++ src/plugins/simulation/SingleCellView/src/singlecellviewsimulationworker.cpp
@@ -27,6 +27,7 @@ specific language governing permissions and limitations under the License.
 //==============================================================================
 
 #include <QElapsedTimer>
+#include <QMutex>
 #include <QThread>
 
 //==============================================================================
