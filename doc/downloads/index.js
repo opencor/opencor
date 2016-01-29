65c10b00ae299c47aa3cbefad7a7401bf9249b7b
Alan Garny <agarny@hellix.com>
2016-01-29 17:07:23 +0100

    Slight updates to the downloads page [ci skip].

--- doc/downloads/index.js
+++ doc/downloads/index.js
@@ -40,7 +40,7 @@ var jsonData = { "versions": [
                      ],
                      "changes": [
                        { "change": "<strong>Viewer widget:</strong> fixed the rendering of some equations (see issue <a href=\"https://github.com/opencor/opencor/issues/869\">#869</a>)." },
-                       { "change": "<strong>Single Cell view:</strong> fixed a regression issue following our work on issue <a href=\"https://github.com/opencor/opencor/issues/590\">#590</a> (see issue <a href=\"https://github.com/opencor/opencor/issues/868\">#868</a>)." }
+                       { "change": "<strong>Single Cell view:</strong> fixed a regression issue following our work on issue <a href=\"https://github.com/opencor/opencor/issues/590\">#590</a> (see issues <a href=\"https://github.com/opencor/opencor/issues/866\">#866</a> and <a href=\"https://github.com/opencor/opencor/issues/868\">#868</a>). Fixed a small problem with the progress bar not always getting reset (see issue <a href=\"https://github.com/opencor/opencor/issues/874\">#874</a>)." }
                      ]
                    },
                    { "major": 0, "minor": 4, "patch": 0, "day": 29, "month": 4, "year": 2015, "type": 0,
