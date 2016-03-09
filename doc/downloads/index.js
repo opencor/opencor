629e2bc085078ce5942da44b7cacce250f7b78fa
Alan Garny <agarny@hellix.com>
2016-03-09 11:32:28 +0100

    Some minor cleaning up [ci skip].

--- doc/downloads/index.js
+++ doc/downloads/index.js
@@ -39,7 +39,7 @@ var jsonData = { "versions": [
                        }
                      ],
                      "changes": [
-                       { "change": "<strong>General:</strong> fixed a problem with OpenCOR firing up on 'old' versions of OS X, but not opening the CellML/SED-ML file referenced in an OpenCOR URL (see issue <a href=\"https://github.com/opencor/opencor/issues/909\">#909</a>)." }
+                       { "change": "<strong>General:</strong> fixed a problem with OpenCOR firing up an 'old' version of OS X, but not opening the CellML/SED-ML file referenced in an OpenCOR URL (see issue <a href=\"https://github.com/opencor/opencor/issues/909\">#909</a>)." }
                      ]
                    },
                    { "major": 0, "minor": 4, "patch": 0, "day": 29, "month": 4, "year": 2015, "type": 0,
