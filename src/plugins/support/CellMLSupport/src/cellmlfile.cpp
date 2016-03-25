opencor/opencor

commit 55da5ee36ccb979b91965cdc2e413a593230db98

CellML 1.1 validation: now warn the user that an issue may be related to an (in)directly imported CellML file (#888).

src/plugins/support/CellMLSupport/src/cellmlfile.cpp
@@ -1085,6 +1085,15 @@ bool CellmlFile::exportTo(const QString &pFileName,
 
 //==============================================================================
 
+CellmlFile::Version CellmlFile::version() const
+{
+    // Return our version
+
+    return CellmlFile::version(mModel);
+}
+
+//==============================================================================
+
 CellmlFile::Version CellmlFile::version(iface::cellml_api::Model *pModel)
 {
     // Return the version of the given CellML model, if any

src/plugins/support/CellMLSupport/src/cellmlfile.h
@@ -126,6 +126,8 @@ public:
     bool exportTo(const QString &pFileName,
                   const QString &pUserDefinedFormatFileName);
 
+    Version version() const;
+
     static Version version(iface::cellml_api::Model *pModel);
     static Version version(CellmlFile *pCellmlFile);
     static Version version(const QString &pFileName);

src/plugins/editing/RawCellMLView/i18n/RawCellMLView_fr.ts
@@ -12,4 +12,15 @@
         <translation>le &lt;a href=&quot;http://cellml-api.sourceforge.net/&quot;&gt;service de validation CellML&lt;/a&gt; est connu pour avoir des problèmes et peut donc incorrectement (in)valider certains fichiers CellML.</translation>
     </message>
 </context>
+<context>
+    <name>OpenCOR::RawCellMLView::RawCellmlViewWidget</name>
+    <message>
+        <source>The issue reported below may be related to this CellML file or to one of its (in)directly imported CellML files.</source>
+        <translation>Le problème mentionné ci-dessous peut être en rapport avec ce fichier CellML ou avec un de ses fichiers CellML qui a été (in)directement importé.</translation>
+    </message>
+    <message>
+        <source>The issues reported below may be related to this CellML file and/or to one or several of its (in)directly imported CellML files.</source>
+        <translation>Les problèmes mentionnés ci-dessous peuvent être en rapport avec ce fichier CellML et/ou avec un ou plusieurs de ses fichiers CellML qui ont été (in)directement importés.</translation>
+    </message>
+</context>
 </TS>

src/plugins/editing/RawCellMLView/src/rawcellmlviewwidget.cpp
@@ -330,6 +330,16 @@ bool RawCellmlViewWidget::validate(const QString &pFileName,
 
         bool res = cellmlFile->isValid(editingWidget->editor()->contents(), cellmlFileIssues);
 
+        // Warn the user about the CellML issues being maybe for a (in)direclty
+        // imported CellML file, should we be dealing with a CellML 1.1 file
+
+        if (cellmlFile->version() != CellMLSupport::CellmlFile::Cellml_1_0) {
+            editorList->addItem(EditorList::EditorListItem::Information,
+                                (cellmlFileIssues.count() == 1)?
+                                    tr("The issue reported below may be related to this CellML file or to one of its (in)directly imported CellML files."):
+                                    tr("The issues reported below may be related to this CellML file and/or to one or several of its (in)directly imported CellML files."));
+        }
+
         // Add whatever issue there may be to our list and select the first one
         // of them
 

commit 05ce6a6b247d37c2ea48eaec7d2e51e31688352b

Fixed a small issue that got introduced in commit 55da5ee [ci skip].

src/plugins/support/CellMLSupport/src/cellmlfile.cpp
@@ -928,11 +928,14 @@ QString CellmlFile::importedFileContents(const QString &pImportedFileName) const
 
 //==============================================================================
 
-QString CellmlFile::cmetaId() const
+QString CellmlFile::cmetaId()
 {
     // Return the CellML model's cmeta:id
 
-    return QString::fromStdWString(mModel->cmetaId());
+    if (load())
+        return QString::fromStdWString(mModel->cmetaId());
+    else
+        return QString();
 }
 
 //==============================================================================
@@ -1081,11 +1084,14 @@ bool CellmlFile::exportTo(const QString &pFileName,
 
 //==============================================================================
 
-CellmlFile::Version CellmlFile::version() const
+CellmlFile::Version CellmlFile::version()
 {
     // Return our version
 
-    return CellmlFile::version(mModel);
+    if (load())
+        return CellmlFile::version(mModel);
+    else
+        return CellmlFile::Unknown;
 }
 
 //==============================================================================

src/plugins/support/CellMLSupport/src/cellmlfile.h
@@ -119,14 +119,14 @@ public:
 
     QString importedFileContents(const QString &pImportedFileName) const;
 
-    QString cmetaId() const;
+    QString cmetaId();
     QString xmlBase();
 
     bool exportTo(const QString &pFileName, const Version &pVersion);
     bool exportTo(const QString &pFileName,
                   const QString &pUserDefinedFormatFileName);
 
-    Version version() const;
+    Version version();
 
     static Version version(iface::cellml_api::Model *pModel);
     static Version version(const QString &pFileName);