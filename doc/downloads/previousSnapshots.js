1e9b059a631f00035de5d82249d3fc7822287ac3
Alan Garny <agarny@hellix.com>
2016-05-30 11:08:18 +1200

    jQuery: upgraded to version 2.2.4 (closes #970).

--- doc/downloads/previousSnapshots.js
+++ doc/downloads/previousSnapshots.js
@@ -1,4 +1,30 @@
 var jsonData = { "versions": [
+                   { "major": 0, "minor": 0, "patch": 0, "day": 30, "month": 5, "year": 2016, "type": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "OS X", "supported": "Mac OS X 10.7 (Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ],
+                     "changes": [
+                       { "change": "<strong>General:</strong> added support for Ubuntu 16.04 LTS (Xenial Xerus; see issue <a href=\"https://github.com/opencor/opencor/issues/965\">#965</a>) and renamed the Viewer plugin to MathMLViewer (see issue <a href=\"https://github.com/opencor/opencor/issues/940\">#940</a>)." },
+                       { "change": "<strong>Physiome Model Repository window:</strong> properly handle JSON responses from PMR (see issue <a href=\"https://github.com/opencor/opencor/issues/930\">#930</a>). The plugin is now called PMRWindow (see issues <a href=\"https://github.com/opencor/opencor/issues/935\">#935</a> and <a href=\"https://github.com/opencor/opencor/issues/958\">#958</a>)." },
+                       { "change": "<strong>Single Cell view:</strong> fixed a problem with an infinite loop in modification prompting (see issue <a href=\"https://github.com/opencor/opencor/issues/941\">#941</a>). Fixed a problem with opening CellML models from SED-ML documents (see issue <a href=\"https://github.com/opencor/opencor/issues/942\">#942</a>)." },
+                       { "change": "<strong>Support:</strong> added a PMR web service plugin ((see issue <a href=\"https://github.com/opencor/opencor/issues/931\">#931</a>)." },
+                       { "change": "<strong>Third-party libraries:</strong> upgraded <a href=\"https://jquery.com/\">jQuery</a> to version 2.2.3 (see issue <a href=\"https://github.com/opencor/opencor/issues/934\">#934</a>), <a href=\"https://khan.github.io/KaTeX/\">KaTeX</a> to version 0.6.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/948\">#948</a>), <a href=\"https://libgit2.github.com/\">libgit2</a> to version 0.24.1 (see issue <a href=\"https://github.com/opencor/opencor/issues/936\">#936</a>), <a href=\"http://sbml.org/Software/libSBML/\">libSBML</a> to version 5.13.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/949\">#949</a>), <a href=\"http://www.llvm.org/\">LLVM</a> to version 3.8.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/913\">#913</a>), <a href=\"https://www.openssl.org/\">OpenSSL</a> to version 1.0.2h (Windows only; see issue <a href=\"https://github.com/opencor/opencor/issues/956\">#956</a>) and <a href=\"https://riverbankcomputing.com/software/qscintilla/intro\">QScintilla</a> to version 2.9.2 (see issue <a href=\"https://github.com/opencor/opencor/issues/947\">#947</a>)." }
+                     ]
+                   },
                    { "major": 0, "minor": 0, "patch": 0, "day": 31, "month": 3, "year": 2016, "type": 2,
                      "platforms": [
                        { "name": "Windows", "supported": "Windows 7 and later",
