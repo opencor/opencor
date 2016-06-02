6c43d5e8ad7975a6b13418d8c827b3a1176fb926
Alan Garny <agarny@hellix.com>
2016-06-02 16:13:11 +1200

    Single Cell view: slight improvement to the alignment of graph panels [ci skip].

--- src/plugins/simulation/SingleCellView/src/singlecellviewgraphpanelplotwidget.cpp
+++ src/plugins/simulation/SingleCellView/src/singlecellviewgraphpanelplotwidget.cpp
@@ -1400,49 +1400,54 @@ void SingleCellViewGraphPanelPlotWidget::removeNeighbor(SingleCellViewGraphPanel
 void SingleCellViewGraphPanelPlotWidget::alignWithNeighbors(const bool &pCanReplot,
                                                             const bool &pForceAlignment)
 {
-    // Align ourselves with our neighbours
+    // Align ourselves with our neighbours unless we don't have any in which
+    // case we just replot ourselves, if allowed
 
-    for (int i = 0; i < 2; ++i) {
-        // Note: we do the below twice because there are cases where it won't
-        //       work properly. Indeed, say that you have two graph panels. By
-        //       default, their Y axis will range from 0 to 1,000. Now, say that
-        //       you zoom out the first graph panel and end up with somewhat
-        //       wide labels for the Y axis, e.g. from -1,000,000 to 1,000.
-        //       While doing this, the second graph panel will have kept
-        //       aligning itself with the first graph panel, as expected. Now,
-        //       say that you reset the zoom of the first graph panel. At this
-        //       point, you would expect the Y axis to range from 0 to 1,000 and
-        //       it is the case, but we can see loads of empty space to the left
-        //       of the Y axis and this on both graph panels. However, that
-        //       empty space disappears if we do the below twice. It clearly has
-        //       something to do with the internals of QwtPlot, yet it's not
-        //       clear how to get it 'right' without doing the below twice...
+    if (mNeighbors.isEmpty()) {
+        if (pCanReplot)
+            replotNow();
+    } else {
+        for (int i = 0; i < 2; ++i) {
+            // Note: we do the below twice because there are cases where it may
+            //       not work properly. Indeed, say that we have two graph
+            //       panels, which by default will have their Y axis ranging
+            //       from 0 to 1,000. Now, say that we zoom out the first graph
+            //       panel and end up with somewhat wide Y labels, e.g. from
+            //       -1,000,000 to 1,000. While doing this, the second graph
+            //       panel will have kept aligning itself with the first graph
+            //       panel, as expected. Now, say that we reset the zoom of the
+            //       first graph panel. At this point, you would expect the Y
+            //       axis to range from 0 to 1,000 and it is the case, but we
+            //       can see loads of empty space to the left of the Y axis and
+            //       this on both graph panels. However, that empty space
+            //       disappears if we do the below twice...
 
-        SingleCellViewGraphPanelPlotWidgets selfPlusNeighbors = SingleCellViewGraphPanelPlotWidgets() << this << mNeighbors;
-        double oldMaxExtent = axisWidget(QwtPlot::yLeft)->scaleDraw()->minimumExtent();
-        double newMaxExtent = 0;
+            SingleCellViewGraphPanelPlotWidgets selfPlusNeighbors = SingleCellViewGraphPanelPlotWidgets() << this << mNeighbors;
+            double oldMaxExtent = axisWidget(QwtPlot::yLeft)->scaleDraw()->minimumExtent();
+            double newMaxExtent = 0;
 
-        foreach (SingleCellViewGraphPanelPlotWidget *plot, selfPlusNeighbors) {
-            QwtScaleWidget *scaleWidget = plot->axisWidget(QwtPlot::yLeft);
-            QwtScaleDraw *scaleDraw = scaleWidget->scaleDraw();
+            foreach (SingleCellViewGraphPanelPlotWidget *plot, selfPlusNeighbors) {
+                QwtScaleWidget *scaleWidget = plot->axisWidget(QwtPlot::yLeft);
+                QwtScaleDraw *scaleDraw = scaleWidget->scaleDraw();
 
-            scaleDraw->setMinimumExtent(0.0);
+                scaleDraw->setMinimumExtent(0.0);
 
-            double extent = scaleDraw->extent(scaleWidget->font());
+                double extent = scaleDraw->extent(scaleWidget->font());
 
-            if (extent > newMaxExtent)
-                newMaxExtent = extent;
-        }
+                if (extent > newMaxExtent)
+                    newMaxExtent = extent;
+            }
 
-        foreach (SingleCellViewGraphPanelPlotWidget *plot, selfPlusNeighbors) {
-            plot->axisWidget(QwtPlot::yLeft)->scaleDraw()->setMinimumExtent(newMaxExtent);
+            foreach (SingleCellViewGraphPanelPlotWidget *plot, selfPlusNeighbors) {
+                plot->axisWidget(QwtPlot::yLeft)->scaleDraw()->setMinimumExtent(newMaxExtent);
 
-            if (pCanReplot) {
-                if (pForceAlignment || (newMaxExtent != oldMaxExtent)) {
-                    plot->updateLayout();
-                    plot->replotNow();
-                } else if (plot == this) {
-                    replotNow();
+                if (pCanReplot) {
+                    if (pForceAlignment || (newMaxExtent != oldMaxExtent)) {
+                        plot->updateLayout();
+                        plot->replotNow();
+                    } else if (plot == this) {
+                        replotNow();
+                    }
                 }
             }
         }
