b8c03bd271c8bde84cc6ed806249f2a6031a81b5
Alan Garny <agarny@hellix.com>
2016-06-11 10:51:53 +1200

    Slight updates to the downloads page [ci skip].

--- doc/downloads/previousSnapshots.js
+++ doc/downloads/previousSnapshots.js
@@ -1,4 +1,30 @@
 var jsonData = { "versions": [
+                   { "major": 0, "minor": 0, "patch": 0, "day": 9, "month": 6, "year": 2016, "type": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "OS X", "supported": "Mac OS X 10.7 (Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ],
+                     "changes": [
+                       { "change": "<strong>General:</strong> we now build OpenCOR against <a href=\"https://www.qt.io/\">Qt</a> 5.5.1 (see issue <a href=\"https://github.com/opencor/opencor/issues/971\">#971</a>), which means that OpenCOR is now officially supported on Windows 10 (when it comes to Qt itself). Renamed the WebViewWidget to WebViewerWidget and moved it to its own plugin (WebViewer from the Core plugin; see issue <a href=\"https://github.com/opencor/opencor/issues/976\">#976</a>). Plugins' resources now specify the 'prefix' attribute to avoid name conflicts (see issue <a href=\"https://github.com/opencor/opencor/issues/981\">#981</a>). Centralised the Oxygen library images (see issue <a href=\"https://github.com/opencor/opencor/issues/982\">#982</a>)." },
+                       { "change": "<strong>CellML Text view:</strong> fixed a small problem with all the metadata not being serialised back (see issue <a href=\"https://github.com/opencor/opencor/issues/987\">#987</a>). Fixed a small problem with the serialisation of numbers that have an exponent (see issue <a href=\"https://github.com/opencor/opencor/issues/989\">#989</a>)." },
+                       { "change": "<strong>Single Cell view:</strong> improved the plotting of (running) simulation data when showing/hiding a graph, panning, scaling and zooming in/out (see issue <a href=\"https://github.com/opencor/opencor/issues/980\">#980</a>)." },
+                       { "change": "<strong>MathML viewer:</strong> fixed a small problem with numbers with an exponent not being properly rendered (see issue <a href=\"https://github.com/opencor/opencor/issues/988\">#988</a>)." },
+                       { "change": "<strong>Third-party libraries:</strong> upgraded <a href=\"https://jquery.com/\">jQuery</a> to version 2.2.4 (see issue <a href=\"https://github.com/opencor/opencor/issues/970\">#970</a>)." }
+                     ]
+                   },
                    { "major": 0, "minor": 0, "patch": 0, "day": 30, "month": 5, "year": 2016, "type": 2,
                      "platforms": [
                        { "name": "Windows", "supported": "Windows 7 and later",
