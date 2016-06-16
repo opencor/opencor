3c3634b108dbc27b19ce068ee9cf0339a72f1d55
Alan Garny <agarny@hellix.com>
2016-06-17 11:51:49 +1200

    Some work on merging and renaming some of our plugins (#1004).

---
+++ src/plugins/widget/GraphPanelWidget/src/graphpanelwidgetplugin.cpp
@@ -0,0 +1,51 @@
+/*******************************************************************************
+
+Copyright The University of Auckland
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*******************************************************************************/
+
+//==============================================================================
+// Graph Panel widget plugin
+//==============================================================================
+
+#include "graphpanelwidgetplugin.h"
+
+//==============================================================================
+
+namespace OpenCOR {
+namespace GraphPanelWidget {
+
+//==============================================================================
+
+PLUGININFO_FUNC GraphPanelWidgetPluginInfo()
+{
+    Descriptions descriptions;
+
+    descriptions.insert("en", QString::fromUtf8("a plugin to plot graphs."));
+    descriptions.insert("fr", QString::fromUtf8("une extension pour afficher des graphes."));
+
+    return new PluginInfo("Widget", false, false,
+                          QStringList() << "Core",
+                          descriptions);
+}
+
+//==============================================================================
+
+}   // namespace GraphPanelWidget
+}   // namespace OpenCOR
+
+//==============================================================================
+// End of file
+//==============================================================================
