e33dcf694cf3a5f19cc89977754bb3961816f3e1
Alan Garny <agarny@hellix.com>
2016-06-18 12:10:06 +1200

    Some minor cleaning up [ci skip].

--- src/plugins/widget/EditorWidget/i18n/EditorWidget_fr.ts
+++ src/plugins/widget/EditorWidget/i18n/EditorWidget_fr.ts
@@ -2,7 +2,7 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR" sourcelanguage="en_GB">
 <context>
-    <name>EditorFindReplaceWidget</name>
+    <name>EditorWidgetFindReplaceWidget</name>
     <message>
         <source>Find:</source>
         <translation>Trouver :</translation>
@@ -34,41 +34,6 @@
     <message>
         <source>Find Next</source>
         <translation>Trouver Suivant</translation>
-    </message>
-</context>
-<context>
-    <name>OpenCOR::EditorWidget::EditorFindReplaceWidget</name>
-    <message>
-        <source>Case Sensitive</source>
-        <translation>Sensible À Différence Majuscules-Minuscules</translation>
-    </message>
-    <message>
-        <source>The search is case sensitive</source>
-        <translation>La recherche est sensible à la différence majuscules-minuscules</translation>
-    </message>
-    <message>
-        <source>Whole Words Only</source>
-        <translation>Mots Entiers Seulement</translation>
-    </message>
-    <message>
-        <source>The search is done on whole words only</source>
-        <translation>La recherche est faite sur des mots entiers seulement</translation>
-    </message>
-    <message>
-        <source>Regular Expression</source>
-        <translation>Expression Régulière</translation>
-    </message>
-    <message>
-        <source>The search uses a regular expression</source>
-        <translation>La recherche utilise une expression régulière</translation>
-    </message>
-    <message>
-        <source>Clear Text</source>
-        <translation>Effacer Texte</translation>
-    </message>
-    <message>
-        <source>Clear the text</source>
-        <translation>Effacer le texte</translation>
     </message>
 </context>
 <context>
@@ -108,6 +73,41 @@
     <message>
         <source>Fatal</source>
         <translation>Fatal</translation>
+    </message>
+</context>
+<context>
+    <name>OpenCOR::EditorWidget::EditorWidgetFindReplaceWidget</name>
+    <message>
+        <source>Case Sensitive</source>
+        <translation>Sensible À Différence Majuscules-Minuscules</translation>
+    </message>
+    <message>
+        <source>The search is case sensitive</source>
+        <translation>La recherche est sensible à la différence majuscules-minuscules</translation>
+    </message>
+    <message>
+        <source>Whole Words Only</source>
+        <translation>Mots Entiers Seulement</translation>
+    </message>
+    <message>
+        <source>The search is done on whole words only</source>
+        <translation>La recherche est faite sur des mots entiers seulement</translation>
+    </message>
+    <message>
+        <source>Regular Expression</source>
+        <translation>Expression Régulière</translation>
+    </message>
+    <message>
+        <source>The search uses a regular expression</source>
+        <translation>La recherche utilise une expression régulière</translation>
+    </message>
+    <message>
+        <source>Clear Text</source>
+        <translation>Effacer Texte</translation>
+    </message>
+    <message>
+        <source>Clear the text</source>
+        <translation>Effacer le texte</translation>
     </message>
 </context>
 </TS>
