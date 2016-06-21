ff686df7aa6e5542e9d5124e730f6b8f940aefcb
Alan Garny <agarny@hellix.com>
2016-06-21 20:49:08 +1200

    Some minor cleaning up [ci skip].

--- src/plugins/tools/CellMLTools/tests/tests.cpp
+++ src/plugins/tools/CellMLTools/tests/tests.cpp
@@ -54,14 +54,14 @@ void Tests::exportTests()
 {
     // Try to export a CellML 1.0 file to CellML 1.0
 
-    QString fileName = OpenCOR::fileName("src/plugins/tools/CellMLTools/tests/data/noble_model_1962.cellml");
+    QString fileName = OpenCOR::fileName("models/noble_model_1962.cellml");
 
     QCOMPARE(OpenCOR::runCli(QStringList() << "-c" << "CellMLTools::export" << fileName << "cellml_1_0"),
              QStringList() << "The file is already a CellML 1.0 file." << QString());
 
     // Export a CellML 1.1 file to CellML 1.0
 
-    fileName = OpenCOR::fileName("src/plugins/tools/CellMLTools/tests/data/experiments/periodic-stimulus.xml");
+    fileName = OpenCOR::fileName("doc/developer/functionalTests/res/cellml/cellml_1_1/experiments/periodic-stimulus.xml");
 
     QCOMPARE(OpenCOR::runCli(QStringList() << "-c" << "CellMLTools::export" << fileName << "cellml_1_0"),
              OpenCOR::fileContents(OpenCOR::fileName("src/plugins/tools/CellMLTools/tests/data/cellml_1_0_export.out")) << QString());
