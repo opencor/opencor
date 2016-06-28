38b8e1ab4cea1b68e01f9a1d9a621492c2d19ae9
David Brooks <dave@bcs.co.nz>
2016-06-28 20:33:53 +0100

    Some work on PMRWorkspaces plugin (#969)
    Always select a row after a single click in it.

--- src/plugins/organisation/PMRWorkspaces/src/pmrworkspaceswidget.cpp
+++ src/plugins/organisation/PMRWorkspaces/src/pmrworkspaceswidget.cpp
@@ -534,30 +534,37 @@ void PmrWorkspacesWidget::mouseMoveEvent(QMouseEvent *event)
 void PmrWorkspacesWidget::mousePressEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton) {
-        auto webElement = page()->mainFrame()->hitTestContent(event->pos()).element();
-        while (!webElement.isNull() && webElement.tagName() != "A" && webElement.tagName() != "TR")
-            webElement = webElement.parent();
+        // Find the containg row and highlight it
 
-        if (!webElement.isNull()) {
-            if (webElement.tagName() == "A") {
-                if (webElement.toPlainText().isEmpty()) {
-                    QStringList linkList = webElement.attribute("href").split("|");
-                    if (!linkList[0].compare("clone")) {
-                        cloneWorkspace(linkList[1]);
-                    }
-                }
-                else {
-                    // Text link clicked, e.g. to open a file
+        auto trElement = page()->mainFrame()->hitTestContent(event->pos()).element();
+        while (!trElement.isNull() && trElement.tagName() != "TR")
+            trElement = trElement.parent();
+
+        if (!trElement.isNull()) {
+            QString link = trElement.attribute("id");
+            if (!link.isEmpty()) {
+                if (trElement.hasClass("workspace")
+                 || trElement.hasClass("folder"))
+                    expandHtmlTree(link);
+                setSelected(link);
+            }
+        }
+
+        // Check if an `<a>` element has been clicked
+
+        auto aElement = page()->mainFrame()->hitTestContent(event->pos()).element();
+        while (!aElement.isNull() && aElement.tagName() != "A")
+            aElement = aElement.parent();
+
+        if (!aElement.isNull()) {
+            if (aElement.toPlainText().isEmpty()) {
+                QStringList linkList = aElement.attribute("href").split("|");
+                if (!linkList[0].compare("clone")) {
+                    cloneWorkspace(linkList[1]);
                 }
             }
             else {
-                QString link = webElement.attribute("id");
-                if (!link.isEmpty()) {
-                    if (webElement.hasClass("workspace")
-                     || webElement.hasClass("folder"))
-                        expandHtmlTree(link);
-                    setSelected(link);
-                }
+                // Text link clicked, e.g. to open a file
             }
         }
     }
