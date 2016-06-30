a1f344beb189c253363c5218800820c3abc6ed77
David Brooks <dave@bcs.co.nz>
2016-06-30 11:02:15 +0100

    Minor tidy up.

--- src/plugins/organisation/PMRWorkspaces/src/pmrworkspaceswindow.cpp
+++ src/plugins/organisation/PMRWorkspaces/src/pmrworkspaceswindow.cpp
@@ -365,7 +365,7 @@ void PmrWorkspacesWindow::on_actionUnauthenticate_triggered()
     // Log off PMR
 
     if (QMessageBox::question(this, tr("OpenCOR"),
-                              tr("Log off the Physiome Model Repository")) == QMessageBox::Ok)
+                              tr("Log off the Physiome Model Repository?")) == QMessageBox::Ok)
         mPmrRepository->authenticate(false);
 }
 
