5bca57a832aa32fe4fb71f2c6bb11925ebaca9f6
Alan Garny <agarny@hellix.com>
2016-07-02 01:50:56 +0200

    Slight updates to our downloads pages [ci skip].

--- doc/downloads/previousSnapshots.js
+++ doc/downloads/previousSnapshots.js
@@ -1,4 +1,33 @@
 var jsonData = { "versions": [
+                   { "major": 0, "minor": 0, "patch": 0, "day": 1, "month": 7, "year": 2016, "type": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "OS X", "supported": "Mac OS X 10.7 (Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ],
+                     "changes": [
+                       { "change": "<strong>General:</strong> merged and renamed some of our plugins (see issue <a href=\"https://github.com/opencor/opencor/issues/1004\">#1004</a>). Upgraded to Qt 5.6.1 LTS + Qt WebKit (see issue <a href=\"https://github.com/opencor/opencor/issues/1015\">#1015</a>). A new file is not automatically considered as modified anymore (see issue <a href=\"https://github.com/opencor/opencor/issues/1020\">#1020</a>)." },
+                       { "change": "<strong>CellML support:</strong> can now detect a variable of integration no matter where it is defined in a model (see issue <a href=\"https://github.com/opencor/opencor/issues/995\">#995</a>)." },
+                       { "change": "<strong>Data stores:</strong> data export is now done in its own thread (ee issue <a href=\"https://github.com/opencor/opencor/issues/849\">#849</a>)." },
+                       { "change": "<strong>Text-based editing views:</strong> fixed a small problem with losing the focus and not being able to track changes to a file after having reloaded another file that depends on it (see issue <a href=\"https://github.com/opencor/opencor/issues/996\">#996</a>)." },
+                       { "change": "<strong>CellML Text view:</strong> allow the import / export of the CellML Text format from / to CellML from the command line (see issue <a href=\"https://github.com/opencor/opencor/issues/992\">#992</a>). Allow the user to save the contents of the view to a text file if it cannot be saved as a CellML file (see issue <a href=\"https://github.com/opencor/opencor/issues/998\">#998</a>). Allow comments that contain -- (double dash) in them (see issue <a href=\"https://github.com/opencor/opencor/issues/1019\">#1019</a>)." },
+                       { "change": "<strong>Raw CellML view:</strong> fixed a small problem with OpenCOR crashing when trying to validate a malformed CellML file (see issue <a href=\"https://github.com/opencor/opencor/issues/1018\">#1018</a>)." },
+                       { "change": "<strong>Single Cell view:</strong> now use better colours/symbols in the Parameters window (see issue <a href=\"https://github.com/opencor/opencor/issues/634\">#634</a>). Can now specify the minimum/maximum values of the axes of a graph panel (see issue <a href=\"https://github.com/opencor/opencor/issues/997\">#997</a>). Can now synchronise the X and/or Y axes of all the graph panels (see issue <a href=\"https://github.com/opencor/opencor/issues/1000\">#1000</a>). The plotting of several graph panels is now much smoother (see issue <a href=\"https://github.com/opencor/opencor/issues/1011\">#1011</a>). Fixed a problem with some model parameters not being listed or being listed in the wrong component (see issues <a href=\"https://github.com/opencor/opencor/issues/1014\">#1014</a>, <a href=\"https://github.com/opencor/opencor/issues/1016\">#1016</a> and <a href=\"https://github.com/opencor/opencor/issues/1025\">#1025</a>). Improved the loading/reloading of a file (see issue <a href=\"https://github.com/opencor/opencor/issues/1017\">#1017</a>). Fixed a small problem with plotting a (somewhat) flat graph results in the Y ranges being incorrect (see issue <a href=\"https://github.com/opencor/opencor/issues/1023\">#1023</a>). A (short) simulation now look smooth again (see issue <a href=\"https://github.com/opencor/opencor/issues/1024\">#1024</a>)." },
+                       { "change": "<strong>Third-party libraries:</strong> applied an official patch to <a href=\"https://riverbankcomputing.com/software/qscintilla/intro\">QScintilla</a> 2.9.2 following a (Mac) OS X issue (see issue <a href=\"https://github.com/opencor/opencor/issues/994\">#994</a>). Upgraded <a href=\"https://jquery.com/\">jQuery</a> to version 3.0.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/1001\">#1001</a>) and <a href=\"http://qwt.sourceforge.net/\">Qwt</a> to version 6.1.3 (see issue <a href=\"https://github.com/opencor/opencor/issues/1002\">#1002</a>)." }
+                     ]
+                   },
                    { "major": 0, "minor": 0, "patch": 0, "day": 9, "month": 6, "year": 2016, "type": 2,
                      "platforms": [
                        { "name": "Windows", "supported": "Windows 7 and later",
