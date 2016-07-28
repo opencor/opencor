47bbefcdf22babe1c91ad97a24cee87005a957f1
Alan Garny <agarny@hellix.com>
2016-07-28 15:39:01 +0200

    Central widget: slight improvements to the way we handle changes to a file [ci skip].

--- doc/downloads/index.js
+++ doc/downloads/index.js
@@ -39,7 +39,8 @@ var jsonData = { "versions": [
                        }
                      ],
                      "changes": [
-                       { "change": "<strong>File manager:</strong> only check files when OpenCOR is really active (see issue <a href=\"https://github.com/opencor/opencor/issues/1048\">#1048</a>)." }
+                       { "change": "<strong>File manager:</strong> only check files when OpenCOR is really active (see issue <a href=\"https://github.com/opencor/opencor/issues/1048\">#1048</a>)." },
+                       { "change": "<strong>Text-based editing views:</strong> fixed a small problem resulting in the incorrect tracking of the modified state of a file (see issue <a href=\"https://github.com/opencor/opencor/issues/1050\">#1050</a>)." }
                      ]
                    },
                    { "major": 0, "minor": 4, "patch": 0, "day": 29, "month": 4, "year": 2015, "type": 0,
