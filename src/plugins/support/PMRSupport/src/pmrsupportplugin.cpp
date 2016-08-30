47849ee9f4997e09ecd9fc955dab53f5ee2428b4
Alan Garny <agarny@hellix.com>
2016-08-30 21:24:01 +0200

    Some work on updating the documentation in preparation for the 0.5 release (#1047).

--- src/plugins/support/PMRSupport/src/pmrsupportplugin.cpp
+++ src/plugins/support/PMRSupport/src/pmrsupportplugin.cpp
@@ -33,8 +33,8 @@ PLUGININFO_FUNC PMRSupportPluginInfo()
 {
     Descriptions descriptions;
 
-    descriptions.insert("en", QString::fromUtf8("a plugin to support <a href=\"https://models.physiomeproject.org/\">PMR</a>."));
-    descriptions.insert("fr", QString::fromUtf8("une extension pour supporter <a href=\"https://models.physiomeproject.org/\">PMR</a>."));
+    descriptions.insert("en", QString::fromUtf8("a plugin to support the <a href=\"https://models.physiomeproject.org/\">Physiome Model Repository</a>."));
+    descriptions.insert("fr", QString::fromUtf8("une extension pour supporter le <a href=\"https://models.physiomeproject.org/\">Répertoire de Modèles Physiome</a>."));
 
     return new PluginInfo("Support", false, false,
                           QStringList() << "Core" << "libgit2" << "zlib",
