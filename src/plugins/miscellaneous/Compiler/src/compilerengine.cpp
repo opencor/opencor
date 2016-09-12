698754454ca33b121f5e068038c016daf30ddf5b
Alan Garny <agarny@hellix.com>
2016-09-12 23:36:34 +0200

    LLVM: some work on upgrading to version 3.9.0 (#1099).

--- src/plugins/miscellaneous/Compiler/src/compilerengine.cpp
+++ src/plugins/miscellaneous/Compiler/src/compilerengine.cpp
@@ -184,8 +184,9 @@ bool CompilerEngine::compileCode(const QString &pCode)
     compilationArguments.push_back("clang");
     compilationArguments.push_back("-fsyntax-only");
 //---ISSUE1099---(BEGIN)
-// There is a bug in LLVM 3.9 that crashes the debug version of OpenCOR when
-// using -O3 when, hence we only use -O3 when using a release versio of OpenCOR
+// There is a bug in LLVM 3.9 that crashes the debug version of OpenCOR on
+// Windows when using -O3, hence we only use -O3 when using the release version
+// of OpenCOR...
 #ifndef QT_DEBUG
 //---ISSUE1099---(END)
     compilationArguments.push_back("-O3");
