a370b1968a7f23ac26080ec7d07fed3ae1e89172
Alan Garny <agarny@hellix.com>
2016-09-30 16:54:26 +0200

    Some work on adding support for ApiNATOMY (#1115).

---
+++ src/plugins/miscellaneous/WebBrowserWindow/i18n/WebBrowserWindow_fr.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_GB">
+<context>
+    <name>OpenCOR::WebBrowserWindow::WebBrowserWindowPlugin</name>
+    <message>
+        <source>Web Browser</source>
+        <translation>Navigateur Web</translation>
+    </message>
+    <message>
+        <source>Show/hide the Web Browser window</source>
+        <translation>Montrer/cacher la fenêtre du Navigateur Web</translation>
+    </message>
+</context>
+<context>
+    <name>WebBrowserWindowWindow</name>
+    <message>
+        <source>Web Browser</source>
+        <translation>Navigateur Web</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Rafraîchir</translation>
+    </message>
+    <message>
+        <source>Refresh the Web page</source>
+        <translation>Rafraîchir la page Web</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/miscellaneous/WebBrowserWindow/src/webbrowserwindowwindow.cpp
@@ -0,0 +1,110 @@
+/*******************************************************************************
+
+Copyright The University of Auckland
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*******************************************************************************/
+
+//==============================================================================
+// Web Browser window
+//==============================================================================
+
+#include "borderedwidget.h"
+#include "webbrowserwindowwidget.h"
+#include "webbrowserwindowwindow.h"
+
+//==============================================================================
+
+#include "ui_webbrowserwindowwindow.h"
+
+//==============================================================================
+
+namespace OpenCOR {
+namespace WebBrowserWindow {
+
+//==============================================================================
+
+WebBrowserWindowWindow::WebBrowserWindowWindow(QWidget *pParent) :
+    Core::WindowWidget(pParent),
+    mGui(new Ui::WebBrowserWindowWindow)
+{
+    // Set up the GUI
+
+    mGui->setupUi(this);
+
+#ifdef Q_OS_MAC
+    mGui->urlValue->setAttribute(Qt::WA_MacShowFocusRect, false);
+    // Note: the above removes the focus border since it messes up the look of
+    //       our URL value widget...
+#endif
+
+    // Make the URL value our focus proxy
+
+    setFocusProxy(mGui->urlValue);
+
+    // Create and add the web browser widget
+
+    mWebBrowserWidget = new WebBrowserWindowWidget(this);
+
+#if defined(Q_OS_WIN) || defined(Q_OS_LINUX)
+    mGui->dockWidgetContents->layout()->addWidget(new Core::BorderedWidget(mWebBrowserWidget,
+                                                                           true, true, true, true));
+#elif defined(Q_OS_MAC)
+    mGui->dockWidgetContents->layout()->addWidget(new Core::BorderedWidget(mWebBrowserWidget,
+                                                                           true, false, false, false));
+#else
+    #error Unsupported platform
+#endif
+
+    // Some further initialisations that are done as part of retranslating the
+    // GUI (so that they can be updated when changing languages)
+
+    retranslateUi();
+}
+
+//==============================================================================
+
+WebBrowserWindowWindow::~WebBrowserWindowWindow()
+{
+    // Delete the GUI
+
+    delete mGui;
+}
+
+//==============================================================================
+
+void WebBrowserWindowWindow::retranslateUi()
+{
+    // Retranslate the whole window
+
+    mGui->retranslateUi(this);
+}
+
+//==============================================================================
+
+void WebBrowserWindowWindow::resizeEvent(QResizeEvent *pEvent)
+{
+    // Default handling of the event
+
+    Core::WindowWidget::resizeEvent(pEvent);
+}
+
+//==============================================================================
+
+}   // namespace WebBrowserWindow
+}   // namespace OpenCOR
+
+//==============================================================================
+// End of file
+//==============================================================================
