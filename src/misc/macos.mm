0791b21e48410661b112e485649e2460f505947c
Alan Garny <agarny@hellix.com>
2016-10-20 10:09:30 +0200

    macOS: some work on getting rid of some macOS specific menu items (#1137).
    In case we want to build OpenCOR on earlier versions of macOS (i.e.
    before 10.12 / Sierra), as is the case on Travis CI.

--- src/misc/macos.mm
+++ src/misc/macos.mm
@@ -40,11 +40,13 @@ void removeMacosSpecificMenuItems()
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"NSDisabledDictationMenuItem"];
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"NSDisabledCharacterPaletteMenuItem"];
 
+#ifdef AVAILABLE_MAC_OS_X_VERSION_10_12_AND_LATER
     // Remove (don't allow) the "Show Tab Bar" menu item from the "View" menu,
     // if supported
 
     if ([NSWindow respondsToSelector:@selector(allowsAutomaticWindowTabbing)])
         NSWindow.allowsAutomaticWindowTabbing = NO;
+#endif
 
     // Remove (don't have) the "Enter Full Screen" menu item from the "View"
     // menu
