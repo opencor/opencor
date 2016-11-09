6e578f49ac30deddf352eadec245962193b1469e
Alan Garny <agarny@hellix.com>
2016-11-09 17:15:10 +0100

    Slight updates to the downloads pages [ci skip].

--- doc/downloads/index.js
+++ doc/downloads/index.js
@@ -37,9 +37,7 @@ var jsonData = { "versions": [
                        }
                      ],
                      "changes": [
-                       { "change": "<strong>General:</strong> improved the translation of Qt-related messages (see issue <a href=\"https://github.com/opencor/opencor/issues/1135\">#1135</a>). Improved support for macOS (see issues <a href=\"https://github.com/opencor/opencor/issues/1134\">#1134</a> and <a href=\"https://github.com/opencor/opencor/issues/1137\">#1137</a>). Now have a Preferences dialog so the user can specify his preferences for OpenCOR and its plugins (see issue <a href=\"https://github.com/opencor/opencor/issues/193\">#193</a>)." },
-                       { "change": "<strong>Data store:</strong> made it possible to select a group of data (see issue <a href=\"https://github.com/opencor/opencor/issues/1143\">#1143</a>)." },
-                       { "change": "<strong>Web Browser window:</strong> added a Web browser like window (see issue <a href=\"https://github.com/opencor/opencor/issues/1115\">#1115</a>)." }
+                       { "change": "<strong>Third-party libraries:</strong> upgraded <a href=\"https://libgit2.github.com/\">libgit2</a> to version 0.24.3 (see issue <a href=\"https://github.com/opencor/opencor/issues/1145\">#1145</a>)." }
                      ]
                    },
                    { "major": 0, "minor": 4, "patch": 1, "day": 20, "month": 5, "year": 2015, "type": 0,
--- doc/downloads/previousSnapshots.js
+++ doc/downloads/previousSnapshots.js
@@ -1,3 +1,27 @@
 var jsonData = { "versions": [
+                   { "major": 0, "minor": 0, "patch": 0, "day": 9, "month": 11, "year": 2016, "type": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.8 (Mountain Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ],
+                     "changes": [
+                       { "change": "<strong>General:</strong> improved the translation of Qt-related messages (see issue <a href=\"https://github.com/opencor/opencor/issues/1135\">#1135</a>). Improved support for macOS (see issues <a href=\"https://github.com/opencor/opencor/issues/1134\">#1134</a> and <a href=\"https://github.com/opencor/opencor/issues/1137\">#1137</a>). Now have a Preferences dialog so the user can specify his preferences for OpenCOR and its plugins (see issue <a href=\"https://github.com/opencor/opencor/issues/193\">#193</a>)." },
+                       { "change": "<strong>Data store:</strong> made it possible to select a group of data (see issue <a href=\"https://github.com/opencor/opencor/issues/1143\">#1143</a>)." },
+                       { "change": "<strong>Web Browser window:</strong> added a Web browser like window (see issue <a href=\"https://github.com/opencor/opencor/issues/1115\">#1115</a>)." }
+                     ]
+                   },
                  ]
                };
