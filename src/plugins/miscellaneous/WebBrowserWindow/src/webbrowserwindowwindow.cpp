1e9d3da280bc3d7a0dd493bed76866c5fabe4ed9
Alan Garny <agarny@hellix.com>
2016-11-16 18:33:49 +0100

    Some minor cleaning up.

--- src/plugins/miscellaneous/WebBrowserWindow/src/webbrowserwindowwindow.cpp
+++ src/plugins/miscellaneous/WebBrowserWindow/src/webbrowserwindowwindow.cpp
@@ -120,7 +120,7 @@ WebBrowserWindowWindow::WebBrowserWindowWindow(QWidget *pParent) :
     mWebBrowserWindowWidget->settings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, true);
 
 #if defined(Q_OS_WIN) || defined(Q_OS_LINUX)
-    mGui->layout->addWidget(new Core::BorderedWidget(mWebBrowserWidget,
+    mGui->layout->addWidget(new Core::BorderedWidget(mWebBrowserWindowWidget,
                                                      true, true, true, true));
 #elif defined(Q_OS_MAC)
     mGui->layout->addWidget(new Core::BorderedWidget(mWebBrowserWindowWidget,
@@ -374,7 +374,7 @@ void WebBrowserWindowWindow::on_actionInspect_triggered()
 {
     // Inspect the current page
 
-    mWebBrowserWindowWidget->pageAction(QWebPage::InspectElement)->trigger();
+    mWebBrowserWindowWidget->triggerPageAction(QWebPage::InspectElement);
 }
 
 //==============================================================================
