88eedd37bc0822499351d97d6fbff8cc5207c945
Alan Garny <agarny@hellix.com>
2017-01-11 20:14:06 +0100

    Some minor cleaning up.

--- src/plugins/organisation/PMRWindow/src/pmrwindowwidget.cpp
+++ src/plugins/organisation/PMRWindow/src/pmrwindowwidget.cpp
@@ -44,7 +44,8 @@ namespace PMRWindow {
 
 //==============================================================================
 
-PmrWindowItem::PmrWindowItem(const Type &pType, const QString &pText, const QString &pUrl) :
+PmrWindowItem::PmrWindowItem(const Type &pType, const QString &pText,
+                             const QString &pUrl) :
     QStandardItem(pText),
     mType(pType),
     mUrl(pUrl)
