4f8abd572b4e6f1578df8d838d3ff603242f96ef
Alan Garny <agarny@hellix.com>
2017-01-31 18:26:52 +0100

    PMR support: reordered the French translations [ci skip].

--- src/plugins/support/PMRSupport/i18n/PMRSupport_fr.ts
+++ src/plugins/support/PMRSupport/i18n/PMRSupport_fr.ts
@@ -4,16 +4,12 @@
 <context>
     <name>OpenCOR::PMRSupport::PmrWebService</name>
     <message>
-        <source>&lt;strong&gt;Note:&lt;/strong&gt; you might want to email &lt;a href=&quot;mailto: help@physiomeproject.org&quot;&gt;help@physiomeproject.org&lt;/a&gt; and ask why this is the case.</source>
-        <translation>&lt;strong&gt;Note :&lt;/strong&gt; vous pourriez vouloir envoyer un message à &lt;a href=&quot;mailto: help@physiomeproject.org&quot;&gt;help@physiomeproject.org&lt;/a&gt; et demander pourquoi c&apos;est le cas.</translation>
+        <source>** Unknown name **</source>
+        <translation>** Nom inconnu **</translation>
     </message>
     <message>
-        <source>No exposure files could be found for %1.</source>
-        <translation>Aucuns fichiers d&apos;exposition n&apos;ont pu être trouvés pour %1.</translation>
-    </message>
-    <message>
-        <source>No exposure file information could be found for %1.</source>
-        <translation>Aucunes informations de fichier d&apos;exposition n&apos;ont pu être trouvées pour %1.</translation>
+        <source>The workspace for %1 is already cloned in %2.</source>
+        <translation>L&apos;espace de travail pour %1 est déjà cloné à %2.</translation>
     </message>
     <message>
         <source>The workspace for %1 is not a Git repository.</source>
@@ -24,24 +20,28 @@
         <translation>Aucunes informations d&apos;espace de travail n&apos;ont pu être trouvées pour %1.</translation>
     </message>
     <message>
-        <source>** Unknown name **</source>
-        <translation>** Nom inconnu **</translation>
+        <source>Select Empty Directory</source>
+        <translation>Sélectionner Répertoire Vide</translation>
+    </message>
+    <message>
+        <source>No exposure file information could be found for %1.</source>
+        <translation>Aucunes informations de fichier d&apos;exposition n&apos;ont pu être trouvées pour %1.</translation>
+    </message>
+    <message>
+        <source>&lt;strong&gt;Note:&lt;/strong&gt; you might want to email &lt;a href=&quot;mailto: help@physiomeproject.org&quot;&gt;help@physiomeproject.org&lt;/a&gt; and ask why this is the case.</source>
+        <translation>&lt;strong&gt;Note :&lt;/strong&gt; vous pourriez vouloir envoyer un message à &lt;a href=&quot;mailto: help@physiomeproject.org&quot;&gt;help@physiomeproject.org&lt;/a&gt; et demander pourquoi c&apos;est le cas.</translation>
+    </message>
+    <message>
+        <source>Access to %1 is forbidden.</source>
+        <translation>Accès à %1 est interdit.</translation>
     </message>
     <message>
         <source>No workspace could be found for %1.</source>
         <translation>Aucun espace de travail n&apos;a pu être trouvé pour %1.</translation>
     </message>
     <message>
-        <source>Select Empty Directory</source>
-        <translation>Sélectionner Répertoire Vide</translation>
-    </message>
-    <message>
-        <source>The workspace for %1 is already cloned in %2.</source>
-        <translation>L&apos;espace de travail pour %1 est déjà cloné à %2.</translation>
-    </message>
-    <message>
-        <source>Access to %1 is forbidden.</source>
-        <translation>Accès à %1 est interdit.</translation>
+        <source>No exposure files could be found for %1.</source>
+        <translation>Aucuns fichiers d&apos;exposition n&apos;ont pu être trouvés pour %1.</translation>
     </message>
 </context>
 <context>
@@ -58,12 +58,28 @@
         <translation>Une erreur s&apos;est produite lors du clonage de l&apos;espace de travail.</translation>
     </message>
     <message>
+        <source>An error occurred while trying to commit to the workspace.</source>
+        <translation>Une erreur s&apos;est produite lors de la validation de l&apos;espace de travail.</translation>
+    </message>
+    <message>
+        <source>An error occurred while trying to commit the merge.</source>
+        <translation>Une erreur s&apos;est produite lors de la validation de la fusion.</translation>
+    </message>
+    <message>
         <source>An error occurred while trying to open the workspace.</source>
         <translation>Une erreur s&apos;est produite lors de l&apos;ouverture de l&apos;espace de travail.</translation>
     </message>
     <message>
-        <source>An error occurred while trying to push the workspace.</source>
-        <translation>Une erreur s&apos;est produite lors du poussage de l&apos;espace de travail.</translation>
+        <source>An error occurred while trying to get the status of %1.</source>
+        <translation>Une erreur s&apos;est produite lors de l&apos;obtention de l&apos;état de %1.</translation>
+    </message>
+    <message>
+        <source>An error occurred while trying to get the remote status of %1.</source>
+        <translation>Une erreur s&apos;est produite lors de l&apos;obtention de l&apos;état distant de %1.</translation>
+    </message>
+    <message>
+        <source>An error occurred while trying to stage %1.</source>
+        <translation>Une erreur s&apos;est produite lors de l&apos;indexation de %1.</translation>
     </message>
     <message>
         <source>An error occurred while trying to fetch the remote workspace.</source>
@@ -74,32 +90,12 @@
         <translation>Une erreur s&apos;est produite lors de la fusion de l&apos;espace de travail.</translation>
     </message>
     <message>
-        <source>The following files have conflicts:</source>
-        <translation>Les fichiers suivants ont des conflits :</translation>
-    </message>
-    <message>
-        <source>An error occurred while trying to commit the merge.</source>
-        <translation>Une erreur s&apos;est produite lors de la validation de la fusion.</translation>
-    </message>
-    <message>
-        <source>An error occurred while trying to commit to the workspace.</source>
-        <translation>Une erreur s&apos;est produite lors de la validation de l&apos;espace de travail.</translation>
-    </message>
-    <message>
-        <source>An error occurred while trying to get the remote status of %1.</source>
-        <translation>Une erreur s&apos;est produite lors de l&apos;obtention de l&apos;état distant de %1.</translation>
-    </message>
-    <message>
-        <source>An error occurred while trying to get the status of %1.</source>
-        <translation>Une erreur s&apos;est produite lors de l&apos;obtention de l&apos;état de %1.</translation>
-    </message>
-    <message>
-        <source>An error occurred while trying to stage %1.</source>
-        <translation>Une erreur s&apos;est produite lors de l&apos;indexation de %1.</translation>
-    </message>
-    <message>
         <source>The following file has conflicts:</source>
         <translation>Le fichier suivant a des conflits :</translation>
+    </message>
+    <message>
+        <source>The following files have conflicts:</source>
+        <translation>Les fichiers suivants ont des conflits :</translation>
     </message>
     <message>
         <source> - %1.</source>
@@ -112,6 +108,10 @@
     <message>
         <source> - %1;</source>
         <translation> - %1 ;</translation>
+    </message>
+    <message>
+        <source>An error occurred while trying to push the workspace.</source>
+        <translation>Une erreur s&apos;est produite lors du poussage de l&apos;espace de travail.</translation>
     </message>
 </context>
 </TS>
