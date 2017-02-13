85679ed0d8f42d28ad30cf3b1ef4d2c292cf85d7
Alan Garny <agarny@hellix.com>
2017-02-13 15:57:51 +0100

    PMR Workspaces window: some minor refactoring [ci skip].
    One less file to compile as such.

--- src/plugins/organisation/PMRWorkspacesWindow/src/pmrworkspaceswindowwidget.h
+++ src/plugins/organisation/PMRWorkspacesWindow/src/pmrworkspaceswindowwidget.h
@@ -25,13 +25,12 @@ limitations under the License.
 //==============================================================================
 
 #include "pmrworkspace.h"
-#include "pmrworkspaceswindowitem.h"
 #include "widget.h"
 
 //==============================================================================
 
 #include <QMap>
-#include <QStandardItemModel>
+#include <QStandardItem>
 
 //==============================================================================
 
@@ -57,6 +56,65 @@ namespace PMRSupport {
 //==============================================================================
 
 namespace PMRWorkspacesWindow {
+
+//==============================================================================
+
+class PmrWorkspacesWindowItem : public QStandardItem
+{
+public:
+    enum Type {
+        OwnedWorkspace = QStandardItem::UserType,
+        Workspace      = QStandardItem::UserType+1,
+        Folder         = QStandardItem::UserType+2,
+        File           = QStandardItem::UserType+3
+    };
+
+    explicit PmrWorkspacesWindowItem(const Type &pType,
+                                     PMRSupport::PmrWorkspace *pWorkspace,
+                                     const QIcon &pCollapsedIcon,
+                                     const QIcon &pExpandedIcon);
+    explicit PmrWorkspacesWindowItem(const Type &pType,
+                                     PMRSupport::PmrWorkspace *pWorkspace,
+                                     PMRSupport::PmrWorkspaceFileNode *pFileNode,
+                                     const QIcon &pCollapsedIcon,
+                                     const QIcon &pExpandedIcon);
+    explicit PmrWorkspacesWindowItem(const Type &pType,
+                                     PMRSupport::PmrWorkspace *pWorkspace,
+                                     PMRSupport::PmrWorkspaceFileNode *pFileNode,
+                                     const QIcon &pIcon);
+
+    virtual int type() const;
+
+    PMRSupport::PmrWorkspace * workspace() const;
+    PMRSupport::PmrWorkspaceFileNode * fileNode() const;
+
+    QIcon collapsedIcon() const;
+    void setCollapsedIcon(const QIcon &pCollapsedIcon);
+
+    QIcon expandedIcon() const;
+    void setExpandedIcon(const QIcon &pExpandedIcon);
+
+    QString url() const;
+    QString path() const;
+    QString fileName() const;
+
+private:
+    Type mType;
+
+    PMRSupport::PmrWorkspace *mWorkspace;
+    PMRSupport::PmrWorkspaceFileNode *mFileNode;
+
+    QIcon mCollapsedIcon;
+    QIcon mExpandedIcon;
+
+    void constructor(const Type &pType, PMRSupport::PmrWorkspace *pWorkspace,
+                     PMRSupport::PmrWorkspaceFileNode *pFileNode,
+                     const QIcon &pCollapsedIcon, const QIcon &pExpandedIcon);
+};
+
+//==============================================================================
+
+typedef QList<PmrWorkspacesWindowItem *> PmrWorkspacesWindowItems;
 
 //==============================================================================
 
