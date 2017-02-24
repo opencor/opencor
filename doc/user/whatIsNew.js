295dd3d7d2c9d4b8d74559bc28638a8eb57bb059
Alan Garny <agarny@hellix.com>
2017-02-24 14:15:55 +0100

    Revert "New snapshot."
    This reverts commit ebac51836d383e7eb35b07154eb48cde1214ce9f.

--- doc/user/whatIsNew.js
+++ doc/user/whatIsNew.js
@@ -1,5 +1,5 @@
 var jsonData = { "versions": [
-                   { "anchor": "latest", "description": "Latest snapshot", "day": 24, "month": 2, "year": 2017,
+                   { "anchor": "latest", "description": "Latest snapshot", "day": 10, "month": 2, "year": 2017,
                      "categories": [
                        { "name": "General",
                          "entries": [
