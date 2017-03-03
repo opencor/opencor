5ffc90d93f459ca727d5ea4d564ba795ae877e2f
Alan Garny <agarny@hellix.com>
2017-03-03 20:39:34 +0100

    Some minor cleaning up [ci skip].

--- src/plugins/organisation/PMRWorkspacesWindow/src/pmrworkspaceswindowsynchronizedialog.cpp
+++ src/plugins/organisation/PMRWorkspacesWindow/src/pmrworkspaceswindowsynchronizedialog.cpp
@@ -133,11 +133,11 @@ PmrWorkspacesWindowSynchronizeDialog::PmrWorkspacesWindowSynchronizeDialog(const
     messageWidget->setLayout(messageLayout);
 
     QLabel *messageLabel = new QLabel(tr("Message:"), messageWidget);
-    QFont labelFont = messageLabel->font();
+    QFont newFont = messageLabel->font();
 
-    labelFont.setBold(true);
+    newFont.setBold(true);
 
-    messageLabel->setFont(labelFont);
+    messageLabel->setFont(newFont);
 
     mMessageValue = new QTextEdit(messageWidget);
 
@@ -158,7 +158,7 @@ PmrWorkspacesWindowSynchronizeDialog::PmrWorkspacesWindowSynchronizeDialog(const
 
     QLabel *changesLabel = new QLabel(changesWidget);
 
-    changesLabel->setFont(labelFont);
+    changesLabel->setFont(newFont);
 
     mChangesValue = new QListView(changesWidget);
 
@@ -204,7 +204,7 @@ PmrWorkspacesWindowSynchronizeDialog::PmrWorkspacesWindowSynchronizeDialog(const
     mWebViewerCellmlTextFormatAction = Core::newAction(QIcon(":/CellMLSupport/logo.png"), webViewerToolBarWidget);
 
     webViewerLabel->setAlignment(Qt::AlignBottom);
-    webViewerLabel->setFont(labelFont);
+    webViewerLabel->setFont(newFont);
 
     webViewerSpacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
