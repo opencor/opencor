a08b1b2e8b53215173eb95ef3cd6320ab7763680
Alan Garny <agarny@hellix.com>
2017-03-06 16:57:34 +0100

    Some work on adding support for OpenCMISS-Zinc (#15).

--- src/plugins/miscellaneous/ZincWindow/src/zincwindowwindow.cpp
+++ src/plugins/miscellaneous/ZincWindow/src/zincwindowwindow.cpp
@@ -21,12 +21,23 @@ limitations under the License.
 //==============================================================================
 
 #include "borderedwidget.h"
+#include "corecliutils.h"
 #include "zincwidget.h"
 #include "zincwindowwindow.h"
 
 //==============================================================================
 
 #include "ui_zincwindowwindow.h"
+
+//==============================================================================
+
+#include <QDir>
+
+//==============================================================================
+
+#include "opencmiss/zinc/fieldmodule.hpp"
+#include "opencmiss/zinc/fieldvectoroperators.hpp"
+#include "opencmiss/zinc/status.hpp"
 
 //==============================================================================
 
@@ -43,10 +54,13 @@ ZincWindowWindow::ZincWindowWindow(QWidget *pParent) :
 
     mGui->setupUi(this);
 
-    // Create and add a Zinc window widget
+    // Create and add a Zinc widget
 
     mZincWidget = new ZincWidget::ZincWidget(this);
     mZincContext = new OpenCMISS::Zinc::Context("ZincWindowWindow");
+
+    mZincContext->getMaterialmodule().defineStandardMaterials();
+    mZincContext->getGlyphmodule().defineStandardGlyphs();
 
     connect(mZincWidget, SIGNAL(graphicsInitialized()),
             this, SLOT(graphicsInitialized()));
@@ -61,6 +75,102 @@ ZincWindowWindow::ZincWindowWindow(QWidget *pParent) :
 #else
     #error Unsupported platform
 #endif
+
+    // Add a tri-linear cube to our Zinc widget
+
+    // Make a temporary copy of our .exfile file
+
+    QString exFile = QDir::tempPath()+QDir::separator()+"ZincWindow/trilinearCube.exfile";
+
+    Core::writeResourceToFile(exFile, ":/ZincWindow/trilinearCube.exfile");
+
+    // Load the .exfile to our default region
+
+    OpenCMISS::Zinc::Region region = mZincContext->getDefaultRegion();
+
+    region.readFile(exFile.toUtf8().constData());
+
+    QFile::remove(exFile);
+
+    // Create a field magnitude for our default region
+
+    OpenCMISS::Zinc::Fieldmodule fieldModule = region.getFieldmodule();
+
+    fieldModule.beginChange();
+        OpenCMISS::Zinc::Field coordinates = fieldModule.findFieldByName("coordinates");
+        OpenCMISS::Zinc::FieldMagnitude magnitude = fieldModule.createFieldMagnitude(coordinates);
+
+        magnitude.setManaged(true);
+        magnitude.setName("magnitude");
+    fieldModule.endChange();
+
+    // Customise the scene of our default region
+
+    OpenCMISS::Zinc::Scene scene = region.getScene();
+
+    scene.beginChange();
+        // Black lines limiting our scene
+
+        OpenCMISS::Zinc::Materialmodule materialModule = mZincContext->getMaterialmodule();
+        OpenCMISS::Zinc::GraphicsLines lines = scene.createGraphicsLines();
+
+        lines.setCoordinateField(coordinates);
+        lines.setMaterial(materialModule.findMaterialByName("black"));
+
+        // Green spheres limiting our scene
+
+        OpenCMISS::Zinc::GraphicsPoints nodePoints = scene.createGraphicsPoints();
+
+        nodePoints.setCoordinateField(coordinates);
+        nodePoints.setFieldDomainType(OpenCMISS::Zinc::Field::DOMAIN_TYPE_NODES);
+        nodePoints.setMaterial(materialModule.findMaterialByName("green"));
+
+        // Size of our green spheres
+
+        double doubleValue = 0.05;
+
+        OpenCMISS::Zinc::Graphicspointattributes pointAttr = nodePoints.getGraphicspointattributes();
+
+        pointAttr.setBaseSize(1, &doubleValue);
+        pointAttr.setGlyphShapeType(OpenCMISS::Zinc::Glyph::SHAPE_TYPE_SPHERE);
+
+        // Axes for our scene
+
+        OpenCMISS::Zinc::GraphicsPoints axes = scene.createGraphicsPoints();
+
+        axes.setFieldDomainType(OpenCMISS::Zinc::Field::DOMAIN_TYPE_POINT);
+        axes.setMaterial(materialModule.findMaterialByName("blue"));
+
+        // Length of our axes
+
+        pointAttr = axes.getGraphicspointattributes();
+
+        doubleValue = 1.2;
+
+        pointAttr.setGlyphShapeType(OpenCMISS::Zinc::Glyph::SHAPE_TYPE_AXES_XYZ);
+        pointAttr.setBaseSize(1, &doubleValue);
+
+        // Tesselation for our scene
+
+        OpenCMISS::Zinc::Tessellation fineTessellation = mZincContext->getTessellationmodule().createTessellation();
+        int intValue = 8;
+
+        fineTessellation.setManaged(true);
+        fineTessellation.setMinimumDivisions(1, &intValue);
+        fineTessellation.setName("fine");
+
+        // Isosurfaces for our scene
+
+        OpenCMISS::Zinc::GraphicsContours isosurfaces = scene.createGraphicsContours();
+
+        doubleValue = 1.0;
+
+        isosurfaces.setCoordinateField(coordinates);
+        isosurfaces.setIsoscalarField(magnitude);
+        isosurfaces.setListIsovalues(1, &doubleValue);
+        isosurfaces.setMaterial(materialModule.findMaterialByName("gold"));
+        isosurfaces.setTessellation(fineTessellation);
+    scene.endChange();
 }
 
 //==============================================================================
