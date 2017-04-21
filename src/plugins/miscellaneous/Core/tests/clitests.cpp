e7e3630a9361b7e73e565988f78cae6d3c200881
Alan Garny <agarny@hellix.com>
2017-04-21 17:02:15 +1200

    Updated our CLI tests (#1292).
    The reason it was failing on Travis-CI is that libicui18n.so.56
    couldn’t be found. So, test against the PMR window instead.

--- src/plugins/miscellaneous/Core/tests/clitests.cpp
+++ src/plugins/miscellaneous/Core/tests/clitests.cpp
@@ -71,7 +71,7 @@ void CliTests::cliExcludeTests()
 {
     // Exclude some plugins
 
-    QCOMPARE(OpenCOR::runCli(QStringList() << "-e" << "Core" << "HelpWindow" << "Unknown"),
+    QCOMPARE(OpenCOR::runCli(QStringList() << "-e" << "Core" << "PMRWindow" << "Unknown"),
              OpenCOR::fileContents(OpenCOR::fileName("src/plugins/miscellaneous/Core/tests/data/cli/exclude.out")));
 }
 
@@ -93,7 +93,7 @@ void CliTests::cliIncludeTests()
 {
     // Include some plugins
 
-    QCOMPARE(OpenCOR::runCli(QStringList() << "-i" << "Core" << "HelpWindow" << "Unknown"),
+    QCOMPARE(OpenCOR::runCli(QStringList() << "-i" << "Core" << "PMRWindow" << "Unknown"),
              OpenCOR::fileContents(OpenCOR::fileName("src/plugins/miscellaneous/Core/tests/data/cli/include.out")));
 }
 
