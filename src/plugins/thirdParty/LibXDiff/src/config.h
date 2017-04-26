8e7a09c1d8addedc6f032a99b326a70ffeeeb6ec
Alan Garny <agarny@hellix.com>
2017-04-26 15:05:43 +1200

    Some work on adding LibXDiff as a third-party plugin (#1294).

---
+++ src/plugins/thirdParty/LibXDiff/src/config.h
@@ -0,0 +1,14 @@
+/* Name of package */
+#define PACKAGE "LibXDiff"
+
+/* Version number of package */
+#define VERSION "0.23"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.23"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "LibXDiff 0.23"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
