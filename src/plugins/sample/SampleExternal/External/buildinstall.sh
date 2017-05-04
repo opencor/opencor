b86a2adef9f78d516d7881d6deccc695e2a42a06
David Brooks <dave@bcs.co.nz>
2017-05-05 11:23:14 +1200

    Cleanup: Remove unused sample plugin (#1206).

--- src/plugins/sample/SampleExternal/External/buildinstall.sh
+++
@@ -1,7 +0,0 @@
-gcc -c -Iinclude src/external.cpp
-gcc -dynamiclib -install_name @rpath/libexternal.1.1.dylib \
-    -current_version 1.1 external.o -o lib/libexternal.1.1.dylib
-echo Installing into $1
-mkdir -p $1
-cp -r include $1/
-cp -r lib $1/
