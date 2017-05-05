340294aee45de9b3598a31054aef3abbeb5874a2
Alan Garny <agarny@hellix.com>
2017-05-05 19:29:57 +0200

    Slight updates to our downloads pages (#1106).

--- doc/downloads/index.js
+++ doc/downloads/index.js
@@ -37,18 +37,7 @@ var jsonData = { "versions": [
                        }
                      ],
                      "changes": [
-                       { "change": "<strong>Build system:</strong> make sure that the dependencies of external binaries are properly referenced <a href=\"https://github.com/opencor/opencor/issues/1281\">#1281</a>)." },
-                       { "change": "<strong>General:</strong> Added support for the <a href=\"https://github.com/OpenCMISS/zinc\">Zinc</a> library (see issue <a href=\"https://github.com/opencor/opencor/issues/15\">#15</a>). Don't make our windows have a minimum size (see issue <a href=\"https://github.com/opencor/opencor/issues/1260\">#1260</a>)." },
-                       { "change": "<strong>CLI:</strong> make it possible to include/exclude a plugin to/from the GUI version of OpenCOR (see issue <a href=\"https://github.com/opencor/opencor/issues/1292\">#1292</a>)." },
-                       { "change": "<strong>File Handling interface:</strong> removed isIndirectRemoteFile() since it's not being used anymore (see issue <a href=\"https://github.com/opencor/opencor/issues/1267\">#1267</a>)." },
-                       { "change": "<strong>CellML API:</strong> fixed a problem that made it crash OpenCOR when defining a certain type of unit (see issue <a href=\"https://github.com/opencor/opencor/issues/1278\">#1278</a>)." },
-                       { "change": "<strong>Property Editor widget:</strong> allow for navigation using the (Shift+)Tab key(s) (see issue <a href=\"https://github.com/opencor/opencor/issues/1273\">#1273</a>). Don't go to the next property when cancelling the editing of a property (direct consequence of our work on issue <a href=\"https://github.com/opencor/opencor/issues/1273\">#1273</a>; see issue <a href=\"https://github.com/opencor/opencor/issues/1285\">#1285</a>)." },
-                       { "change": "<strong>PMR-based windows:</strong> now clear the contents of those windows before switching instances (see issue <a href=\"https://github.com/opencor/opencor/issues/1279\">#1279</a>)." },
-                       { "change": "<strong>PMR window:</strong> make sure that the busy widget always disappears (see issue <a href=\"https://github.com/opencor/opencor/issues/1288\">#1288</a>)." },
-                       { "change": "<strong>PMR Workspaces window:</strong> provide more succinct diff results (see issue <a href=\"https://github.com/opencor/opencor/issues/1294\">#1294</a>)." },
-                       { "change": "<strong>CellML Text view:</strong> the import of a CellML file is now always properly converted (see issue <a href=\"https://github.com/opencor/opencor/issues/1257\">#1257</a>)." },
-                       { "change": "<strong>Single Cell view:</strong> renamed the view to Simulation Experiment view (see issue <a href=\"https://github.com/opencor/opencor/issues/1256\">#1256</a>)." },
-                       { "change": "<strong>Third-party libraries:</strong> upgraded <a href=\"https://jquery.com/\">jQuery</a> to version 3.2.1 (see issue <a href=\"https://github.com/opencor/opencor/issues/1264\">#1264</a>)" }
+                       { "change": "<strong>General:</strong> now provide a mechanism to package and download pre-built libraries (see issue <a href=\"https://github.com/opencor/opencor/issues/1106\">#1106</a>)." }
                      ]
                    },
                    { "major": 0, "minor": 4, "patch": 1, "day": 20, "month": 5, "year": 2015, "type": 0,
--- doc/downloads/previousSnapshots.js
+++ doc/downloads/previousSnapshots.js
@@ -1,4 +1,37 @@
 var jsonData = { "versions": [
+                   { "major": 0, "minor": 0, "patch": 0, "day": 28, "month": 4, "year": 2017, "type": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.8 (Mountain Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ],
+                     "changes": [
+                       { "change": "<strong>Build system:</strong> make sure that the dependencies of external binaries are properly referenced <a href=\"https://github.com/opencor/opencor/issues/1281\">#1281</a>)." },
+                       { "change": "<strong>General:</strong> Added support for the <a href=\"https://github.com/OpenCMISS/zinc\">Zinc</a> library (see issue <a href=\"https://github.com/opencor/opencor/issues/15\">#15</a>). Don't make our windows have a minimum size (see issue <a href=\"https://github.com/opencor/opencor/issues/1260\">#1260</a>)." },
+                       { "change": "<strong>CLI:</strong> make it possible to include/exclude a plugin to/from the GUI version of OpenCOR (see issue <a href=\"https://github.com/opencor/opencor/issues/1292\">#1292</a>)." },
+                       { "change": "<strong>File Handling interface:</strong> removed isIndirectRemoteFile() since it's not being used anymore (see issue <a href=\"https://github.com/opencor/opencor/issues/1267\">#1267</a>)." },
+                       { "change": "<strong>CellML API:</strong> fixed a problem that made it crash OpenCOR when defining a certain type of unit (see issue <a href=\"https://github.com/opencor/opencor/issues/1278\">#1278</a>)." },
+                       { "change": "<strong>Property Editor widget:</strong> allow for navigation using the (Shift+)Tab key(s) (see issue <a href=\"https://github.com/opencor/opencor/issues/1273\">#1273</a>). Don't go to the next property when cancelling the editing of a property (direct consequence of our work on issue <a href=\"https://github.com/opencor/opencor/issues/1273\">#1273</a>; see issue <a href=\"https://github.com/opencor/opencor/issues/1285\">#1285</a>)." },
+                       { "change": "<strong>PMR-based windows:</strong> now clear the contents of those windows before switching instances (see issue <a href=\"https://github.com/opencor/opencor/issues/1279\">#1279</a>)." },
+                       { "change": "<strong>PMR window:</strong> make sure that the busy widget always disappears (see issue <a href=\"https://github.com/opencor/opencor/issues/1288\">#1288</a>)." },
+                       { "change": "<strong>PMR Workspaces window:</strong> provide more succinct diff results (see issue <a href=\"https://github.com/opencor/opencor/issues/1294\">#1294</a>)." },
+                       { "change": "<strong>CellML Text view:</strong> the import of a CellML file is now always properly converted (see issue <a href=\"https://github.com/opencor/opencor/issues/1257\">#1257</a>)." },
+                       { "change": "<strong>Single Cell view:</strong> renamed the view to Simulation Experiment view (see issue <a href=\"https://github.com/opencor/opencor/issues/1256\">#1256</a>)." },
+                       { "change": "<strong>Third-party libraries:</strong> upgraded <a href=\"https://jquery.com/\">jQuery</a> to version 3.2.1 (see issue <a href=\"https://github.com/opencor/opencor/issues/1264\">#1264</a>)" }
+                     ]
+                   },
                    { "major": 0, "minor": 0, "patch": 0, "day": 24, "month": 3, "year": 2017, "type": 2,
                      "platforms": [
                        { "name": "Windows", "supported": "Windows 7 and later",
