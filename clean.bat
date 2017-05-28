345b44e31e8db1fe5dc3f290b76cda1ecd8561b6
Alan Garny <agarny@hellix.com>
2017-05-28 09:14:27 +0200

    General: have our cleaning script clean everything only when explicitly requested.

--- clean.bat
+++ clean.bat
@@ -4,12 +4,21 @@ TITLE Cleaning OpenCOR...
 
 SET CurrentPath=%CD%
 
-FOR %%I IN (build ext) DO (
-    CD %CurrentPath%\%%I
+CD %CurrentPath%\build
 
-    FOR /D %%I IN (*.*) DO RMDIR /S /Q "%%I"
-    FOR    %%I IN (*.*) DO DEL /Q "%%I"
-)
+FOR /D %%I IN (*.*) DO RMDIR /S /Q "%%I"
+FOR    %%I IN (*.*) DO DEL /Q "%%I"
+
+IF "%1" == "all" GOTO ALL
+
+GOTO END
+
+:ALL
+
+CD %CurrentPath%\ext
+
+FOR /D %%I IN (*.*) DO RMDIR /S /Q "%%I"
+FOR    %%I IN (*.*) DO DEL /Q "%%I"
 
 FOR %%I IN (distrib src) DO (
     CD %CurrentPath%\%%I
@@ -20,5 +29,7 @@ FOR %%I IN (distrib src) DO (
         )
     )
 )
+
+:END
 
 CD %CurrentPath%
