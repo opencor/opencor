e43928922d2cc68bbf62f676ccac602ad7e2fd55
David Brooks <dave@bcs.co.nz>
2017-06-01 13:07:33 +1200

    Tweaks and new binaries for PythonQt (#1306).

--- src/plugins/miscellaneous/PythonWrapper/src/pythonwrapperdatastore.cpp
+++ src/plugins/miscellaneous/PythonWrapper/src/pythonwrapperdatastore.cpp
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //==============================================================================
 
-#include <PythonQt/pythonqtconversion.h>
+#include <PythonQt/PythonQtConversion.h>
 
 //==============================================================================
 
