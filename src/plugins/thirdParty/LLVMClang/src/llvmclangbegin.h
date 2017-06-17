ae05d409e2d469b92fc82fa8727175da60fad761
Alan Garny <agarny@hellix.com>
2017-06-17 14:05:04 +0200

    Don't need to disable warning #4267 on Windows, except when it comes to LLVM+Clang.

--- src/plugins/thirdParty/LLVMClang/src/llvmclangbegin.h
+++ src/plugins/thirdParty/LLVMClang/src/llvmclangbegin.h
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #pragma warning(push)
     #pragma warning(disable: 4141)
     #pragma warning(disable: 4146)
+    #pragma warning(disable: 4267)
 #elif defined(Q_OS_LINUX)
     #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
     #pragma GCC diagnostic ignored "-Wstrict-aliasing"
