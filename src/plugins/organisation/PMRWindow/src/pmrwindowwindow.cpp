49f14320c2e484e5a2869f314ef06c491a8c471c
Alan Garny <agarny@hellix.com>
2017-07-03 11:38:59 +0200

    Some minor cleaning up [ci skip].

--- src/plugins/organisation/PMRWindow/src/pmrwindowwindow.cpp
+++ src/plugins/organisation/PMRWindow/src/pmrwindowwindow.cpp
@@ -101,7 +101,7 @@ PmrWindowWindow::PmrWindowWindow(QWidget *pParent) :
 
     QFont newFont = mPmrInstanceLabel->font();
 
-    newFont.setPointSizeF(0.75*newFont.pointSizeF());
+    newFont.setPointSizeF(0.7*newFont.pointSizeF());
 
     mPmrInstanceLabel->setAlignment(Qt::AlignCenter);
     mPmrInstanceLabel->setEnabled(false);
