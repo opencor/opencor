fae3147840f44f0f3d67caf4a60f133366dc3b9a
Alan Garny <agarny@hellix.com>
2017-07-13 18:23:04 +0200

    Removed unnecessary include files.

--- src/plugins/miscellaneous/Core/src/corecliutils.cpp
+++ src/plugins/miscellaneous/Core/src/corecliutils.cpp
@@ -33,7 +33,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QChar>
 #include <QCryptographicHash>
 #include <QDir>
-#include <QFileInfo>
 #include <QIODevice>
 #include <QLocale>
 #include <QMap>
