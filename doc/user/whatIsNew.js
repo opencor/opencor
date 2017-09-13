b0e01687f7cbfd2990ca14bfdd44b16ca8ba2543
Alan Garny <agarny@hellix.com>
2017-09-13 11:28:54 +0200

    New snapshot.

--- doc/user/whatIsNew.js
+++ doc/user/whatIsNew.js
@@ -1,5 +1,5 @@
 var jsonData = { "versions": [
-                   { "anchor": "latest", "description": "Latest snapshot", "day": 12, "month": 8, "year": 2017,
+                   { "anchor": "latest", "description": "Latest snapshot", "day": 13, "month": 9, "year": 2017,
                      "categories": [
                        { "name": "General",
                          "entries": [
