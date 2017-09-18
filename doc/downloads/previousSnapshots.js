7466a283eb2c4dad15f5d7712ed4fa05f29643c4
Alan Garny <agarny@hellix.com>
2017-09-18 14:13:49 +0200

    Slight updates to our downloads and what is new pages (#1413).

--- doc/downloads/previousSnapshots.js
+++ doc/downloads/previousSnapshots.js
@@ -1,4 +1,29 @@
 var jsonData = { "versions": [
+                   { "major": 0, "minor": 0, "patch": 0, "day": 13, "month": 9, "year": 2017, "type": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 16.04 LTS (Xenial Xerus) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.8 (Mountain Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ],
+                     "changes": [
+                       { "change": "<strong>General:</strong> use <a href=\"https://www.docker.com/\">Docker</a> with <a href=\"https://en.wikipedia.org/wiki/Ubuntu_(operating_system)\">Ubuntu</a> 16.04 LTS on <a href=\"https://travis-ci.org/\">Travis CI</a> (see issue <a href=\"https://github.com/opencor/opencor/issues/1395\">#1395</a>)." },
+                       { "change": "<strong>Graph Panel widget:</strong> fixed and improved the synchronisation of axes. Added support for log scales (see issue <a href=\"https://github.com/opencor/opencor/issues/1299\">#1299</a>)." },
+                       { "change": "<strong>Solvers:</strong> reference <a href=\"https://computation.llnl.gov/projects/sundials/cvodes\">CVODES</a>/<a href=\"https://computation.llnl.gov/projects/sundials/idas\">IDAS</a> rather than <a href=\"https://computation.llnl.gov/projects/sundials/cvode\">CVODE</a>/<a href=\"https://computation.llnl.gov/projects/sundials/ida\">IDA</a> (see issue <a href=\"https://github.com/opencor/opencor/issues/1405\">#1405</a>)." },
+                       { "change": "<strong>Third-party libraries:</strong> upgraded <a href=\"https://khan.github.io/KaTeX/\">KaTeX</a> to version 0.8.3 (see issue <a href=\"https://github.com/opencor/opencor/issues/1401\">#1401</a>). Upgraded <a href=\"http://www.llvm.org/\">LLVM</a>+<a href=\"http://clang.llvm.org/\">Clang</a> to version 5.0.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/1407\">#1407</a>)." }
+                     ]
+                   },
                    { "major": 0, "minor": 0, "patch": 0, "day": 12, "month": 8, "year": 2017, "type": 2,
                      "platforms": [
                        { "name": "Windows", "supported": "Windows 7 and later",
