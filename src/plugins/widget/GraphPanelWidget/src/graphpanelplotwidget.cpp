2d6c49113d1ef3368a7f5bc2d4287b9edeb45e34
Alan Garny <agarny@hellix.com>
2017-10-05 11:32:15 +0200

    Some minor cleaning up.

--- src/plugins/widget/GraphPanelWidget/src/graphpanelplotwidget.cpp
+++ src/plugins/widget/GraphPanelWidget/src/graphpanelplotwidget.cpp
@@ -1422,8 +1422,12 @@ void GraphPanelPlotWidget::mousePressEvent(QMouseEvent *pEvent)
 
     // Check which action to can carry out
 
-    if (   (pEvent->button() == Qt::LeftButton)
-        && (pEvent->modifiers() == Qt::NoModifier)) {
+    bool noModifiers =    !(pEvent->modifiers() & Qt::ShiftModifier)
+                       && !(pEvent->modifiers() & Qt::ControlModifier)
+                       && !(pEvent->modifiers() & Qt::AltModifier)
+                       && !(pEvent->modifiers() & Qt::MetaModifier);
+
+    if (noModifiers && (pEvent->button() == Qt::LeftButton)) {
         // We want to pan, but only do this if we are not completely zoomed out
 
         if (mCanZoomOutX || mCanZoomOutY) {
@@ -1431,23 +1435,28 @@ void GraphPanelPlotWidget::mousePressEvent(QMouseEvent *pEvent)
 
             mPoint = pEvent->pos();
         }
-    } else if (   (pEvent->button() == Qt::LeftButton)
-               && (pEvent->modifiers() == Qt::ShiftModifier)) {
+    } else if (    (pEvent->modifiers() & Qt::ShiftModifier)
+               && !(pEvent->modifiers() & Qt::ControlModifier)
+               && !(pEvent->modifiers() & Qt::AltModifier)
+               && !(pEvent->modifiers() & Qt::MetaModifier)
+               && (pEvent->button() == Qt::LeftButton)) {
         // We want to show the coordinates
 
         mAction = ShowCoordinates;
 
         mOverlayWidget->setPoint(pEvent->pos());
-    } else if (   (pEvent->button() == Qt::RightButton)
-               && (pEvent->modifiers() == Qt::NoModifier)) {
+    } else if (noModifiers && (pEvent->button() == Qt::RightButton)) {
         // We want to zoom in/out
 
         mAction = Zoom;
 
         mOriginPoint = pEvent->pos();
         mPoint = pEvent->pos();
-    } else if (   (pEvent->button() == Qt::RightButton)
-               && (pEvent->modifiers() == Qt::ControlModifier)) {
+    } else if (   !(pEvent->modifiers() & Qt::ShiftModifier)
+               &&  (pEvent->modifiers() & Qt::ControlModifier)
+               && !(pEvent->modifiers() & Qt::AltModifier)
+               && !(pEvent->modifiers() & Qt::MetaModifier)
+               && (pEvent->button() == Qt::RightButton)) {
         // We want to zoom a region, but only allow it if we are not already
         // fully zoomed in
 
@@ -1551,7 +1560,10 @@ void GraphPanelPlotWidget::wheelEvent(QWheelEvent *pEvent)
 {
     // Handle the wheel mouse button for zooming in/out
 
-    if (pEvent->modifiers() == Qt::NoModifier) {
+    if (   !(pEvent->modifiers() & Qt::ShiftModifier)
+        && !(pEvent->modifiers() & Qt::ControlModifier)
+        && !(pEvent->modifiers() & Qt::AltModifier)
+        && !(pEvent->modifiers() & Qt::MetaModifier)) {
         // Make sure that we are not already carrying out a action
 
         if (mAction == None) {
