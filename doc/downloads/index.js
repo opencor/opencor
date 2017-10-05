98f2877d617314dcfd87be1fc53f7157eab2f76e
Alan Garny <agarny@hellix.com>
2017-10-05 17:21:10 +0200

    Reverted our 'New snapshot' (#1412).

--- doc/downloads/index.js
+++ doc/downloads/index.js
@@ -18,7 +18,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "major": 0, "minor": 0, "patch": 0, "day": 5, "month": 10, "year": 2017, "type": 2,
+                   { "major": 0, "minor": 0, "patch": 0, "day": 25, "month": 9, "year": 2017, "type": 2,
                      "platforms": [
                        { "name": "Windows", "supported": "Windows 7 and later",
                          "files": [
