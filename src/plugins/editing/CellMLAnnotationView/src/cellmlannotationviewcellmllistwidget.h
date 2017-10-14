149006390413ff6cc96195b149bced4f61bd4093
Alan Garny <agarny@hellix.com>
2017-10-14 15:18:41 +0200

    Some minor cleaning up.

--- src/plugins/editing/CellMLAnnotationView/src/cellmlannotationviewcellmllistwidget.h
+++ src/plugins/editing/CellMLAnnotationView/src/cellmlannotationviewcellmllistwidget.h
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QModelIndex>
 #include <QStandardItem>
+#include <QStyledItemDelegate>
 #include <QStyleOptionViewItem>
 
 //==============================================================================
