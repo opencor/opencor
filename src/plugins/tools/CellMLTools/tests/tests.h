e50a1bb235a403a7eacb19348b217444d47d8948
Alan Garny <agarny@hellix.com>
2018-06-26 14:09:29 +0200

    CellML tools: slightly reorganised our tests.

--- src/plugins/tools/CellMLTools/tests/tests.h
+++ src/plugins/tools/CellMLTools/tests/tests.h
@@ -35,7 +35,8 @@ class Tests : public QObject
 
 private slots:
     void helpTests();
-    void exportTests();
+    void exportToUserDefinedFormatTests();
+    void exportToCellml10Tests();
 };
 
 //==============================================================================
