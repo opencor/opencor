d73e6a308b1ceca3be86c5166c8fb0bade7c19bd
Alan Garny <agarny@hellix.com>
2018-06-30 15:00:02 +0200

    CellML support: reworked `CellmlFile::fileVersion()`.
    The idea is that if the given file is managed, then we directly return
    its CellML version while, if it’s not managed, we try to load it and
    return its version.

--- src/plugins/support/CellMLSupport/src/cellmlfile.cpp
+++ src/plugins/support/CellMLSupport/src/cellmlfile.cpp
@@ -1175,10 +1175,26 @@ CellmlFile::Version CellmlFile::fileVersion(const QString &pFileName)
 
     CellmlFile *cellmlFile = CellmlFileManager::instance()->cellmlFile(pFileName);
 
-    if (cellmlFile)
+    if (cellmlFile) {
+        // The given CellML file is managed, so simply return its version
+
         return cellmlFile->version();
-    else
-        return Unknown;
+    } else {
+        // The given CellML file is not managed, so try to load it and return
+        // its version
+
+        ObjRef<iface::cellml_api::CellMLBootstrap> cellmlBootstrap = CreateCellMLBootstrap();
+        ObjRef<iface::cellml_api::DOMModelLoader> modelLoader = cellmlBootstrap->modelLoader();
+        ObjRef<iface::cellml_api::Model> model;
+
+        try {
+            model = modelLoader->loadFromURL(QUrl::fromPercentEncoding(QUrl::fromLocalFile(pFileName).toEncoded()).toStdWString());
+        } catch (...) {
+            return Unknown;
+        }
+
+        return modelVersion(model);
+    }
 }
 
 //==============================================================================
