26bdfb5c08e8ee9b5e9c1bd9edac3e236821e5b6
Alan Garny <agarny@hellix.com>
2018-10-12 12:09:39 +1300

    Some minor cleaning up.

--- src/plugins/solver/ForwardEulerSolver/src/forwardeulersolverplugin.cpp
+++ src/plugins/solver/ForwardEulerSolver/src/forwardeulersolverplugin.cpp
@@ -35,8 +35,8 @@ PLUGININFO_FUNC ForwardEulerSolverPluginInfo()
 {
     Descriptions descriptions;
 
-    descriptions.insert("en", QString::fromUtf8("a plugin that implements the <a href=\"http://en.wikipedia.org/wiki/Euler_method\">Foward Euler method</a> to solve <a href=\"https://en.wikipedia.org/wiki/Ordinary_differential_equation\">ODEs</a>."));
-    descriptions.insert("fr", QString::fromUtf8("une extension qui implémente la <a href=\"http://en.wikipedia.org/wiki/Euler_method\">méthode Foward Euler</a> pour résoudre des <a href=\"https://en.wikipedia.org/wiki/Ordinary_differential_equation\">EDOs</a>."));
+    descriptions.insert("en", QString::fromUtf8("a plugin that implements the <a href=\"http://en.wikipedia.org/wiki/Euler_method\">Forward Euler method</a> to solve <a href=\"https://en.wikipedia.org/wiki/Ordinary_differential_equation\">ODEs</a>."));
+    descriptions.insert("fr", QString::fromUtf8("une extension qui implémente la <a href=\"http://en.wikipedia.org/wiki/Euler_method\">méthode Forward Euler</a> pour résoudre des <a href=\"https://en.wikipedia.org/wiki/Ordinary_differential_equation\">EDOs</a>."));
 
     return new PluginInfo(PluginInfo::Solver, true, false,
                           QStringList(),
