712da78d8014789513b47ae1e33035144c8491ea
Alan Garny <agarny@hellix.com>
2018-11-20 17:53:23 +1300

    Some minor cleaning up.

--- src/plugins/simulation/SimulationExperimentView/src/simulationexperimentviewsimulationwidget.cpp
+++ src/plugins/simulation/SimulationExperimentView/src/simulationexperimentviewsimulationwidget.cpp
@@ -671,8 +671,8 @@ void SimulationExperimentViewSimulationWidget::updateSimulationMode()
     mClearSimulationResultsAction->setEnabled(    mSimulation->results()->size()
                                               && !simulationModeEnabled);
     mSimulationResultsExportAction->setEnabled(    mSimulationResultsExportDropDownMenu->actions().count()
-                                            &&  mSimulation->results()->size()
-                                            && !simulationModeEnabled);
+                                               &&  mSimulation->results()->size()
+                                               && !simulationModeEnabled);
 
     // Give the focus to our focus proxy, in case we leave our simulation mode
     // (so that the user can modify simulation data, etc.)
