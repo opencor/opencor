67318b3a287b4df8f3a0e8604a4e601db53ecfc5
Alan Garny <agarny@hellix.com>
2018-12-18 16:40:17 +1300

    Some minor cleaning up.

--- src/plugins/simulation/PendulumWindow/src/pendulumwindowwindow.cpp
+++ src/plugins/simulation/PendulumWindow/src/pendulumwindowwindow.cpp
@@ -36,6 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QCheckBox>
 #include <QDir>
 #include <QLabel>
+#include <QSlider>
 
 //==============================================================================
 
