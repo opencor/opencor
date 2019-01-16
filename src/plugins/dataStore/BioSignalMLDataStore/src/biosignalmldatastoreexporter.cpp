8a6ee87b57e904b571f8aadab53b58283809dbfc
Alan Garny <agarny@hellix.com>
2019-01-16 18:04:35 +1300

    Some minor cleaning up.

--- src/plugins/dataStore/BioSignalMLDataStore/src/biosignalmldatastoreexporter.cpp
+++ src/plugins/dataStore/BioSignalMLDataStore/src/biosignalmldatastoreexporter.cpp
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //==============================================================================
 
-#include "biosignalml/biosignalml.h"
+//#include "biosignalml/biosignalml.h"
 #include "biosignalml/data/hdf5.h"
 
 //==============================================================================
