d0abac84098595f8941ef569dc398db1c3d5a09f
Alan Garny <agarny@hellix.com>
2019-01-17 15:32:32 +1300

    BioSignalML data store: can now import the data contained in a BioSignalML file (#1845).

--- src/plugins/dataStore/BioSignalMLDataStore/src/biosignalmldatastoreimporter.cpp
+++ src/plugins/dataStore/BioSignalMLDataStore/src/biosignalmldatastoreimporter.cpp
@@ -25,6 +25,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //==============================================================================
 
+#include "biosignalml/data/hdf5.h"
+
+//==============================================================================
+
 namespace OpenCOR {
 namespace BioSignalMLDataStore {
 
@@ -39,10 +43,71 @@ BiosignalmlDataStoreImporterWorker::BiosignalmlDataStoreImporterWorker(DataStore
 
 void BiosignalmlDataStoreImporterWorker::run()
 {
-//---ISSUE1845--- TO BE DONE...
+    // Import our BioSignalML file in our data store
+
+    bsml::HDF5::Recording *recording = nullptr;
+    QString errorMessage = QString();
+
+    try {
+        // Retrieve our clock and determine our number of data points
+
+        recording = new bsml::HDF5::Recording(mImportData->fileName().toStdString());
+
+        std::vector<double> clockTicks = recording->get_clock(recording->get_clock_uris().front())->read();
+        quint64 nbOfDataPoints = clockTicks.size();
+
+        // Retrieve the values of our different variables
+
+        std::list<rdf::URI> signalUris = recording->get_signal_uris();
+        int nbOfVariables = int(signalUris.size());
+        bsml::data::TimeSeries::Ptr *variables = new bsml::data::TimeSeries::Ptr[nbOfVariables] {};
+        bsml::data::TimeSeries::Ptr *variable = variables-1;
+
+        for (auto signalUri : signalUris)
+            *(++variable) = recording->get_signal(signalUri)->read();
+
+        // Set up our data store
+
+        DataStore::DataStore *dataStore = mImportData->dataStore();
+        double *values = new double[nbOfVariables] {};
+
+        dataStore->addVariables(values, nbOfVariables);
+
+        // Add a run to our data store and store the values of our different
+        // variables to it
+
+        double oneOverNbOfDataPoints = 1.0/nbOfDataPoints;
+
+        if (dataStore->addRun(nbOfDataPoints)) {
+            for (quint64 i = 1; i <= nbOfDataPoints; ++i) {
+                for (int j = 0; j < nbOfVariables; ++j)
+                    values[j] = variables[j]->data()[i-1];
+
+                dataStore->addValues(clockTicks[i-1]);
+
+                emit progress(mImportData, i*oneOverNbOfDataPoints);
+            }
+        } else {
+            errorMessage = tr("The memory needed to store the data could not be allocated.");
+        }
+
+        delete[] values;
+        delete[] variables;
+    } catch (bsml::data::Exception &exception) {
+        // Something went wrong, so retrieve the error message
+
+        errorMessage = tr("The data could not be imported from BioSignalML (%1).").arg(exception.what());
+    }
+
+    if (recording) {
+        recording->close();
+
+        delete recording;
+    }
+
     // Let people know that our import is done
 
-    emit done(mImportData, QString());
+    emit done(mImportData, errorMessage);
 }
 
 //==============================================================================
