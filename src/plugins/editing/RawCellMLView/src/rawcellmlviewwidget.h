5dadc8b96a4f86ebef31eee21acafbc082ce5d02
Alan Garny <agarny@hellix.com>
2019-01-25 14:14:06 +1300

    Some minor cleaning up.

--- src/plugins/editing/RawCellMLView/src/rawcellmlviewwidget.h
+++ src/plugins/editing/RawCellMLView/src/rawcellmlviewwidget.h
@@ -67,8 +67,6 @@ public:
 
     void retranslateUi() override;
 
-    bool contains(const QString &pFileName) const;
-
     void initialize(const QString &pFileName, bool pUpdate = true);
     void finalize(const QString &pFileName);
 
