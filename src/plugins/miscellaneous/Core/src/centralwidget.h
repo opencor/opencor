b33675b6bb48803ec85c228e89b9e7b964c63145
Alan Garny <agarny@hellix.com>
2019-02-20 11:30:12 +1300

    Some minor cleaning up.

--- src/plugins/miscellaneous/Core/src/centralwidget.h
+++ src/plugins/miscellaneous/Core/src/centralwidget.h
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //==============================================================================
 
 #include "file.h"
-#include "guiinterface.h"
 #include "widget.h"
 #include "viewinterface.h"
 
