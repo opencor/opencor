0e63e8f438da2c97d3b582f1b568a6807eeed7ec
Alan Garny <agarny@hellix.com>
2019-05-13 13:07:34 +1200

    New snapshot.

--- doc/downloads/index.js
+++ doc/downloads/index.js
@@ -18,7 +18,7 @@ var jsonData = { "versions": [
                        }
                      ]
                    },
-                   { "major": 0, "minor": 0, "patch": 0, "day": 5, "month": 5, "year": 2019, "type": 2, "license": 2,
+                   { "major": 0, "minor": 0, "patch": 0, "day": 13, "month": 5, "year": 2019, "type": 2, "license": 2,
                      "platforms": [
                        { "name": "Windows", "supported": "Windows 7 and later",
                          "files": [
