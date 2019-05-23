5cda8de15b311ca2ccc77203d377788b3c27466c
David Brooks <dave@bcs.co.nz>
2019-05-24 11:52:13 +1200

    Python: minor tidy.

--- src/plugins/dataStore/DataStore/src/datastorepythonwrapper.h
+++ src/plugins/dataStore/DataStore/src/datastorepythonwrapper.h
@@ -40,10 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //==============================================================================
 
-#include <functional>
-
-//==============================================================================
-
 namespace OpenCOR {
 namespace DataStore {
 
