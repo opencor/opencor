952ba71a667c017f527ada963fdccb18e47caf09
Alan Garny <agarny@hellix.com>
2019-06-14 09:41:05 +1200

    CellML support: improved the validation reporting of models with imports (#2109).

--- src/plugins/editing/RawCellMLView/i18n/RawCellMLView_fr.ts
+++ src/plugins/editing/RawCellMLView/i18n/RawCellMLView_fr.ts
@@ -11,14 +11,6 @@
 <context>
     <name>OpenCOR::RawCellMLView::RawCellmlViewWidget</name>
     <message>
-        <source>The issue reported below may be related to this CellML file or to one of its (in)directly imported CellML files.</source>
-        <translation>Le problème mentionné ci-dessous peut être en rapport avec ce fichier CellML ou avec un de ses fichiers CellML qui a été (in)directement importé.</translation>
-    </message>
-    <message>
-        <source>The issues reported below may be related to this CellML file and/or to one or several of its (in)directly imported CellML files.</source>
-        <translation>Les problèmes mentionnés ci-dessous peuvent être en rapport avec ce fichier CellML et/ou avec un ou plusieurs de ses fichiers CellML qui ont été (in)directement importés.</translation>
-    </message>
-    <message>
         <source>the &lt;a href=&quot;https://github.com/cellmlapi/cellml-api/&quot;&gt;CellML validation service&lt;/a&gt; is known to have limitations and may therefore incorrectly (in)validate certain CellML files.</source>
         <translation>le &lt;a href=&quot;https://github.com/cellmlapi/cellml-api/&quot;&gt;service de validation CellML&lt;/a&gt; est connu pour avoir des problèmes et peut donc incorrectement (in)valider certains fichiers CellML.</translation>
     </message>
