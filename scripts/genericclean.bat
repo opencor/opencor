35b119b24670e6696764869b0a8533d35d823367
Alan Garny <agarny@hellix.com>
2019-09-23 19:00:08 +1200

    Some minor cleaning up.

--- scripts/genericclean.bat
+++ scripts/genericclean.bat
@@ -1,16 +1,10 @@
 @ECHO OFF
 
-IF "%1" == "all" GOTO ALL1
-
-TITLE Cleaning OpenCOR...
-
-GOTO NEXT
-
-:ALL1
-
-TITLE Cleaning all of OpenCOR...
-
-:NEXT
+IF "%1" == "all" (
+    TITLE Cleaning all of OpenCOR...
+) ELSE (
+    TITLE Cleaning OpenCOR...
+)
 
 SET CurrentPath=%CD%
 
@@ -20,17 +14,11 @@ FOR    %%I IN (*.*) DO ATTRIB -R "%%I"
 FOR /D %%I IN (*.*) DO RMDIR /S /Q "%%I"
 FOR    %%I IN (*.*) DO DEL /Q "%%I"
 
-IF "%1" == "all" GOTO ALL2
+IF "%1" == "all" (
+    CD %CurrentPath%\ext
 
-GOTO END
-
-:ALL2
-
-CD %CurrentPath%\ext
-
-FOR /D %%I IN (*.*) DO RMDIR /S /Q "%%I"
-FOR    %%I IN (*.*) DO DEL /Q "%%I"
-
-:END
+    FOR /D %%I IN (*.*) DO RMDIR /S /Q "%%I"
+    FOR    %%I IN (*.*) DO DEL /Q "%%I"
+)
 
 CD %CurrentPath%
