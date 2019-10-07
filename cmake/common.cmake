68fe128678fd5e4c6a7af3f4102ee1424a304a08
David Brooks <dave@bcs.co.nz>
2019-10-08 11:53:57 +1300

    Python: tidy up build dependency code, making sure that the path to Python is set in scripts *before* they are used.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -287,7 +287,7 @@ macro(add_plugin PLUGIN_NAME)
         # Create a custom target for installing files
         # Note: this is to prevent Ninja from getting confused with circular
         #       references...
-        # Note: we use this target in Python build scripts when setting
+        # Note: we use this target in the PythonQtAPI build script when setting
         #       the PYTHON_DEPENDENCIES list
 
         SET(INSTALL_EXTERNAL_FILES_TARGET "${PROJECT_NAME}_INSTALL_EXTERNAL_FILES")
