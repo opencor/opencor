75726d8297cd39d4e4355e01ca0229c2c92fa7dc
Alan Garny <agarny@hellix.com>
2019-10-16 16:26:04 +1300

    Some minor cleaning up.

--- distrib/windows/start_jupyter.py
+++ distrib/windows/start_jupyter.py
@@ -1,21 +1,28 @@
-import sys, os
+import os
 import subprocess
+import sys
 
 # Get OpenCOR's root directory
+
 root = '\\'.join(sys.executable.split('\\')[:-3])
 
-# Add the location of our binaries to the head of the system PATH
+# Add the location of our binaries to the beginning of the system PATH
+
 path = os.environ['PATH'].split(';')
+
 path.insert(0, '%s\\bin' % root)
 path.insert(0, '%s\\Python\\Scripts' % root)
 path.insert(0, '%s\\Python\\bin' % root)
+
 os.environ['PATH'] = ';'.join(path)
 
+# Start Jupyter and wait for it to terminate
+
 try:
-    # Start Jupyter and wait for it to be terminated
     jupyter = subprocess.Popen(['jupyter'] + sys.argv[1:],
                                stderr=sys.stderr,
                                stdout=sys.stdout)
+
     jupyter.wait()
 except KeyboardInterrupt:
     pass
