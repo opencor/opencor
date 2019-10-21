25ecc1fe8eb1ac4e59a606e14eba44104bb9d793
Alan Garny <agarny@hellix.com>
2019-10-21 14:45:33 +1300

    Some minor cleaning up.

--- src/plugins/dataStore/DataStore/src/datastorepythonwrapper.cpp
+++ src/plugins/dataStore/DataStore/src/datastorepythonwrapper.cpp
@@ -40,14 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //==============================================================================
 
-#include "PythonQt/PythonQtConversion.h"
-
-//==============================================================================
-
-#include <stdexcept>
-
-//==============================================================================
-
 namespace OpenCOR {
 namespace DataStore {
 
