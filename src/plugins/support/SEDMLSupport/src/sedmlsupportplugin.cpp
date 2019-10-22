9f98faa7678b0f04dc1dc40c2fe3e77f33f73427
Alan Garny <agarny@hellix.com>
2019-10-22 16:16:30 +1300

    Some minor cleaning up.

--- src/plugins/support/SEDMLSupport/src/sedmlsupportplugin.cpp
+++ src/plugins/support/SEDMLSupport/src/sedmlsupportplugin.cpp
@@ -262,7 +262,7 @@ void SEDMLSupportPlugin::newSedmlFile()
     // Make sure that the file has indeed been created
 
     if (createStatus != Core::FileManager::Status::Created) {
-        qFatal("FATAL ERROR | %s:%d: the new SED-ML file did not get created.", __FILE__, __LINE__);
+        qFatal("FATAL ERROR | %s:%d: the new SED-ML file could not be created.", __FILE__, __LINE__);
     }
 #endif
 }
