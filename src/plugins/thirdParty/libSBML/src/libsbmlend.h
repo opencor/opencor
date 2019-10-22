9985e5f4e8c5fc975e252101e7091b9f2eaddb03
Alan Garny <agarny@hellix.com>
2019-10-22 14:08:34 +1300

    Some minor cleaning up.

--- src/plugins/thirdParty/libSBML/src/libsbmlend.h
+++ src/plugins/thirdParty/libSBML/src/libsbmlend.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // libSBML end
 //==============================================================================
 
-#ifdef Q_OS_WIN
+#if defined(Q_OS_WIN)
     #pragma warning(pop)
 #elif defined(Q_OS_MAC)
     #pragma clang diagnostic pop
