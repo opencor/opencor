86b59af18f0fc55b1d945d1a958da2a9b01fb70b
Alan Garny <agarny@hellix.com>
2019-10-30 11:25:37 +0900

    Python: cleaned up the code for the PythonShell plugin.

--- src/plugins/miscellaneous/PythonShell/src/pythonshellplugin.cpp
+++ src/plugins/miscellaneous/PythonShell/src/pythonshellplugin.cpp
@@ -27,13 +27,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //==============================================================================
 
-#include <QCoreApplication>
-#include <QFile>
-#include <QSettings>
-#include <QTimer>
-
-//==============================================================================
-
 namespace OpenCOR {
 namespace PythonShell {
 
@@ -44,12 +37,10 @@ PLUGININFO_FUNC PythonShellPluginInfo()
     Descriptions descriptions;
 
     descriptions.insert("en", QString::fromUtf8("the Python shell plugin."));
-    descriptions.insert("fr", QString::fromUtf8("the Python shell plugin."));
+    descriptions.insert("fr", QString::fromUtf8("le plugin shell Python."));
 
     return new PluginInfo(PluginInfo::Category::Miscellaneous, true, true,
-                          QStringList() << "Core" << "DataStore"
-                                        << "SimulationSupport"
-                                        << "PythonQtSupport",
+                          QStringList() << "Core" << "PythonQtSupport" << "SimulationSupport",
                           descriptions);
 }
 
@@ -95,11 +86,16 @@ void PythonShellPlugin::runHelpCommand()
 {
     // Output the commands we support
 
-    std::cout << "Commands supported by the Python shell plugin:" << std::endl;
-    std::cout << " * Display the commands supported by the plugin:" << std::endl;
+    std::cout << "Commands supported by the PythonShell plugin:" << std::endl;
+    std::cout << " * Display the commands supported by the PythonShell plugin:" << std::endl;
     std::cout << "      help" << std::endl;
     std::cout << " * Run an interactive Python shell in OpenCOR's environment:" << std::endl;
-    std::cout << "      python [option] ... [-c cmd | -m mod | file | -] [arg] ..." << std::endl;
+    std::cout << "      [python] [<option> ...] [-c <command> | -m <module> | <file> | -] [<argument> ...]" << std::endl;
+    std::cout << "   where" << std::endl;
+    std::cout << "      -c <command> executes a program passed in as a string" << std::endl;
+    std::cout << "      -m <module> runs a library module as a script" << std::endl;
+    std::cout << "      <file> runs a program read from a script file" << std::endl;
+    std::cout << "      - runs a program read from the standard input" << std::endl;
 }
 
 //==============================================================================
@@ -109,13 +105,12 @@ bool PythonShellPlugin::runPython(const QStringList &pArguments, int &pRes)
     // The following has been adapted from `Programs/python.c` in the Python
     // source code
 
-    const int argC = pArguments.size() + 1;
+    const int argC = pArguments.size()+1;
     auto argV = reinterpret_cast<wchar_t **>(PyMem_RawMalloc((argC+1)*sizeof(wchar_t*)));
-
     auto argVCopy = reinterpret_cast<wchar_t **>(PyMem_RawMalloc((argC+1)*sizeof(wchar_t*)));
 
     if ((argV == nullptr) || (argVCopy == nullptr)) {
-        fprintf(stderr, "out of memory\n");
+        std::cerr << "Out of memory." << std::endl;
 
         pRes = 1;
 
@@ -129,7 +124,7 @@ bool PythonShellPlugin::runPython(const QStringList &pArguments, int &pRes)
     char *oldloc = _PyMem_RawStrdup(setlocale(LC_ALL, nullptr));
 
     if (oldloc == nullptr) {
-        fprintf(stderr, "out of memory\n");
+        std::cerr << "Out of memory." << std::endl;
 
         pRes = 1;
 
@@ -146,7 +141,7 @@ bool PythonShellPlugin::runPython(const QStringList &pArguments, int &pRes)
         if (argV[i] == nullptr) {
             PyMem_RawFree(oldloc);
 
-            fprintf(stderr, "Fatal Python error: unable to decode the command line argument #%i\n", i+1);
+            std::cerr << "Fatal Python error: unable to decode the command line argument #" << i+1 << "." << std::endl;
 
             pRes = 1;
 
