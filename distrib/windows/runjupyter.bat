c3f7ff9bf5d03436bf60a72703fd8c84a2fe40ee
Alan Garny <agarny@hellix.com>
2019-11-01 13:56:20 +0900

    Python: merged `setpythonpath` into `runjupyter` since, now, it's only used there.

--- distrib/windows/runjupyter.bat
+++ distrib/windows/runjupyter.bat
@@ -4,6 +4,6 @@ TITLE Running the Jupyter environment...
 
 SET appDir=%~dp0
 
-CALL "%appDir%setpythonpath.bat"
+"%appDir%Python\bin\Python.exe" "%appDir%Python\Scripts\set_python_path.py" "%appDir%Python" -s
 
 "%appDir%Python\bin\Python.exe" "%appDir%Python\Scripts\start_jupyter.py" %*
--- distrib/windows/setpythonpath.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-SET appDir=%~dp0
-
-"%appDir%Python\bin\Python.exe" "%appDir%Python\Scripts\set_python_path.py" "%appDir%Python" -s
