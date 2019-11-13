23a6258b7f21467a4131218960278805f0e0f074
Alan Garny <agarny@hellix.com>
2019-11-13 14:21:55 +1300

    Some minor cleaning up.

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Main
 //==============================================================================
 
+#include <Qt>
+
+//==============================================================================
+
 #ifdef Q_OS_LINUX
     #include "Python.h"
     // Note: this needs to be included before <QObject> to prevent name clashes...
@@ -41,10 +45,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #include "checkforupdatesdialog.h"
     #include "splashscreenwindow.h"
 #endif
-
-//==============================================================================
-
-#include <Qt>
 
 //==============================================================================
 
