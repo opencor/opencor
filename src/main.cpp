358349c9204fc2e27d42822189aec6d248774073
Alan Garny <agarny@hellix.com>
2019-11-25 17:41:38 +1300

    Make sure that we can build OpenCOR without Python support on Linux.

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //==============================================================================
 
-#ifdef Q_OS_LINUX
+#if defined(PYTHON_SUPPORT) and defined(Q_OS_LINUX)
     #include "Python.h"
     // Note: this needs to be included before <QObject> to prevent name clashes...
 #endif
@@ -72,7 +72,7 @@ int main(int pArgC, char *pArgV[])
     //       using dlopen() (see
     //       https://www.akkadia.org/drepper/dsohowto.pdf)...
 
-#ifdef Q_OS_LINUX
+#if defined(PYTHON_SUPPORT) and defined(Q_OS_LINUX)
     Py_NoUserSiteDirectory = 1;
 #endif
 
