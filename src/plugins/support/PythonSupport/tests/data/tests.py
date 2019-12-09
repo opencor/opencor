4ea4845a18bdac3c5b782e299129a98bf0cdfa7b
Alan Garny <agarny@hellix.com>
2019-12-09 16:41:40 +1300

    Some minor cleaning up.

--- src/plugins/support/PythonSupport/tests/data/tests.py
+++ src/plugins/support/PythonSupport/tests/data/tests.py
@@ -50,6 +50,8 @@ def test_simulation(title: str, file_name_or_url: str, first: bool = True) -> No
     oc.close_simulation(simulation)
 
 
+# Test for an unknown local/remote file
+
 try:
     test_simulation('Unknown local file', 'unknown')
 except Exception as e:
@@ -60,6 +62,8 @@ try:
                     'https://unknown', False)
 except Exception as e:
     print(repr(e))
+
+# Test for an invalid local/remote file
 
 try:
     test_simulation('Invalid local file', 'cellml/underconstrained_model.cellml', False)
@@ -73,13 +77,19 @@ try:
 except Exception as e:
     print(repr(e))
 
+# Test for a local/remote CellML file
+
 test_simulation('Local CellML file', 'cellml/lorenz.cellml', False)
 test_simulation('Remote CellML file',
                 'https://raw.githubusercontent.com/opencor/opencor/master/models/tests/cellml/lorenz.cellml', False)
 
+# Test for a local/remote SED-ML file
+
 test_simulation('Local SED-ML file', 'sedml/lorenz.sedml', False)
 test_simulation('Remote SED-ML file',
                 'https://raw.githubusercontent.com/opencor/opencor/master/models/tests/sedml/lorenz.sedml', False)
+
+# Test for a local/remote COMBINE archive
 
 test_simulation('Local COMBINE archive', 'combine/lorenz.omex', False)
 test_simulation('Remote COMBINE archive',
