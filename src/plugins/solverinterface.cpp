9eda696887ae28099e87c1f97f83f9f5f4bba069
Alan Garny <agarny@hellix.com>
2020-01-22 12:44:18 +1300

    Some minor cleaning up.

--- src/plugins/solverinterface.cpp
+++ src/plugins/solverinterface.cpp
@@ -30,7 +30,7 @@ void doNonLinearSolve(char *pRuntime,
                       double *pParameters, int pSize, void *pUserData)
 {
     // Retrieve the NLA solver which we should use and solve our NLA system
-    // Note: we shouldn't always have an NLA solver, but better be safe than
+    // Note: we should always have an NLA solver, but better be safe than
     //       sorry...
 
     OpenCOR::Solver::NlaSolver *nlaSolver = OpenCOR::Solver::nlaSolver(pRuntime);
