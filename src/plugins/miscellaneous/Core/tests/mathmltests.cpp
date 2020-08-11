a4864f0fe109cbbe9bc0651c60c027134f3a3bf3
Alan Garny <agarny@hellix.com>
2020-08-11 16:44:31 +1200

    Core tests: we need our MathML tests to use QTEST_GUILESS_MAIN() on Windows.
    Otherwise, we get something like:
    ```
    ********* Start testing of MathmlTests *********
    Config: Using QtTest library 5.12.9, Qt 5.12.9 (x86_64-little_endian-llp64 shared (dynamic) debug build; by MSVC 2017)
    PASS   : MathmlTests::initTestCase()
    QFATAL : MathmlTests::plusTests() ASSERT: "!m_baseURI.isRelative()" in file environment\qgenericstaticcontext.cpp, line 82
    FAIL!  : MathmlTests::plusTests() Received a fatal error.
    Unknown file(0) : failure location
    Totals: 1 passed, 1 failed, 0 skipped, 0 blacklisted, 91ms
    ********* Finished testing of MathmlTests *********
    ```

--- src/plugins/miscellaneous/Core/tests/mathmltests.cpp
+++ src/plugins/miscellaneous/Core/tests/mathmltests.cpp
@@ -284,7 +284,7 @@ void MathmlTests::trigonometricTests()
 
 //==============================================================================
 
-QTEST_APPLESS_MAIN(MathmlTests)
+QTEST_GUILESS_MAIN(MathmlTests)
 
 //==============================================================================
 // End of file
