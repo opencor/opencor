3dc9075c04eb15c8cd4e17ebd664a26129b86cc1
Alan Garny <agarny@hellix.com>
2020-12-06 20:06:13 +1300

    Added a CI script (#2423).

--- ci.bat
+++ ci.bat
@@ -1,29 +1,111 @@
 @ECHO OFF
 
-CALL %~dp0clean
-CALL %~dp0scripts\genericmake CIRelease
+SET AppDir=%~dp0..\
 
-SET ExitCode=!ERRORLEVEL!
+IF EXIST %AppDir%build (
+    FOR %%X IN (ninja.exe) DO (
+        SET NinjaFound=%%~$PATH:X
+    )
 
-IF !ExitCode! NEQ 0 (
+    IF DEFINED NinjaFound (
+        SET Generator=Ninja
+        SET CMakeGenerator=Ninja
+    ) ELSE (
+        SET Generator=JOM
+        SET CMakeGenerator=NMake Makefiles JOM
+    )
+
+    SET OrigDir=%CD%
+
+    CALL %AppDir%clean
+
+    TITLE Building and testing the release version of OpenCOR (using !Generator!)...
+
+    CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+    CD %AppDir%build
+
+    cmake -G "!CMakeGenerator!" -DCMAKE_BUILD_TYPE=Release -DENABLE_SAMPLE_PLUGINS=ON -DENABLE_TEST_PLUGINS=ON -DENABLE_TESTS=ON ..
+
+    SET ExitCode=!ERRORLEVEL!
+
+    IF NOT !ExitCode! EQU 0 (
+        CD %OrigDir%
+
+        EXIT /B !ExitCode!
+    )
+
+    FOR /F "TOKENS=1,* DELIMS= " %%X IN ("%*") DO (
+        SET Args=%%Y
+    )
+
+    IF DEFINED NinjaFound (
+        ninja !Args!
+
+        SET ExitCode=!ERRORLEVEL!
+    ) ELSE (
+        jom !Args!
+
+        SET ExitCode=!ERRORLEVEL!
+    )
+
+    CD %OrigDir%
+
+    IF NOT !ExitCode! EQU 0 (
+        EXIT /B !ExitCode!
+    )
+
+    SET AppTestsExe=%AppDir%build\bin\runtests.exe
+
+    %AppTestsExe%
+
+    SET ExitCode=!ERRORLEVEL!
+
+    IF NOT !ExitCode! EQU 0 (
+        EXIT /B !ExitCode!
+    )
+
+    CALL %AppDir%clean
+
+    TITLE Building and testing the debug version of OpenCOR (using !Generator!)...
+
+    CD %AppDir%build
+
+    cmake -G "!CMakeGenerator!" -DCMAKE_BUILD_TYPE=Debug -DENABLE_SAMPLE_PLUGINS=ON -DENABLE_TEST_PLUGINS=ON -DENABLE_TESTS=ON ..
+
+    SET ExitCode=!ERRORLEVEL!
+
+    IF NOT !ExitCode! EQU 0 (
+        CD %OrigDir%
+
+        EXIT /B !ExitCode!
+    )
+
+    IF DEFINED NinjaFound (
+        ninja !Args!
+
+        SET ExitCode=!ERRORLEVEL!
+    ) ELSE (
+        jom !Args!
+
+        SET ExitCode=!ERRORLEVEL!
+    )
+
+    CD %OrigDir%
+
+    IF NOT !ExitCode! EQU 0 (
+        EXIT /B !ExitCode!
+    )
+
+    SET AppTestsExe=%AppDir%build\bin\runtests.exe
+
+    %AppTestsExe%
+
+    SET ExitCode=!ERRORLEVEL!
+
     EXIT /B !ExitCode!
+) ELSE (
+    ECHO OpenCOR's build directory is missing.
+
+    EXIT /B 1
 )
-
-CALL %~dp0runtests
-
-SET ExitCode=!ERRORLEVEL!
-
-IF !ExitCode! NEQ 0 (
-    EXIT /B !ExitCode!
-)
-
-CALL %~dp0clean
-CALL %~dp0scripts\genericmake CIDebug
-
-SET ExitCode=!ERRORLEVEL!
-
-IF !ExitCode! NEQ 0 (
-    EXIT /B !ExitCode!
-)
-
-CALL %~dp0runtests
--- scripts/genericmake.bat
+++ scripts/genericmake.bat
@@ -5,24 +5,14 @@ SET AppDir=%~dp0..\
 IF EXIST %AppDir%build (
     IF "%1" == "Release" (
         SET CMakeBuildType=Release
-        SET EnableSamplePlugins=OFF
-        SET EnableTestPlugins=OFF
         SET EnableTests=OFF
     ) ELSE IF "%1" == "Debug" (
         SET CMakeBuildType=Debug
-        SET EnableSamplePlugins=OFF
-        SET EnableTestPlugins=OFF
         SET EnableTests=ON
-    ) ELSE IF "%1" == "CIRelease" (
-        SET CMakeBuildType=Release
-        SET EnableSamplePlugins=ON
-        SET EnableTestPlugins=ON
-        SET EnableTests=ON
-    ) ELSE IF "%1" == "CIDebug" (
-        SET CMakeBuildType=Debug
-        SET EnableSamplePlugins=ON
-        SET EnableTestPlugins=ON
-        SET EnableTests=ON
+    ) ELSE (
+        ECHO Only the Release and Debug options are supported.
+
+        EXIT /B 1
     )
 
     FOR %%X IN (ninja.exe) DO (
@@ -31,8 +21,10 @@ IF EXIST %AppDir%build (
 
     IF DEFINED NinjaFound (
         SET Generator=Ninja
+        SET CMakeGenerator=Ninja
     ) ELSE (
         SET Generator=JOM
+        SET CMakeGenerator=NMake Makefiles JOM
     )
 
     IF "%1" == "Release" (
@@ -41,7 +33,7 @@ IF EXIST %AppDir%build (
         SET TitleTests= and its tests
     )
 
-    TITLE Making OpenCOR!TitleTests! using !Generator!...
+    TITLE Building OpenCOR!TitleTests! (using !Generator!)...
 
     CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
@@ -49,33 +41,31 @@ IF EXIST %AppDir%build (
 
     CD %AppDir%build
 
-    IF DEFINED NinjaFound (
-        SET CMakeGenerator=Ninja
-    ) ELSE (
-        SET CMakeGenerator=NMake Makefiles JOM
-    )
-
-    cmake -G "!CMakeGenerator!" -DCMAKE_BUILD_TYPE=!CMakeBuildType! -DENABLE_SAMPLE_PLUGINS=!EnableSamplePlugins! -DENABLE_TEST_PLUGINS=!EnableTestPlugins! -DENABLE_TESTS=!EnableTests! ..
+    cmake -G "!CMakeGenerator!" -DCMAKE_BUILD_TYPE=!CMakeBuildType! -DENABLE_TESTS=!EnableTests! ..
 
     SET ExitCode=!ERRORLEVEL!
 
-    IF !ExitCode! EQU 0 (
-        FOR /F "TOKENS=1,* DELIMS= " %%X IN ("%*") DO (
-            SET Args=%%Y
-        )
+    IF NOT !ExitCode! EQU 0 (
+        CD %OrigDir%
 
-        IF DEFINED NinjaFound (
-            ninja !Args!
-
-            SET ExitCode=!ERRORLEVEL!
-        ) ELSE (
-            jom !Args!
-
-            SET ExitCode=!ERRORLEVEL!
-        )
+        EXIT /B !ExitCode!
     )
 
-    CD "%OrigDir%"
+    FOR /F "TOKENS=1,* DELIMS= " %%X IN ("%*") DO (
+        SET Args=%%Y
+    )
+
+    IF DEFINED NinjaFound (
+        ninja !Args!
+
+        SET ExitCode=!ERRORLEVEL!
+    ) ELSE (
+        jom !Args!
+
+        SET ExitCode=!ERRORLEVEL!
+    )
+
+    CD %OrigDir%
 
     EXIT /B !ExitCode!
 ) ELSE (
