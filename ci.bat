c9bc9c83aabc7a65fc149eaa03764fb2b022004b
Alan Garny <agarny@hellix.com>
2021-03-31 01:11:14 +1300

    Windows: trying to find out what is going wrong with our Python coverage tests.
    Indeed, they regularly fail on our CI Windows machine...!?

--- ci.bat
+++ ci.bat
@@ -57,6 +57,8 @@ IF EXIST %AppDir%build (
         EXIT /B !ExitCode!
     )
 
+    !AppDir!build\bin\run -c PythonShell !AppDir!\src\plugins\support\PythonSupport\tests\data\coveragetests.py
+
     SET AppTestsExe=!AppDir!build\bin\runtests.exe
 
     !AppTestsExe!
