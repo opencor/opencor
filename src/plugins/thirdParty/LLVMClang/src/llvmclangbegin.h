b11e7c933d2886d2559ec7375f29c8f7e04becdd
Alan Garny <agarny@hellix.com>
2022-05-25 13:04:41 +1200

    Some minor cleaning up.

--- src/plugins/thirdParty/LLVMClang/src/llvmclangbegin.h
+++ src/plugins/thirdParty/LLVMClang/src/llvmclangbegin.h
@@ -27,7 +27,6 @@ along with this program. If not, see <https://gnu.org/licenses>.
 
 #if defined(_MSC_VER) && !defined(__clang__)
     #pragma warning(push)
-    #pragma warning(disable: 4005)
     #pragma warning(disable: 4146)
     #pragma warning(disable: 4244)
     #pragma warning(disable: 4251)
@@ -35,25 +34,22 @@ along with this program. If not, see <https://gnu.org/licenses>.
     #pragma warning(disable: 4275)
     #pragma warning(disable: 4291)
     #pragma warning(disable: 4624)
+    #pragma warning(disable: 4996)
 #elif defined(__GNUC__) && !defined(__clang__)
     #pragma GCC diagnostic push
-    #if defined(__GNUC__) && (__GNUC__ >= 8)
-        #pragma GCC diagnostic ignored "-Wclass-memaccess"
-    #endif
-    #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
     #pragma GCC diagnostic ignored "-Wredundant-move"
-    #pragma GCC diagnostic ignored "-Wstrict-aliasing"
     #pragma GCC diagnostic ignored "-Wunused-parameter"
 #else
     #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wbitfield-enum-conversion"
     #pragma clang diagnostic ignored "-Wcast-align"
     #pragma clang diagnostic ignored "-Wcomma"
-    #pragma clang diagnostic ignored "-Wconversion"
-    #pragma clang diagnostic ignored "-Wdeprecated"
+    #pragma clang diagnostic ignored "-Wdeprecated-copy-with-dtor"
     #pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
     #pragma clang diagnostic ignored "-Wduplicate-enum"
     #pragma clang diagnostic ignored "-Wextra-semi"
     #pragma clang diagnostic ignored "-Wimplicit-int-conversion"
+    #pragma clang diagnostic ignored "-Winconsistent-missing-destructor-override"
     #pragma clang diagnostic ignored "-Wmissing-noreturn"
     #pragma clang diagnostic ignored "-Wmissing-variable-declarations"
     #pragma clang diagnostic ignored "-Wold-style-cast"
@@ -65,11 +61,9 @@ along with this program. If not, see <https://gnu.org/licenses>.
     #pragma clang diagnostic ignored "-Wshorten-64-to-32"
     #pragma clang diagnostic ignored "-Wsigned-enum-bitfield"
     #pragma clang diagnostic ignored "-Wswitch-enum"
-    #pragma clang diagnostic ignored "-Wundef"
+    #pragma clang diagnostic ignored "-Wtautological-type-limit-compare"
     #pragma clang diagnostic ignored "-Wundefined-func-template"
-    #pragma clang diagnostic ignored "-Wundefined-reinterpret-cast"
     #pragma clang diagnostic ignored "-Wunused-parameter"
-    #pragma clang diagnostic ignored "-Wunused-template"
     #pragma clang diagnostic ignored "-Wweak-vtables"
 #endif
 
